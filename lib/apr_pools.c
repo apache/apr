1e63ec2080290dc78337687fbcbccf2a0216759e
Ryan Bloom <rbb@apache.org>
1999-09-08 19:56:21 +0000

    Fix some STUPID typos I made on my last commit. Submitted by:  Greg Stein
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59204 13f79535-47bb-0310-9956-ffa450edef68

--- lib/apr_pools.c
+++ lib/apr_pools.c
@@ -75,10 +75,10 @@
 #ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
 #endif
-#ifdef HAVE_SYS_STAT_H
+#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
 #endif
-#ifdef HAVE_SYS_STAT_H
+#ifdef HAVE_FCNTL_H
 #include <fcntl.h>
 #endif
 
