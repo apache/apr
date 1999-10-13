21abfaee3d1acbadb5697dbec2fcb82b9edc77e3
David Reid <dreid@apache.org>
1999-10-13 22:49:52 +0000

    Remove some silly typo's, some naughty tabs that crept in and generally tidy up.  Also start adding in ap_null_cleanup in line with recent changes.
    There is so little difference between intra and inter proc in these
    implementations, and I'm not sure that BeOS will ever need different
    versions due to the way it works.  I'll give it some thought but might
    rationalise these into a single set of routines.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59344 13f79535-47bb-0310-9956-ffa450edef68

--- locks/beos/crossproc.c
+++ locks/beos/crossproc.c
@@ -55,10 +55,12 @@
 
 #include "apr_lock.h"
 #include "apr_general.h"
+#include "apr_lib.h"
 #include "locks.h"
 
-ap_status_t lock_inter_cleanup(ap_lock_t *lock)
+ap_status_t lock_inter_cleanup(void * data)
 {
+    ap_lock_t *lock = (ap_lock_t*)data;
     if (lock->curr_locked == 1) {
     	if (atomic_add(&lock->ben_interproc , -1) > 1){
     		release_sem (lock->sem_interproc);
@@ -71,14 +73,14 @@ ap_status_t create_inter_lock(ap_lock_t *new)
 {
     new->sem_interproc = (sem_id)ap_palloc(new->cntxt, sizeof(sem_id));
     new->ben_interproc = (int32)ap_palloc(new->cntxt, sizeof(int32));
-    
+
     new->ben_interproc = 0;
-    new->sem_interproc = create_sem(0, "ap_intraproc");
+    new->sem_interproc = create_sem(0, "ap_interproc");
     if (new->sem_interproc < B_NO_ERROR){
     	lock_inter_cleanup(new);
         return errno;
     }
-    new->curr_locked == 0;
+    new->curr_locked = 0;
     ap_register_cleanup(new->cntxt, (void *)new, lock_inter_cleanup,
                         ap_null_cleanup);
     return APR_SUCCESS;
@@ -91,18 +93,18 @@ ap_status_t lock_inter(ap_lock_t *lock)
 	} else {
 		return errno;
 	}
-    lock->curr_locked == 1;
+    lock->curr_locked = 1;
     return APR_SUCCESS;
 }
 
 ap_status_t unlock_inter(ap_lock_t *lock)
 {
 	if (atomic_add(&lock->ben_interproc, -1) > 1){
-		release_sem(lock->sem_interproc);
+        release_sem(lock->sem_interproc);
+        lock->curr_locked = 0;
     } else {
     	return errno;
     }
-    lock->curr_locked == 0;
     return APR_SUCCESS;
 }
 
