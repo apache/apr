9afd193d03cd5a3b4d2f539cc2231b3fd0d00082
Manoj Kasichainula <manoj@apache.org>
1999-11-04 07:24:23 +0000

    a rewrite of ap_fprintf that fixes a potential buffer overflow, a memory leak, and more.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59450 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -356,28 +356,18 @@ API_EXPORT(int) ap_fprintf(struct file_t *fptr, const char *format, ...)
 {
     int cc;
     va_list ap;
-    ap_vformatter_buff_t vbuff;
     char *buf;
-    ap_ssize_t len;
+    int len;
 
     buf = malloc(HUGE_STRING_LEN);
     if (buf == NULL) {
         return 0;
     }
-    /* save one byte for nul terminator */
-    vbuff.curpos = buf;
-    vbuff.endpos = buf + len - 1;
     va_start(ap, format);
-#if 0
-    cc = ap_vformatter(printf_flush, &vbuff, format, ap);
+    len = ap_vsnprintf(buf, HUGE_STRING_LEN, format, ap);
+    cc = ap_puts(buf, fptr);
     va_end(ap);
-    *vbuff.curpos = '\0';
-#endif
-    vsprintf(buf, format, ap);
-    len = strlen(buf);
-    cc = ap_write(fptr, buf, &len);
-    va_end(ap);
-    return (cc == -1) ? len : cc;
+    free(buf);
+    return (cc == APR_SUCCESS) ? len : -1;
 }
-
 
