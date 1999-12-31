f73e4afbae9c23935fd6e3ea44aeeecab3053ba4
Ryan Bloom <rbb@apache.org>
1999-12-31 01:16:53 +0000

    Fix a nasty little bug that was causing a seg fault everytime we tried starting Apache with an existing pid file.  When APR allocates space, it has to allocate the right amount.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59562 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/open.c
+++ file_io/unix/open.c
@@ -311,7 +311,7 @@ ap_status_t ap_ferror(ap_file_t *fptr)
  */
 ap_status_t ap_open_stderr(struct file_t **thefile, ap_context_t *cont)
 {
-    (*thefile) = ap_pcalloc(cont, sizeof(ap_os_file_t *));
+    (*thefile) = ap_pcalloc(cont, sizeof(struct file_t));
     if ((*thefile) == NULL) {
         return APR_ENOMEM;
     }
