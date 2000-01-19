279ea20e93a75f123acd7f75472de0df17859362
Ryan Bloom <rbb@apache.org>
2000-01-19 01:14:57 +0000

    Finish the commits for the change in the header files.  Basically, this hides all of the Apache macros that modules don't need access to.  This should have been committed with the modules, but I wasn't paying attention to the directory I was in when I ran the commit. Submitted by:	Manoj Kasichainula and Ryan Bloom
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59608 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_general.h
+++ include/apr_general.h
@@ -182,6 +182,40 @@ typedef int               ap_signum_t;
 #define APR_INLINE /*nop*/
 #endif
 
+/* Finding offsets of elements within structures.
+ * Taken from the X code... they've sweated portability of this stuff
+ * so we don't have to.  Sigh...
+ */
+
+#if defined(CRAY) || (defined(__arm) && !defined(LINUX))
+#ifdef __STDC__
+#define XtOffset(p_type,field) _Offsetof(p_type,field)
+#else
+#ifdef CRAY2
+#define XtOffset(p_type,field) \
+        (sizeof(int)*((unsigned int)&(((p_type)NULL)->field)))
+
+#else /* !CRAY2 */
+
+#define XtOffset(p_type,field) ((unsigned int)&(((p_type)NULL)->field))
+
+#endif /* !CRAY2 */
+#endif /* __STDC__ */
+#else /* ! (CRAY || __arm) */
+
+#define XtOffset(p_type,field) \
+        ((long) (((char *) (&(((p_type)NULL)->field))) - ((char *) NULL)))
+
+#endif /* !CRAY */
+
+#ifdef offsetof
+#define XtOffsetOf(s_type,field) offsetof(s_type,field)
+#else
+#define XtOffsetOf(s_type,field) XtOffset(s_type*,field)
+#endif
+
+
+
 /* Context functions */
 ap_status_t ap_create_context(ap_context_t **, ap_context_t *);
 ap_status_t ap_destroy_context(struct context_t *cont);
