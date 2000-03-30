b3b0a47e925096e6813804cbfd669fa797720f10
Ryan Bloom <rbb@apache.org>
2000-03-30 00:37:37 +0000

    Fix reading of buffered files on Unix Submitted by:	Jon Travis <jtravis@covalent.net> Reviewed by:	Ryan Bloom
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59748 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -70,7 +70,7 @@ ap_status_t ap_read(struct file_t *thefile, void *buf, ap_ssize_t *nbytes)
 {
     ap_ssize_t rv;
 
-    if (thefile->filedes < 0) {
+    if (thefile->filedes < 0 && !thefile->buffered) {
         *nbytes = 0;
         return APR_EBADF;
     }
