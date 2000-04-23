ceba48455a5860f4796c12aea17f43e59f54bc0a
Roy T. Fielding <fielding@apache.org>
2000-04-23 01:33:40 +0000

    Replace APR_MD5_CTX with ap_MD5_ctx_t, as suggested by Bill Rowe.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59934 13f79535-47bb-0310-9956-ffa450edef68

--- lib/apr_md5.c
+++ lib/apr_md5.c
@@ -181,7 +181,7 @@ static unsigned char PADDING[64] =
 
 /* MD5 initialization. Begins an MD5 operation, writing a new context.
  */
-API_EXPORT(ap_status_t) ap_MD5Init(APR_MD5_CTX * context)
+API_EXPORT(ap_status_t) ap_MD5Init(ap_MD5_ctx_t * context)
 {
     context->count[0] = context->count[1] = 0;
     /* Load magic initialization constants. */
@@ -196,7 +196,7 @@ API_EXPORT(ap_status_t) ap_MD5Init(APR_MD5_CTX * context)
    operation, processing another message block, and updating the
    context.
  */
-API_EXPORT(ap_status_t) ap_MD5Update(APR_MD5_CTX * context, const unsigned char *input,
+API_EXPORT(ap_status_t) ap_MD5Update(ap_MD5_ctx_t * context, const unsigned char *input,
 			   unsigned int inputLen)
 {
     unsigned int i, idx, partLen;
@@ -252,7 +252,7 @@ API_EXPORT(ap_status_t) ap_MD5Update(APR_MD5_CTX * context, const unsigned char 
 /* MD5 finalization. Ends an MD5 message-digest operation, writing the
    the message digest and zeroizing the context.
  */
-API_EXPORT(ap_status_t) ap_MD5Final(unsigned char digest[16], APR_MD5_CTX * context)
+API_EXPORT(ap_status_t) ap_MD5Final(unsigned char digest[16], ap_MD5_ctx_t * context)
 {
     unsigned char bits[8];
     unsigned int idx, padLen;
@@ -446,7 +446,7 @@ API_EXPORT(ap_status_t) ap_MD5Encode(const char *pw, const char *salt,
     const char *sp, *ep;
     unsigned char final[16];
     int sl, pl, i;
-    APR_MD5_CTX ctx, ctx1;
+    ap_MD5_ctx_t ctx, ctx1;
     unsigned long l;
 
     /* 
