06529256967b3d7d64f204b0c53ce6cced070afe
Bill Stoddard <stoddard@apache.org>
2000-04-24 14:57:54 +0000

    Change names of strnatcmp & strnatcasecmp to cpmply with APR naming conventions
    Submitted by:	William Rowe
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@59945 13f79535-47bb-0310-9956-ffa450edef68

--- lib/apr_strnatcmp.c
+++ lib/apr_strnatcmp.c
@@ -140,12 +140,12 @@ static int strnatcmp0(char const *a, char const *b, int fold_case)
 
 
 
-int strnatcmp(char const *a, char const *b) {
+int ap_strnatcmp(char const *a, char const *b) {
      return strnatcmp0(a, b, 0);
 }
 
 
 /* Compare, recognizing numeric string and ignoring case. */
-int strnatcasecmp(char const *a, char const *b) {
+int ap_strnatcasecmp(char const *a, char const *b) {
      return strnatcmp0(a, b, 1);
 }
