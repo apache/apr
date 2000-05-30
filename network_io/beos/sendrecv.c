533d63b679e690c296d50d9b263203b9f674bfac
Ryan Bloom <rbb@apache.org>
2000-05-30 18:43:06 +0000

    Put the BeOS network code back into APR.  According the David Reid, without this code the newer releases of BeOS don't work.  I would still like to see this code combined with the Unix code, but I get the feeling that this code is going to morph as the native BeOS networking API becomes more mature.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60119 13f79535-47bb-0310-9956-ffa450edef68

---
+++ network_io/beos/sendrecv.c
@@ -0,0 +1,150 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "apr_private.h"
+#ifdef HAVE_NETINET_TCP_H
+#include "../unix/sendrecv.c"
+#else
+#include "networkio.h"
+
+static ap_status_t wait_for_io_or_timeout(ap_socket_t *sock, int for_read)
+{
+    struct timeval tv, *tvptr;
+    fd_set fdset;
+    int srv;
+
+    do {
+        FD_ZERO(&fdset);
+        FD_SET(sock->socketdes, &fdset);
+        if (sock->timeout < 0) {
+            tvptr = NULL;
+        }
+        else {
+            tv.tv_sec = sock->timeout / AP_USEC_PER_SEC;
+            tv.tv_usec = sock->timeout % AP_USEC_PER_SEC;
+            tvptr = &tv;
+        }
+        srv = select(sock->socketdes + 1,
+            for_read ? &fdset : NULL,
+            for_read ? NULL : &fdset, 
+            NULL,
+            tvptr);
+            /* TODO - timeout should be smaller on repeats of this loop */
+    } while (srv == -1 && errno == EINTR);
+
+    if (srv == 0) {
+        return APR_TIMEUP;
+    }
+    else if (srv < 0) {
+        return errno;
+    }
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_send(ap_socket_t *sock, const char *buf, ap_ssize_t *len)
+{
+    ssize_t rv;
+	
+    do {
+        rv = send(sock->socketdes, buf, (*len), 0);
+    } while (rv == -1 && errno == EINTR);
+
+    if (rv == -1 && errno == EWOULDBLOCK && sock->timeout > 0) {
+        ap_status_t arv = wait_for_io_or_timeout(sock, 0);
+        if (arv != APR_SUCCESS) {
+            *len = 0;
+            return arv;
+        }
+        else {
+            do {
+                rv = send(sock->socketdes, buf, (*len), 0);
+            } while (rv == -1 && errno == EINTR);
+        }
+    }
+    if (rv == -1) {
+        *len = 0;
+        return errno;
+    }
+    (*len) = rv;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_recv(ap_socket_t *sock, char *buf, ap_ssize_t *len)
+{
+    ap_ssize_t rv;
+   
+    do {
+        rv = recv(sock->socketdes, buf, (*len), 0);
+    } while (rv == -1 && errno == EINTR);
+
+    if (rv == -1 && errno == EWOULDBLOCK && sock->timeout > 0) {
+        ap_status_t arv = wait_for_io_or_timeout(sock, 1);
+        if (arv != APR_SUCCESS) {
+            *len = 0;
+            return arv;
+        }
+        else {
+            do {
+                rv = recv(sock->socketdes, buf, (*len), 0);
+            } while (rv == -1 && errno == EINTR);
+        }
+    }
+    if (rv == -1) {
+        (*len) = 0;
+        return errno;
+    }
+    (*len) = rv;
+    return APR_SUCCESS;
+}
+#endif
