dab4161eb64b450fc95d55654538c4612522d4c0
Ryan Bloom <rbb@apache.org>
2000-05-30 18:43:06 +0000

    Put the BeOS network code back into APR.  According the David Reid, without this code the newer releases of BeOS don't work.  I would still like to see this code combined with the Unix code, but I get the feeling that this code is going to morph as the native BeOS networking API becomes more mature.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60119 13f79535-47bb-0310-9956-ffa450edef68

---
+++ network_io/beos/poll.c
@@ -0,0 +1,224 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "apr_private.h"
+#ifdef HAVE_NETINET_TCP_H
+#include "../unix/poll.c"
+#else
+#include "networkio.h"
+
+/*  BeOS R4 doesn't have a poll function, but R5 will have */
+/*  so for the time being we try our best with an implementaion that */
+/*  uses select.  However, select on beos isn't that hot either, so */
+/*  until R5 we have to live with a less than perfect implementation */
+
+/*  Apparently those sneaky people at Be included support for write in */
+/*  select for R4.5 of BeOS.  So here we use code that uses the write */
+/*  bits. */
+    
+ap_status_t ap_setup_poll(ap_pollfd_t **new, ap_int32_t num, ap_pool_t *cont)
+{
+    (*new) = (ap_pollfd_t *)ap_palloc(cont, sizeof(ap_pollfd_t) * num);
+    if ((*new) == NULL) {
+        return APR_ENOMEM;
+    }
+    (*new)->cntxt = cont;
+    (*new)->read = (fd_set *)ap_palloc(cont, sizeof(fd_set));
+    (*new)->write = (fd_set *)ap_palloc(cont, sizeof(fd_set));
+    (*new)->except = (fd_set *)ap_palloc(cont, sizeof(fd_set));
+    FD_ZERO((*new)->read);
+    FD_ZERO((*new)->write);
+    FD_ZERO((*new)->except);
+    (*new)->highsock = -1;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_add_poll_socket(ap_pollfd_t *aprset,
+                               ap_socket_t *sock, ap_int16_t event)
+{
+    if (event & APR_POLLIN) {
+        FD_SET(sock->socketdes, aprset->read);
+    }
+    if (event & APR_POLLPRI) {
+        FD_SET(sock->socketdes, aprset->read);
+    }
+    if (event & APR_POLLOUT) {
+        FD_SET(sock->socketdes, aprset->write);
+    }
+    if (sock->socketdes > aprset->highsock) {
+        aprset->highsock = sock->socketdes;
+    }
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_poll(ap_pollfd_t *aprset, ap_int32_t *nsds, 
+		    ap_interval_time_t timeout)
+{
+    int rv;
+    struct timeval tv, *tvptr;
+
+    if (timeout < 0) {
+        tvptr = NULL;
+    }
+    else {
+        tv.tv_sec = timeout / AP_USEC_PER_SEC;
+        tv.tv_usec = timeout % AP_USEC_PER_SEC;
+	tvptr = &tv;
+    }
+
+    rv = select(aprset->highsock + 1, aprset->read, aprset->write, 
+                    NULL, tvptr);
+
+    (*nsds) = rv;
+    if ((*nsds) == 0) {
+        return APR_TIMEUP;
+    }
+    if ((*nsds) < 0) {
+        return APR_EEXIST;
+    }
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_revents(ap_int16_t *event, ap_socket_t *sock, ap_pollfd_t *aprset)
+{
+    ap_int16_t revents = 0;
+    char data[256];
+    int dummy = 256;
+
+    if (FD_ISSET(sock->socketdes, aprset->read)) {
+        revents |= APR_POLLIN;
+        if (recv(sock->socketdes, &data, 0, 0) == -1) {
+            switch (errno) {
+                case ECONNRESET:
+                case ECONNABORTED:
+                case ESHUTDOWN:
+                case ENETRESET: {
+                    revents ^= APR_POLLIN;
+                    revents |= APR_POLLHUP;
+                    break;
+                }
+                case ENOTSOCK: {
+                    revents ^= APR_POLLIN;
+                    revents |= APR_POLLNVAL;
+                }
+                default: {
+                    revents ^= APR_POLLIN;
+                    revents |= APR_POLLERR;
+                }
+            }
+        }
+    }
+    if (FD_ISSET(sock->socketdes, aprset->write)) {
+        revents |= APR_POLLOUT;
+    }
+
+    /* Still no support for execpt bits in BeOS R4.5 so for the time being */
+    /* we can't check this.  Hopefully the error checking above will allow */
+    /* sufficient errors to be recognised to cover this. */
+
+    /*if (FD_ISSET(sock->socketdes, aprset->except)) {
+        revents |= APR_POLLPRI;
+    }*/
+
+    (*event) = revents;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_remove_poll_socket(ap_pollfd_t *aprset, ap_socket_t *sock)
+{
+    FD_CLR(sock->socketdes, aprset->read);
+    FD_CLR(sock->socketdes, aprset->read);
+    FD_CLR(sock->socketdes, aprset->write);
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_clear_poll_sockets(ap_pollfd_t *aprset, ap_int16_t event)
+{
+    if (event & APR_POLLIN) {
+        FD_ZERO(aprset->read);
+    }
+    if (event & APR_POLLPRI) {
+        FD_ZERO(aprset->read);
+    }
+    if (event & APR_POLLOUT) {
+        FD_ZERO(aprset->write);
+    }
+    aprset->highsock = 0;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_polldata(ap_pollfd_t *pollfd, char *key, void *data)
+{
+    if (pollfd != NULL) {
+        return ap_get_userdata(data, key, pollfd->cntxt);
+    }
+    else {
+        data = NULL;
+        return APR_ENOFILE;
+    }
+}
+
+ap_status_t ap_set_polldata(ap_pollfd_t *pollfd, void *data, char *key,
+                            ap_status_t (*cleanup) (void *))
+{
+    if (pollfd != NULL) {
+        return ap_set_userdata(data, key, cleanup, pollfd->cntxt);
+    }
+    else {
+        data = NULL;
+        return APR_ENOFILE;
+    }
+}
+#endif
+
---
+++ network_io/beos/sockaddr.c
@@ -0,0 +1,183 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "apr_private.h"
+#ifdef HAVE_NETINET_TCP_H
+#include "../unix/sockaddr.c"
+#else
+#include "networkio.h"
+
+ap_status_t ap_set_local_port(ap_socket_t *sock, ap_uint32_t port)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+    sock->local_addr->sin_port = htons((short)port);
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_set_remote_port(ap_socket_t *sock, ap_uint32_t port)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+    sock->remote_addr->sin_port = htons((short)port);
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_local_port(ap_uint32_t *port, ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+    *port = ntohs(sock->local_addr->sin_port);
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_remote_port(ap_uint32_t *port, ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+    *port = ntohs(sock->remote_addr->sin_port);
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_set_local_ipaddr(ap_socket_t *sock, const char *addr)
+{
+    u_long ipaddr;
+
+    if (!sock) {
+        return APR_EBADF;
+    }
+    
+    if (!strcmp(addr, APR_ANYADDR)) {
+        sock->local_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+        return APR_SUCCESS;
+    }
+    
+    ipaddr = inet_addr(addr);
+    
+    if (ipaddr == -1) {
+        return errno;
+    }
+    
+    sock->local_addr->sin_addr.s_addr = ipaddr;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_set_remote_ipaddr(ap_socket_t *sock, const char *addr)
+{
+    u_long ipaddr;
+
+    if (!sock) {
+        return APR_EBADF;
+    }
+    
+    if (!strcmp(addr, APR_ANYADDR)) {
+        sock->remote_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+        return APR_SUCCESS;
+    }
+    
+    ipaddr = inet_addr(addr);
+    
+    if (ipaddr == (u_long)-1) {
+        return errno;
+    }
+    
+    sock->remote_addr->sin_addr.s_addr = ipaddr;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_local_ipaddr(char **addr, const ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+
+    *addr = ap_pstrdup(sock->cntxt, inet_ntoa(sock->local_addr->sin_addr));
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_remote_ipaddr(char **addr, const ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+
+    *addr = ap_pstrdup(sock->cntxt, inet_ntoa(sock->remote_addr->sin_addr));
+    return APR_SUCCESS;
+}
+
+
+ap_status_t ap_get_local_name(struct sockaddr_in **name, const ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+
+    *name = sock->local_addr;
+    return APR_SUCCESS;
+}
+
+ap_status_t ap_get_remote_name(struct sockaddr_in **name, const ap_socket_t *sock)
+{
+    if (!sock) {
+        return APR_EBADF;
+    }
+
+    *name = sock->remote_addr;
+    return APR_SUCCESS;
+}
+#endif
