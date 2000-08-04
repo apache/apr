d57851fbc49e3c9a8e8030ddf0d92ceaa23c537b
(no author) <(no author)@unknown>
2000-08-04 12:51:23 +0000

    This commit was manufactured by cvs2svn to create tag 'APACHE_2_0_ALPHA_5'.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/tags/APACHE_2_0_ALPHA_5@60476 13f79535-47bb-0310-9956-ffa450edef68

--- misc/win32/libapr.c
+++
@@ -1,7 +0,0 @@
-/*
- * Placeholder to force aprlib.dll creation with no LNK4001 error
- *
- * However, this isn't a bad place to store dynamic-only functions 
- * that determine which version of apr the application has loaded.
- * These functions are of (less?) importance to static-bound apps.
- */
