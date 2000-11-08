3b1f79261bd874773e7b6742dca50fe26f51de60
Tony Finch <fanf@apache.org>
2000-11-08 00:54:23 +0000

    Use the right index into the new array whan resizing the hash table.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60643 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_hash.c
+++ tables/apr_hash.c
@@ -191,7 +191,7 @@ static void expand_array(apr_hash_t *ht)
     new_max = ht->max * 2 + 1;
     new_array = alloc_array(ht, new_max);
     for (hi = apr_hash_first(ht); hi; hi = apr_hash_next(hi)) {
-	i = hi->this->hash & ht->max;
+	i = hi->this->hash & new_max;
 	hi->this->next = new_array[i];
 	new_array[i] = hi->this;
     }	       
