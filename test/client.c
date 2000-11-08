664b4e6f13d4a70d280fb9cfda8c3fe363ca8467
David Reid <dreid@apache.org>
2000-11-08 11:02:13 +0000

    Change to apr_port_t to remove compiler warnings and bring us up to date.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60645 13f79535-47bb-0310-9956-ffa450edef68

--- test/client.c
+++ test/client.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
     char msgbuf[80];
     char *local_ipaddr, *remote_ipaddr;
     char *dest = "127.0.0.1";
-    apr_uint32_t local_port, remote_port;
+    apr_port_t local_port, remote_port;
     apr_interval_time_t read_timeout = -1;
 
     setbuf(stdout, NULL);
