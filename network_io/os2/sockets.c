dc73868ddc2682e909b7b75c4b6262d3d4f93e63
Brian Havard <bjh@apache.org>
2000-11-14 14:08:38 +0000

    OS/2: soclose() segfaults if passed -1 so prevent this from happening if a closed socket is closed (was happening before BUFF was removed).
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60724 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/os2/sockets.c
+++ network_io/os2/sockets.c
@@ -69,6 +69,11 @@
 static apr_status_t socket_cleanup(void *sock)
 {
     apr_socket_t *thesocket = sock;
+
+    if (thesocket->socketdes < 0) {
+        return APR_EINVALSOCK;
+    }
+
     if (soclose(thesocket->socketdes) == 0) {
         thesocket->socketdes = -1;
         return APR_SUCCESS;
