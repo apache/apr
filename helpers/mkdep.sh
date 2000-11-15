f4a84154fe43662d0191e0205c1b8800773aa3c1
Greg Stein <gstein@apache.org>
2000-11-15 20:00:37 +0000

    *) no reason to leave Makefile.bak turds since Makefile is a generated file       (from Makefile.in) *) add some comments about what is happening
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60735 13f79535-47bb-0310-9956-ffa450edef68

--- helpers/mkdep.sh
+++ helpers/mkdep.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cp Makefile Makefile.bak \
-    && sed -ne '1,/^# DO NOT REMOVE/p' Makefile > Makefile.new \
+#
+# 1) remove everything after the DO NOT REMOVE
+# 2) generate the dependencies, adding them to the end of Makefile.new
+# 3) move the Makefile.new back into place
+#
+# Note that we use && to ensure that Makefile is not changed if an error
+# occurs during the process
+#
+sed -ne '1,/^# DO NOT REMOVE/p' Makefile > Makefile.new \
     && gcc -MM  $* >> Makefile.new \
     && mv Makefile.new Makefile
