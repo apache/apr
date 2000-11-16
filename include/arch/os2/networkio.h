70d104e0379ca61ff4bca848af2182612572c833
Jeff Trawick <trawick@apache.org>
2000-11-16 02:17:29 +0000

    port initial set of David Reid's IPv6 support to OS/2 (not compiled, not tested)
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60738 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/os2/networkio.h
+++ include/arch/os2/networkio.h
@@ -66,9 +66,8 @@
 struct apr_socket_t {
     apr_pool_t *cntxt;
     int socketdes;
-    struct sockaddr_in *local_addr;
-    struct sockaddr_in *remote_addr;
-    int addr_len;
+    apr_sockaddr_t *local_addr;
+    apr_sockaddr_t *remote_addr;
     apr_interval_time_t timeout;
     int nonblock;
     int local_port_unknown;
