a79578117f73f79f420bc450a0a2d7ad4a9743b4
Jeff Trawick <trawick@apache.org>
2000-11-16 01:51:36 +0000

    This is a small step toward's David Reid's IPv6 patch.  I started with the part of David's patch which uses apr_sockaddr_t instead of sockaddr_in inside apr_socket_t, got everything to compile (and seem to run properly on Unix), and simplified some of the code which allocates storage for apr_socket_t and fills out fields in apr_sockaddr_t.
    I didn't add all the code from the patch that supports IPv6
    throughout.  I didn't add any of the API enhancements.
    
    This includes a port of the code to Win32.  I'll later commit the
    required changes for OS/2.
    
    Submitted by:	David Reid
    Reviewed by:	Jeff Trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60737 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/win32/sockets.c
+++ network_io/win32/sockets.c
@@ -59,7 +59,6 @@
 #include "apr_portable.h"
 #include <string.h>
 
-
 static apr_status_t socket_cleanup(void *sock)
 {
     apr_socket_t *thesocket = sock;
@@ -73,22 +72,57 @@ static apr_status_t socket_cleanup(void *sock)
     return APR_SUCCESS;
 }
 
+static void set_socket_vars(apr_socket_t *sock, int family)
+{
+    sock->local_addr->sa.sin.sin_family = family;
+    sock->remote_addr->sa.sin.sin_family = family;
+
+    if (family == AF_INET) {
+        sock->local_addr->sa_len = sizeof(struct sockaddr_in);
+        sock->local_addr->addr_str_len = 16;
+        sock->local_addr->ipaddr_ptr = &(sock->local_addr->sa.sin.sin_addr);
+        sock->local_addr->ipaddr_len = sizeof(struct in_addr);
+
+        sock->remote_addr->sa_len = sizeof(struct sockaddr_in);
+        sock->remote_addr->addr_str_len = 16;
+        sock->remote_addr->ipaddr_ptr = &(sock->remote_addr->sa.sin.sin_addr);
+        sock->remote_addr->ipaddr_len = sizeof(struct in_addr);
+    }
+#if APR_HAVE_IPV6
+    else if (family == AF_INET6) {
+        sock->local_addr->sa_len = sizeof(struct sockaddr_in6);
+        sock->local_addr->addr_str_len = 46;
+        sock->local_addr->ipaddr_ptr = &(sock->local_addr->sa.sin6.sin6_addr);
+        sock->local_addr->ipaddr_len = sizeof(struct in6_addr);
+
+        sock->remote_addr->sa_len = sizeof(struct sockaddr_in6);
+        sock->remote_addr->addr_str_len = 46;
+        sock->remote_addr->ipaddr_ptr = &(sock->remote_addr->sa.sin6.sin6_addr);
+        sock->remote_addr->ipaddr_len = sizeof(struct in6_addr);
+    }
+#endif
+}                                                                                                  
+static void alloc_socket(apr_socket_t **new, apr_pool_t *p)
+{
+    *new = (apr_socket_t *)apr_pcalloc(p, sizeof(apr_socket_t));
+    (*new)->cntxt = p;
+    (*new)->local_addr = (apr_sockaddr_t *)apr_pcalloc((*new)->cntxt,
+                                                       sizeof(apr_sockaddr_t));
+    (*new)->remote_addr = (apr_sockaddr_t *)apr_pcalloc((*new)->cntxt,
+                                                        sizeof(apr_sockaddr_t));
+}
+
 apr_status_t apr_create_tcp_socket(apr_socket_t **new, apr_pool_t *cont)
 {
-    (*new) = (apr_socket_t *)apr_pcalloc(cont, sizeof(apr_socket_t));
+    alloc_socket(new, cont);
 
     if ((*new) == NULL) {
         return APR_ENOMEM;
     }
-    (*new)->cntxt = cont; 
-    (*new)->local_addr = (struct sockaddr_in *)apr_pcalloc((*new)->cntxt,
-                                                          sizeof(struct sockaddr_in));
-    (*new)->remote_addr = (struct sockaddr_in *)apr_pcalloc((*new)->cntxt,
-                          sizeof(struct sockaddr_in));
-
     if (((*new)->local_addr == NULL) || ((*new)->remote_addr == NULL)) {
         return APR_ENOMEM;
     }
+
     /* For right now, we are not using socket groups.  We may later.
      * No flags to use when creating a socket, so use 0 for that parameter as well.
      */
@@ -96,13 +130,7 @@ apr_status_t apr_create_tcp_socket(apr_socket_t **new, apr_pool_t *cont)
     if ((*new)->sock == INVALID_SOCKET) {
         return apr_get_netos_error();
     }
-
-    (*new)->local_addr->sin_family = AF_INET;
-    (*new)->remote_addr->sin_family = AF_INET;
-
-    (*new)->addr_len = sizeof(*(*new)->local_addr);
-
-    (*new)->local_addr->sin_port = 0;   
+    set_socket_vars(*new, AF_INET);
 
     (*new)->timeout = -1;
     (*new)->disconnected = 0;
@@ -147,11 +175,13 @@ apr_status_t apr_close_socket(apr_socket_t *thesocket)
 
 apr_status_t apr_bind(apr_socket_t *sock)
 {
-    if (bind(sock->sock, (struct sockaddr *)sock->local_addr, sock->addr_len) == -1) {
+    if (bind(sock->sock, 
+             (struct sockaddr *)&sock->local_addr->sa, 
+             sock->local_addr->sa_len) == -1) {
         return apr_get_netos_error();
     }
     else {
-        if (sock->local_addr->sin_port == 0) {
+        if (sock->local_addr->sa.sin.sin_port == 0) {
             sock->local_port_unknown = 1; /* ephemeral port */
         }
         return APR_SUCCESS;
@@ -168,27 +198,20 @@ apr_status_t apr_listen(apr_socket_t *sock, apr_int32_t backlog)
 
 apr_status_t apr_accept(apr_socket_t **new, apr_socket_t *sock, apr_pool_t *connection_context)
 {
-    (*new) = (apr_socket_t *)apr_pcalloc(connection_context, 
-                            sizeof(apr_socket_t));
+    alloc_socket(new, connection_context);
+    set_socket_vars(*new, sock->local_addr->sa.sin.sin_family);
 
-    (*new)->cntxt = connection_context;
-    (*new)->local_addr = (struct sockaddr_in *)apr_pcalloc((*new)->cntxt, 
-                 sizeof(struct sockaddr_in));
-    (*new)->remote_addr = (struct sockaddr_in *)apr_pcalloc((*new)->cntxt,
-                 sizeof(struct sockaddr_in));
-    memcpy((*new)->local_addr, sock->local_addr, sizeof(struct sockaddr_in));
-
-    (*new)->addr_len = sizeof(struct sockaddr_in);
     (*new)->timeout = -1;   
     (*new)->disconnected = 0;
 
-    (*new)->sock = accept(sock->sock, (struct sockaddr *)(*new)->local_addr,
-                        &(*new)->addr_len);
+    (*new)->remote_addr->sa_len = sizeof((*new)->remote_addr->sa);
+    (*new)->sock = accept(sock->sock, 
+                          (struct sockaddr *)&(*new)->remote_addr->sa,
+                          &(*new)->remote_addr->sa_len);
 
     if ((*new)->sock == INVALID_SOCKET) {
         return apr_get_netos_error();
     }
-
     *(*new)->local_addr = *sock->local_addr;
 
     if (sock->local_port_unknown) {
@@ -197,7 +220,7 @@ apr_status_t apr_accept(apr_socket_t **new, apr_socket_t *sock, apr_pool_t *conn
     }
 
     if (sock->local_interface_unknown ||
-        sock->local_addr->sin_addr.s_addr == 0) {
+        sock->local_addr->sa.sin.sin_addr.s_addr == 0) {
         /* If the interface address inside the listening socket's local_addr wasn't
          * up-to-date, we don't know local interface of the connected socket either.
          *
@@ -225,22 +248,20 @@ apr_status_t apr_connect(apr_socket_t *sock, const char *hostname)
     if (hostname != NULL) {
         if (*hostname >= '0' && *hostname <= '9' && 
             strspn(hostname, "0123456789.") == strlen(hostname)) {
-            sock->remote_addr->sin_addr.s_addr = inet_addr(hostname);
+            sock->remote_addr->sa.sin.sin_addr.s_addr = inet_addr(hostname);
         }
         else {
             hp = gethostbyname(hostname);
             if (!hp)  {
                 return apr_get_netos_error();
             }
-            memcpy((char *)&sock->remote_addr->sin_addr, hp->h_addr_list[0], hp->h_length);
-            sock->addr_len = sizeof(*sock->remote_addr);
+            memcpy((char *)&sock->remote_addr->sa.sin.sin_addr, hp->h_addr_list[0], 
+                   hp->h_length);
         }
     }
     
-    sock->remote_addr->sin_family = AF_INET;
-
-    if (connect(sock->sock, (const struct sockaddr *)sock->remote_addr, 
-                sock->addr_len) == SOCKET_ERROR) {
+    if (connect(sock->sock, (const struct sockaddr *)&sock->remote_addr->sa.sin,
+                sock->remote_addr->sa_len) == SOCKET_ERROR) {
         lasterror = apr_get_netos_error();
         if (lasterror != APR_FROM_OS_ERROR(WSAEWOULDBLOCK)) {
             return lasterror;
@@ -253,10 +274,10 @@ apr_status_t apr_connect(apr_socket_t *sock, const char *hostname)
         }
     }
     /* connect was OK .. amazing */
-    if (sock->local_addr->sin_port == 0) {
+    if (sock->local_addr->sa.sin.sin_port == 0) {
         sock->local_port_unknown = 1;
     }
-    if (sock->local_addr->sin_addr.s_addr == 0) {
+    if (sock->local_addr->sa.sin.sin_addr.s_addr == 0) {
         /* must be using free-range port */
         sock->local_interface_unknown = 1;
     }
@@ -283,22 +304,9 @@ apr_status_t apr_get_os_sock(apr_os_sock_t *thesock, apr_socket_t *sock)
 apr_status_t apr_put_os_sock(apr_socket_t **sock, apr_os_sock_t *thesock, 
                            apr_pool_t *cont)
 {
-    if (cont == NULL) {
-        return APR_ENOPOOL;
-    }
     if ((*sock) == NULL) {
-        (*sock) = (apr_socket_t *)apr_pcalloc(cont, sizeof(apr_socket_t));
-        (*sock)->cntxt = cont;
-        (*sock)->local_addr = (struct sockaddr_in *)apr_pcalloc((*sock)->cntxt,
-                             sizeof(struct sockaddr_in));
-        (*sock)->remote_addr = (struct sockaddr_in *)apr_pcalloc((*sock)->cntxt,
-                              sizeof(struct sockaddr_in));
-
-        if ((*sock)->local_addr == NULL || (*sock)->remote_addr == NULL) {
-            return APR_ENOMEM;
-        }
-     
-        (*sock)->addr_len = sizeof(*(*sock)->local_addr);
+        alloc_socket(sock, cont);
+        set_socket_vars(*sock, AF_INET);
         (*sock)->timeout = -1;
         (*sock)->disconnected = 0;
     }
