5e8d66affca53e5dc480d0b5a49998c914ef1ab1
Greg Stein <gstein@apache.org>
2000-11-29 07:37:41 +0000

    *) Add functions for file-level locking/unlocking. *) Add test program for the new functions.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60811 13f79535-47bb-0310-9956-ffa450edef68

---
+++ file_io/unix/flock.c
@@ -0,0 +1,150 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "fileio.h"
+
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#endif
+#ifdef HAVE_SYS_FILE_H
+#include <sys/file.h>
+#endif
+
+apr_status_t apr_lock_file(apr_file_t *thefile, int type)
+{
+    int rc;
+
+#if defined(HAVE_FCNTL_H)
+    {
+        struct flock l = { 0 };
+        int fc;
+
+        l.l_whence = SEEK_SET;  /* lock from current point */
+        l.l_start = 0;          /* begin lock at this offset */
+        l.l_len = 0;            /* lock to end of file */
+        if ((type & APR_FLOCK_TYPEMASK) == APR_FLOCK_SHARED)
+            l.l_type = F_RDLCK;
+        else
+            l.l_type = F_WRLCK;
+
+        fc = (type & APR_FLOCK_NONBLOCK) ? F_SETLK : F_SETLKW;
+
+        /* keep trying if fcntl() gets interrupted (by a signal) */
+        while ((rc = fcntl(thefile->filedes, fc, &l)) < 0 && errno == EINTR)
+            continue;
+
+        if (rc == -1)
+            return errno;
+    }
+#elif defined(HAVE_SYS_FILE_H)
+    {
+        int ltype;
+
+        if ((type & APR_FLOCK_TYPEMASK) == APR_FLOCK_SHARED)
+            ltype = LOCK_SH;
+        else
+            ltype = LOCK_EX;
+        if ((type & APR_FLOCK_NONBLOCK) != 0)
+            ltype |= LOCK_NB;
+
+        /* keep trying if flock() gets interrupted (by a signal) */
+        while ((rc = flock(thefile->filedes, ltype)) < 0 && errno == EINTR)
+            continue;
+
+        if (rc == -1)
+            return errno;
+    }
+#else
+#error No file locking mechanism is available.
+#endif
+
+    return APR_SUCCESS;
+}
+
+apr_status_t apr_unlock_file(apr_file_t *thefile)
+{
+    int rc;
+
+#if defined(HAVE_FCNTL_H)
+    {
+        struct flock l = { 0 };
+
+        l.l_whence = SEEK_SET;  /* lock from current point */
+        l.l_start = 0;          /* begin lock at this offset */
+        l.l_len = 0;            /* lock to end of file */
+        l.l_type = F_UNLCK;
+
+        /* keep trying if fcntl() gets interrupted (by a signal) */
+        while ((rc = fcntl(thefile->filedes, F_SETLKW, &l)) < 0
+               && errno == EINTR)
+            continue;
+
+        if (rc == -1)
+            return errno;
+    }
+#elif defined(HAVE_SYS_FILE_H)
+    {
+        /* keep trying if flock() gets interrupted (by a signal) */
+        while ((rc = flock(thefile->filedes, LOCK_UN)) < 0 && errno == EINTR)
+            continue;
+
+        if (rc == -1)
+            return errno;
+    }
+#else
+#error No file locking mechanism is available.
+#endif
+
+    return APR_SUCCESS;
+}
---
+++ test/testflock.c
@@ -0,0 +1,173 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+/*
+ * USAGE
+ *
+ * Start one process, no args, and place it into the background. Start a
+ * second process with the "-r" switch to attempt a read on the file
+ * created by the first process.
+ *
+ * $ ./testflock &
+ * ...messages...
+ * $ ./testflock -r
+ * ...messages...
+ *
+ * The first process will sleep for 30 seconds while holding a lock. The
+ * second process will attempt to grab it (non-blocking) and fail. It
+ * will then grab it with a blocking scheme. When the first process' 30
+ * seconds are up, it will exit (thus releasing its lock). The second
+ * process will acquire the lock, then exit.
+ */
+
+#include "apr_pools.h"
+#include "apr_file_io.h"
+#include "apr_time.h"
+#include "apr_general.h"
+#include "apr_getopt.h"
+#include "apr_strings.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TESTFILE "testfile.tmp"
+
+static apr_pool_t *pool = NULL;
+
+
+static void errmsg(const char *msg)
+{
+    if (pool != NULL)
+        apr_destroy_pool(pool);
+    fprintf(stderr, msg);
+    exit(1);
+}
+
+static void do_read(void)
+{
+    apr_file_t *file;
+    apr_status_t status;
+
+    if (apr_open(&file, TESTFILE, APR_WRITE,
+                 APR_OS_DEFAULT, pool) != APR_SUCCESS)
+        errmsg("Could not open test file.\n");
+    printf("Test file opened.\n");
+
+    status = apr_lock_file(file, APR_FLOCK_EXCLUSIVE | APR_FLOCK_NONBLOCK);
+    if (!APR_STATUS_IS_EAGAIN(status)) {
+        char msg[200];
+        errmsg(apr_psprintf(pool, "Expected EAGAIN. Got %d: %s.\n",
+                            status, apr_strerror(status, msg, sizeof(msg))));
+    }
+    printf("First attempt: we were properly locked out.\nWaiting for lock...");
+    fflush(stdout);
+
+    if (apr_lock_file(file, APR_FLOCK_EXCLUSIVE) != APR_SUCCESS)
+        errmsg("Could not establish lock on test file.");
+    printf(" got it.\n");
+
+    (void) apr_close(file);
+    printf("Exiting.\n");
+}
+
+static void do_write(void)
+{
+    apr_file_t *file;
+
+    if (apr_open(&file, TESTFILE, APR_WRITE|APR_CREATE, APR_OS_DEFAULT,
+                 pool) != APR_SUCCESS)
+        errmsg("Could not create file.\n");
+    printf("Test file created.\n");
+
+    if (apr_lock_file(file, APR_FLOCK_EXCLUSIVE) != APR_SUCCESS)
+        errmsg("Could not lock the file.\n");
+    printf("Lock created.\nSleeping...");
+    fflush(stdout);
+
+    apr_sleep(30000000);        /* 30 seconds */
+
+    (void) apr_close(file);
+    printf(" done.\nExiting.\n");
+}
+
+int main(int argc, const char * const *argv)
+{
+    int reader = 0;
+    apr_status_t status;
+    char optchar;
+    const char *optarg;
+    apr_getopt_t *opt;
+
+    if (apr_initialize() != APR_SUCCESS)
+        errmsg("Could not initialize APR.\n");
+    atexit(apr_terminate);
+
+    if (apr_create_pool(&pool, NULL) != APR_SUCCESS)
+        errmsg("Could not create global pool.\n");
+
+    if (apr_initopt(&opt, pool, argc, argv) != APR_SUCCESS)
+        errmsg("Could not parse options.\n");
+
+    while ((status = apr_getopt(opt, "r", &optchar, &optarg)) == APR_SUCCESS) {
+        if (optchar == 'r')
+            ++reader;
+    }
+
+    if (reader)
+        do_read();
+    else
+        do_write();
+
+    return 0;
+}
