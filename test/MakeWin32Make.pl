3b5bb9a02af47ef9147c5490e44b84c9e40d67af
William A. Rowe Jr <wrowe@apache.org>
2000-12-04 20:39:58 +0000

    Test Makefile.in -> Makefile converter for Win32 ... I'll wrap with a   .dsp soonish.  I have no issues with using a .pl for apr/test builders,   since they are the exception, rather than the typical user.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@60884 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/MakeWin32Make.pl
@@ -0,0 +1,41 @@
+use IO::File;
+
+$srcfl = new IO::File "Makefile.in", "r" || die "failed to open .in file";
+$dstfl = new IO::File "Makefile", "w" || die "failed to create Makefile";
+
+print $dstfl "LINK=link.exe\n";
+
+while ($t = <$srcfl>) {
+
+    if ($t =~ s|\@CFLAGS\@|\/nologo \/MDd \/W3 \/Gm \/GX \/Zi \/Od \/D "_DEBUG" \/D "WIN32" \/D APR_DECLARE_STATIC \/FD|) {
+        $t =~ s|-g ||;
+    }
+    $t =~ s|\@LDFLAGS\@|\/nologo \/debug \/machine:I386|;
+
+    $t =~ s|\@RM\@|del|;
+    $t =~ s|(\$\(RM\)) -f|$1|;
+    $t =~ s|\@CC\@|cl|;
+    $t =~ s|\@RANLIB\@||;
+    $t =~ s|\@OPTIM\@||;
+    $t =~ s|\@LIBS\@|kernel32\.lib user32\.lib advapi32\.lib ws2_32\.lib wsock32\.lib|;
+    $t =~ s|-I\$\(INCDIR\)|\/I "\$\(INCDIR\)"|;
+    $t =~ s|\.\.\/libapr\.a|\.\./LibD/apr\.lib|;
+    if ($t =~ s|\@EXEEXT\@|\.exe|) {
+        $t =~ s|\$\(CC\) \$\(CFLAGS\)|\$\(LINK\) \/subsystem:console|;
+        $t =~ s|-o (\S+)|\/out:\"$1\"|;
+        $t =~ s|--export-dynamic ||; 
+        $t =~ s|-fPIC ||;
+    }
+    if ($t =~ s|\$\(CC\) -shared|\$\(LINK\) \/subsystem:windows \/dll|) {
+        $t =~ s|-o (\S+)|\/out:\"$1\"|;
+    }
+    while ($t =~ s|\.a\b|\.lib|) {}
+    while ($t =~ s|\.o\b|\.obj|) {}
+    while ($t =~ s|\.so\b|\.dll|) {}
+
+    print $dstfl $t;
+
+}
+
+undef $srcfl;
+undef $dstfl;
