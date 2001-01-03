d6f5228a7761b8c6481c1c8f49af083672fce3b7
Brian Havard <bjh@apache.org>
2001-01-03 01:48:48 +0000

    OS/2: Copy pool pointer into a socket's apr_sockaddr_t's
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61012 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/os2/sockets.c
+++ network_io/os2/sockets.c
@@ -112,15 +112,18 @@ static void set_socket_vars(apr_socket_t *sock, int family)
         sock->remote_addr->ipaddr_len = sizeof(struct in6_addr);
     }
 #endif
-}                                                                                                  
+}
+
 static void alloc_socket(apr_socket_t **new, apr_pool_t *p)
 {
     *new = (apr_socket_t *)apr_pcalloc(p, sizeof(apr_socket_t));
     (*new)->cntxt = p;
     (*new)->local_addr = (apr_sockaddr_t *)apr_pcalloc((*new)->cntxt,
                                                        sizeof(apr_sockaddr_t));
+    (*new)->local_addr->pool = p;
     (*new)->remote_addr = (apr_sockaddr_t *)apr_pcalloc((*new)->cntxt,
                                                         sizeof(apr_sockaddr_t));
+    (*new)->remote_addr->pool = p;
 }
 
 apr_status_t apr_create_socket(apr_socket_t **new, int ofamily, int type,
