3f198d6b87b1729aef8eeea19d21b5aa96b05acd
Greg Stein <gstein@apache.org>
2001-01-11 00:13:23 +0000

    add an accessor to the number of key/value pairs in the hash table.
    Submitted by: Bill Tutt <billtut@microsoft.com>
    Reviewed by: Greg Stein
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61045 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_hash.c
+++ tables/apr_hash.c
@@ -314,3 +314,8 @@ APR_DECLARE(void) apr_hash_set(apr_hash_t *ht,
     }
     /* else key not present and val==NULL */
 }
+
+APR_DECLARE(void) apr_hash_count(apr_hash_t *ht, apr_size_t *count)
+{
+    *count = ht->count;
+}
