ffa01c26a7fd1e565cb9027b8fe45d18c37ab1df
Ryan Bloom <rbb@apache.org>
2001-01-19 03:13:02 +0000

    All platforms now register a cleanup when a DSO is loaded.  This just makes a practice uniform across all platforms.  In the past, this was done differently on different platforms.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61076 13f79535-47bb-0310-9956-ffa450edef68

--- dso/win32/dso.c
+++ dso/win32/dso.c
@@ -59,6 +59,12 @@
 
 #if APR_HAS_DSO
 
+static apr_status_t dso_cleanup(void *thedso)
+{
+    apr_dso_handle_t *dso = thedso;
+    return apr_dso_unload(dso);
+}
+
 APR_DECLARE(apr_status_t) apr_dso_load(struct apr_dso_handle_t **res_handle, 
                                        const char *path, apr_pool_t *ctx)
 {
@@ -111,6 +117,9 @@ APR_DECLARE(apr_status_t) apr_dso_load(struct apr_dso_handle_t **res_handle,
     (*res_handle)->handle = (void*)os_handle;
     (*res_handle)->cont = ctx;
     (*res_handle)->load_error = APR_SUCCESS;
+
+    apr_register_cleanup(ctx, *res_handle, dso_cleanup, apr_null_cleanup);
+
     return APR_SUCCESS;
 }
     
