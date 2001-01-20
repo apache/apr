79ce568c221f055f302cbb769c29e6f4f4530a6c
William A. Rowe Jr <wrowe@apache.org>
2001-01-20 21:39:05 +0000

    The platform changes in apr to support the APR_FINFO_wanted declaration   changes for apr_stat/lstat/getfileinfo.  Much, much work to do here,   but this gets us started.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61086 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/pipe.c
+++ file_io/win32/pipe.c
@@ -91,7 +91,7 @@ APR_DECLARE(apr_status_t) apr_create_pipe(apr_file_t **in, apr_file_t **out, apr
 
     (*in) = (apr_file_t *)apr_pcalloc(p, sizeof(apr_file_t));
     (*in)->cntxt = p;
-    (*in)->n.fname = "\0\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
+    (*in)->fname = "\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
     (*in)->pipe = 1;
     (*in)->timeout = -1;
     (*in)->ungetchar = -1;
@@ -103,7 +103,7 @@ APR_DECLARE(apr_status_t) apr_create_pipe(apr_file_t **in, apr_file_t **out, apr
 
     (*out) = (apr_file_t *)apr_pcalloc(p, sizeof(apr_file_t));
     (*out)->cntxt = p;
-    (*in)->n.fname = "\0\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
+    (*in)->fname = "\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
     (*out)->pipe = 1;
     (*out)->timeout = -1;
     (*out)->ungetchar = -1;
@@ -161,7 +161,7 @@ apr_status_t apr_create_nt_pipe(apr_file_t **in, apr_file_t **out,
 
     (*in) = (apr_file_t *)apr_pcalloc(p, sizeof(apr_file_t));
     (*in)->cntxt = p;
-    (*in)->n.fname = "\0\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
+    (*in)->fname = "\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
     (*in)->pipe = 1;
     (*in)->timeout = -1;
     (*in)->ungetchar = -1;
@@ -174,7 +174,7 @@ apr_status_t apr_create_nt_pipe(apr_file_t **in, apr_file_t **out,
 
     (*out) = (apr_file_t *)apr_pcalloc(p, sizeof(apr_file_t));
     (*out)->cntxt = p;
-    (*in)->n.fname = "\0\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
+    (*in)->fname = "\0"; // What was this??? : apr_pstrdup(p, "PIPE"); */
     (*out)->pipe = 1;
     (*out)->timeout = -1;
     (*out)->ungetchar = -1;
