f1f480d6489bf4cd032e454b33ca9fea39448dce
Jeff Trawick <trawick@apache.org>
2001-01-24 21:11:52 +0000

    get filestat.c to compile again
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61121 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/filestat.c
+++ file_io/unix/filestat.c
@@ -86,20 +86,20 @@ static apr_status_t fill_out_finfo(apr_finfo_t *finfo, struct stat *info,
 { 
     finfo->valid = APR_FINFO_MIN | APR_FINFO_IDENT | APR_FINFO_NLINK
                  | APR_FINFO_OWNER | APR_FINFO_PROT;
-    finfo->protection = apr_unix_mode2perms(info.st_mode);
-    finfo->filetype = filetype_from_mode(info.st_mode);
-    finfo->user = info.st_uid;
-    finfo->group = info.st_gid;
-    finfo->size = info.st_size;
-    finfo->inode = info.st_ino;
-    finfo->device = info.st_dev;
-    finfo->nlink = info.st_nlink;
-    apr_ansi_time_to_apr_time(&finfo->atime, info.st_atime);
-    apr_ansi_time_to_apr_time(&finfo->mtime, info.st_mtime);
-    apr_ansi_time_to_apr_time(&finfo->ctime, info.st_ctime);
+    finfo->protection = apr_unix_mode2perms(info->st_mode);
+    finfo->filetype = filetype_from_mode(info->st_mode);
+    finfo->user = info->st_uid;
+    finfo->group = info->st_gid;
+    finfo->size = info->st_size;
+    finfo->inode = info->st_ino;
+    finfo->device = info->st_dev;
+    finfo->nlink = info->st_nlink;
+    apr_ansi_time_to_apr_time(&finfo->atime, info->st_atime);
+    apr_ansi_time_to_apr_time(&finfo->mtime, info->st_mtime);
+    apr_ansi_time_to_apr_time(&finfo->ctime, info->st_ctime);
     /* ### needs to be revisited  
      * if (wanted & APR_FINFO_CSIZE) {
-     *   finfo->csize = info.st_blocks * 512;
+     *   finfo->csize = info->st_blocks * 512;
      *   finfo->valid |= APR_FINFO_CSIZE;
      * }
      */
