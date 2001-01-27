03590aa9a303c70aa78431b0ae8d7a462a0c1f39
William A. Rowe Jr <wrowe@apache.org>
2001-01-27 06:39:14 +0000

    And why these values, anyways... abstract them out
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61132 13f79535-47bb-0310-9956-ffa450edef68

--- dso/win32/dso.c
+++ dso/win32/dso.c
@@ -81,7 +81,7 @@ APR_DECLARE(apr_status_t) apr_dso_load(struct apr_dso_handle_t **res_handle,
     apr_oslevel_e os_level;
     if (!apr_get_oslevel(ctx, &os_level) && os_level >= APR_WIN_NT) 
     {
-        apr_wchar_t wpath[8192];
+        apr_wchar_t wpath[APR_PATH_MAX];
         apr_status_t rv;
         if (rv = utf8_to_unicode_path(wpath, sizeof(wpath) 
                                               / sizeof(apr_wchar_t), path)) {
@@ -97,14 +97,14 @@ APR_DECLARE(apr_status_t) apr_dso_load(struct apr_dso_handle_t **res_handle,
     else
 #endif
     {
-        char fspec[MAX_PATH], *p = fspec;
+        char fspec[APR_PATH_MAX], *p = fspec;
         /* Must convert path from / to \ notation.
          * Per PR2555, the LoadLibraryEx function is very picky about slashes.
          * Debugging on NT 4 SP 6a reveals First Chance Exception within NTDLL.
          * LoadLibrary in the MS PSDK also reveals that it -explicitly- states
          * that backslashes must be used for the LoadLibrary family of calls.
          */
-        apr_cpystrn(fspec, path, MAX_PATH);
+        apr_cpystrn(fspec, path, sizeof(fspec));
         while (p = strchr(p, '/'))
             *p = '\\';
         
