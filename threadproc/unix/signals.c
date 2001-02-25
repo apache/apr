c71879d3fc8578e18cfc3066d19a7408c8d0797f
Ryan Bloom <rbb@apache.org>
2001-02-25 06:39:34 +0000

    Don't define the signal thread logic is sigwait isn't defined.  Hopefully any platform like this will be able to mimic the signal thread, but for now this fixes any machine without sigwait.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61296 13f79535-47bb-0310-9956-ffa450edef68

--- threadproc/unix/signals.c
+++ threadproc/unix/signals.c
@@ -267,7 +267,7 @@ const char *apr_signal_get_description(int signum)
 
 #endif /* SYS_SIGLIST_DECLARED */
 
-#if APR_HAS_THREADS && !defined(OS2)
+#if APR_HAS_THREADS && !defined(OS2) && defined(HAVE_SIGWAIT)
 static void *signal_thread_func(void *signal_handler)
 {
     sigset_t sig_mask;
