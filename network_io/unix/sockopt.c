4b869ba1a41db0a92b7d72ca47bc6336d23d005e
David Reid <dreid@apache.org>
2001-04-03 00:43:09 +0000

    The actual code for accept_filters on FreeBSD for APR.  This is NOT yet tested but does build :)  Use at your own risk.
    Obtained from:   Ryan Bloom <rbb@covalent.com>
    Reviewed by:	 David Reid <dreid@apache.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61438 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockopt.c
+++ network_io/unix/sockopt.c
@@ -303,3 +303,19 @@ apr_status_t apr_gethostname(char *buf, apr_int32_t len, apr_pool_t *cont)
         return APR_SUCCESS;
 }
 
+#ifdef SO_ACCEPTFILTER
+apr_status_t apr_socket_accept_filter(apr_socket_t *sock, char *name, 
+                                      char *args)
+{
+    struct accept_filter_arg af;
+    strncpy(af.af_name, name, 16);
+    strncpy(af.af_arg, args, 256 - 16);
+
+    if ((setsockopt(sock->socketdes, SOL_SOCKET, SO_ACCEPTFILTER,
+          &af, sizeof(af))) < 0) {
+        return errno;
+    }
+    return APR_SUCCESS;
+}
+#endif
+
