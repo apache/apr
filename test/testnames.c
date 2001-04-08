8765bb3e2b587ae17a1c72d8050ec024bc71671b
William A. Rowe Jr <wrowe@apache.org>
2001-04-08 08:08:19 +0000

    Add my testbench that demonstrates apr_filepath_merge.  This would be   better written to handle and compare a list of demonstration cases to   the expected results, but I've no time for it this weekend
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61483 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/testnames.c
@@ -0,0 +1,130 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "apr_file_io.h"
+#include "apr_file_info.h"
+#include "apr_errno.h"
+#include "apr_general.h"
+#include "apr_pools.h"
+#include "apr_lib.h"
+
+apr_pool_t *context;
+
+static void closeapr(void)
+{
+    apr_pool_destroy(context);
+    apr_terminate();
+}
+
+static void mergeresult(char *rootpath, char *addpath, apr_int32_t mergetype, char *tdesc)
+{
+    char errmsg[256];
+    char *dstpath = NULL;
+    char *srcpath;
+    apr_status_t status = apr_filepath_merge(&dstpath, 
+                                strcmp(rootpath, "NULL") ? rootpath : NULL,
+                                strcmp(addpath, "NULL") ? addpath : NULL,
+                                mergetype, context);
+    apr_strerror(status, errmsg, sizeof(errmsg));
+    if (dstpath) {
+        fprintf(stderr, "%s result for %s\n\tResult Path \"%s\"\n", errmsg, tdesc, dstpath);
+        srcpath = dstpath;
+        status = apr_filepath_root(&dstpath, &srcpath, context);
+        if (srcpath != dstpath) {
+            apr_strerror(status, errmsg, sizeof(errmsg));
+            fprintf(stderr, "\tRoot of \"%s\" (%s)\n", dstpath, errmsg);
+        }
+    }
+    else {
+        fprintf(stderr, "%s result for %s\n", errmsg, tdesc, dstpath);
+    }
+}
+
+#define merge_result(r, a, t) mergeresult(r, a, t, #t)
+
+int main(void)
+{
+    char rootpath[256];
+    char addpath[256];
+
+    if (apr_initialize() != APR_SUCCESS) {
+        fprintf(stderr, "Couldn't initialize.");
+        exit(-1);
+    }
+    atexit(closeapr);
+    if (apr_pool_create(&context, NULL) != APR_SUCCESS) {
+        fprintf(stderr, "Couldn't allocate context.");
+        exit(-1);
+    }
+
+    fprintf(stdout, "Testing file truepath.\n");
+
+    while (1) {
+        fprintf(stdout, "\nEnter a root path$ ");
+        if (!gets(rootpath))
+            exit(0);
+        fprintf(stdout, "Enter an add path$ ");
+        if (!gets(addpath))
+            exit(0);
+        merge_result(rootpath, addpath, 0);
+        merge_result(rootpath, addpath, APR_FILEPATH_NOTABOVEROOT);
+        merge_result(rootpath, addpath, APR_FILEPATH_SECUREROOT);
+        merge_result(rootpath, addpath, APR_FILEPATH_NOTABSOLUTE);
+        merge_result(rootpath, addpath, APR_FILEPATH_NOTRELATIVE);
+    }
+    return (0);
+}
