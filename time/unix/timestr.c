ee50336496ac2c5d3dc6dd737f52ececae98c70e
David Reid <dreid@apache.org>
2001-04-12 22:44:42 +0000

    Daniel Padwa <daniel.padwa@gs.com> pointed out the last patch broke the build on Solaris, so this patch is an attempt to get it building again.
    The eventual solution will be more general access functions for
    getting/setting the timezone from the different structures. I've made
    a partial start at it.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61519 13f79535-47bb-0310-9956-ffa450edef68

--- time/unix/timestr.c
+++ time/unix/timestr.c
@@ -181,7 +181,11 @@ apr_status_t apr_strftime(char *s, apr_size_t *retsize, apr_size_t max,
     tm.tm_wday = xt->tm_wday;
     tm.tm_yday = xt->tm_yday;
     tm.tm_isdst = xt->tm_isdst;
+#if defined(HAVE_GMTOFF)
     tm.tm_gmtoff = xt->tm_gmtoff;
+#elif defined(HAVE___OFFSET)
+    tm.__tm_gmtoff = xt->tm_gmtoff;
+#endif
     (*retsize) = strftime(s, max, format, &tm);
     return APR_SUCCESS;
 }
