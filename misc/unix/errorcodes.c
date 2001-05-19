d3586f4926cc1802bc079a8acdacb034b1b8c68e
David Reid <dreid@apache.org>
2001-05-19 13:53:06 +0000

    More cleanup and improvement of the memory code...
    - general code clean up (from Sander Striker)
    - add a type flag to the cleanup's we register (from Sander Striker)
    - add an extra error code to signify you asked for a function
      that isn't available in a memory system
    - add the new memory codes into errorcodes.c so we can report
      the errors (missed first time around)
    
    The types for the cleanups should allow us to be much more selective
    in the cleanup we call.  Sander has some work in progress that will use
    these to provide a better feel of how they'll work.
    
    Submitted by:	Sander Striker <striker@samba-tng.org>
    Reviewed by:	David Reid
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61660 13f79535-47bb-0310-9956-ffa450edef68

--- misc/unix/errorcodes.c
+++ misc/unix/errorcodes.c
@@ -166,7 +166,14 @@ static char *apr_error_string(apr_status_t statcode)
         return "The given path was above the root path";
     case APR_EBADPATH:
         return "The given path misformatted or contained invalid characters";
-     default:
+    case APR_EMEMSYS:
+        return "The memory system passed does not exist";
+    case APR_EMEMFUNC:
+        return "The function requested is not available in the memory "
+               "system used";
+    case APR_ENOCLEANUP:
+        return "The requested cleanup function does not exist";
+    default:
         return "Error string not specified yet";
     }
 }
