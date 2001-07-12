84fc8ae8d977cf13a129d68c6f99c0543c132a6c
Bill Stoddard <stoddard@apache.org>
2001-07-12 03:41:05 +0000

    Handle the return from select correctly.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61920 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/win32/sockets.c
+++ network_io/win32/sockets.c
@@ -282,8 +282,9 @@ APR_DECLARE(apr_status_t) apr_connect(apr_socket_t *sock, apr_sockaddr_t *sa)
 
     if (connect(sock->sock, (const struct sockaddr *)&sa->sa.sin,
                 sa->salen) == SOCKET_ERROR) {
+        int rc;
         struct timeval tv, *tvptr;
-        fd_set fdset;
+        fd_set wfdset, efdset;
 
         rv = apr_get_netos_error();
         if (rv != APR_FROM_OS_ERROR(WSAEWOULDBLOCK)) {
@@ -300,8 +301,11 @@ APR_DECLARE(apr_status_t) apr_connect(apr_socket_t *sock, apr_sockaddr_t *sa)
         }
 
         /* wait for the connect to complete or timeout */
-        FD_ZERO(&fdset);
-        FD_SET(sock->sock, &fdset);
+        FD_ZERO(&wfdset);
+        FD_SET(sock->sock, &wfdset);
+        FD_ZERO(&efdset);
+        FD_SET(sock->sock, &efdset);
+
         if (sock->timeout < 0) {
             tvptr = NULL;
         }
@@ -310,8 +314,21 @@ APR_DECLARE(apr_status_t) apr_connect(apr_socket_t *sock, apr_sockaddr_t *sa)
             tv.tv_usec = sock->timeout % APR_USEC_PER_SEC;
             tvptr = &tv;
         }
-        if (select(FD_SETSIZE+1, NULL, &fdset, NULL, tvptr) == SOCKET_ERROR) {
+        rc = select(FD_SETSIZE+1, NULL, &wfdset, &efdset, tvptr);
+        if (rc == SOCKET_ERROR) {
             return apr_get_netos_error();
+        }
+        else if (!rc) {
+            return APR_FROM_OS_ERROR(WSAETIMEDOUT);
+        }
+        /* Evaluate the efdset */
+        if (FD_ISSET(sock->sock, &efdset)) {
+            /* The connect failed. */
+            unsigned int rclen = sizeof(rc);
+            if (getsockopt(sock->sock, SOL_SOCKET, SO_ERROR, (char*) &rc, &rclen)) {
+                return apr_get_netos_error();
+            }
+            return APR_FROM_OS_ERROR(rc);
         }
     }
     /* connect was OK .. amazing */
