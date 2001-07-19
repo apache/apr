cd7502aa01d414b2200b3951afb5158543c8fe30
David Reid <dreid@apache.org>
2001-07-19 11:58:33 +0000

    Some formatting changes, no code change.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@61969 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_tables.c
+++ tables/apr_tables.c
@@ -96,7 +96,7 @@ static void make_array_core(apr_array_header_t *res, apr_pool_t *p,
      * array of zero elts.
      */
     if (nelts < 1) {
-	nelts = 1;
+        nelts = 1;
     }
 
     res->elts = apr_pcalloc(p, nelts * elt_size);
@@ -120,14 +120,14 @@ APR_DECLARE(apr_array_header_t *) apr_array_make(apr_pool_t *p,
 APR_DECLARE(void *) apr_array_push(apr_array_header_t *arr)
 {
     if (arr->nelts == arr->nalloc) {
-	int new_size = (arr->nalloc <= 0) ? 1 : arr->nalloc * 2;
-	char *new_data;
+        int new_size = (arr->nalloc <= 0) ? 1 : arr->nalloc * 2;
+        char *new_data;
 
-	new_data = apr_pcalloc(arr->pool, arr->elt_size * new_size);
+        new_data = apr_pcalloc(arr->pool, arr->elt_size * new_size);
 
-	memcpy(new_data, arr->elts, arr->nalloc * arr->elt_size);
-	arr->elts = new_data;
-	arr->nalloc = new_size;
+        memcpy(new_data, arr->elts, arr->nalloc * arr->elt_size);
+        arr->elts = new_data;
+        arr->nalloc = new_size;
     }
 
     ++arr->nelts;
