708395afacaf72195a40ce1bbf4e3c31abb89747
Bradley Nicholes <bnicholes@apache.org>
2001-08-02 20:29:14 +0000

    NetWare port of the threadproc functions
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62088 13f79535-47bb-0310-9956-ffa450edef68

---
+++ threadproc/netware/thread.c
@@ -0,0 +1,245 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "apr.h"
+#include "apr_portable.h"
+#include "threadproc.h"
+
+
+apr_status_t apr_threadattr_create(apr_threadattr_t **new,
+                                                apr_pool_t *cont)
+{
+    (*new) = (apr_threadattr_t *)apr_palloc(cont, 
+              sizeof(apr_threadattr_t));
+
+    if ((*new) == NULL) {
+        return APR_ENOMEM;
+    }
+
+    (*new)->cntxt = cont;
+    return APR_SUCCESS;
+}
+
+apr_status_t apr_threadattr_detach_set(apr_threadattr_t *attr,apr_int32_t on)
+{
+    attr->detach = on;
+	return APR_SUCCESS;   
+}
+
+apr_status_t apr_threadattr_detach_get(apr_threadattr_t *attr)
+{
+    if (attr->detach == 1)
+        return APR_DETACH;
+    return APR_NOTDETACH;
+}
+
+apr_status_t apr_thread_create(apr_thread_t **new,
+ 											apr_threadattr_t *attr, 
+                             				apr_thread_start_t func,
+ 											void *data,
+ 											apr_pool_t *cont)
+{
+    apr_status_t stat;
+    size_t stacksize;
+    long flags = NX_CTX_AUTO_CLEANUP;
+  	char threadName[NX_MAX_OBJECT_NAME_LEN+1];
+   	//srj added for nks giving the threads names
+	
+	sprintf(threadName, "Apache-2.0 %003ld",data);
+    
+    (*new) = (apr_thread_t *)apr_palloc(cont, sizeof(apr_thread_t));
+
+    if ((*new) == NULL) {
+        return APR_ENOMEM;
+    }
+    
+    (*new)->cntxt = cont;
+    
+    stat = apr_pool_create(&(*new)->cntxt, cont);
+    if (stat != APR_SUCCESS) {
+        return stat;
+    }
+    
+    if (attr) {
+        stacksize = attr->stack_size; 
+        if (attr->detach)
+            flags |= NX_THR_DETACHED;
+        else
+            flags |= NX_THR_JOINABLE;
+    }
+    else {
+        stacksize = APR_DEFAULT_STACK_SIZE;
+        flags |= NX_THR_JOINABLE;
+    }
+    
+    (*new)->ctx = NXContextAlloc(
+    	/* void(*start_routine)(void *arg)*/(void (*)(void *)) func,
+     	/* void *arg */										   data,
+     	/* int priority */ 									   NX_PRIO_MED,
+     	/* NXSize_t stackSize */							   stacksize,
+     	/* long flags */									   NX_CTX_NORMAL,
+     	/* int *error */									   &stat);
+		
+     	                                                                   
+  	stat = NXContextSetName(
+		 	/* NXContext_t ctx */			(*new)->ctx,
+			/* const char *name */			threadName);
+
+  	stat = NXThreadCreate(
+        	/* NXContext_t context */		(*new)->ctx,
+        	/* long flags */				flags,
+        	/* NXThreadId_t *thread_id */	&(*new)->td);
+
+    if(stat==0)
+     	return APR_SUCCESS;
+        
+	return(stat);// if error    
+}
+
+apr_os_thread_t apr_os_thread_current()
+{
+    return NXThreadGetId();
+}
+
+int apr_os_thread_equal(apr_os_thread_t tid1, apr_os_thread_t tid2)
+{
+    return (tid1 == tid2);
+}
+
+apr_status_t apr_thread_yield()
+{
+    NXThreadYield();
+    return APR_SUCCESS;
+}
+
+apr_status_t apr_thread_exit(apr_thread_t *thd,
+                                          apr_status_t *retval)
+{
+    apr_pool_destroy(thd->cntxt);
+    NXThreadExit((void *)*retval);
+    return APR_SUCCESS;
+}
+
+apr_status_t apr_thread_join(apr_status_t *retval,
+                                          apr_thread_t *thd)
+{
+    apr_status_t  stat;    
+    NXThreadId_t dthr;
+
+    if ((stat = NXThreadJoin(thd->td, &dthr, (void *)&retval)) == 0) {
+        return APR_SUCCESS;
+    }
+    else {
+        return stat;
+    }
+}
+
+apr_status_t apr_thread_detach(apr_thread_t *thd)
+{
+    return APR_SUCCESS;
+}
+
+apr_status_t apr_thread_data_get(void **data, const char *key,
+                                             apr_thread_t *thread)
+{
+    if (thread != NULL) {
+            return apr_pool_userdata_get(data, key, thread->cntxt);
+    }
+    else {
+        data = NULL;
+        return APR_ENOTHREAD;
+    }
+}
+
+apr_status_t apr_thread_data_set(void *data, const char *key,
+                              apr_status_t (*cleanup) (void *),
+                              apr_thread_t *thread)
+{
+    if (thread != NULL) {
+       return apr_pool_userdata_set(data, key, cleanup, thread->cntxt);
+    }
+    else {
+        data = NULL;
+        return APR_ENOTHREAD;
+    }
+}
+
+
+/*SRJ Need to resolve
+ APR_DECLARE(apr_status_t) apr_os_thread_get(apr_os_thread_t **thethd,
+                                            apr_thread_t *thd)
+{
+    if (thd == NULL) {
+        return APR_ENOTHREAD;
+    }
+    *thethd = thd->td;
+    return APR_SUCCESS;
+}
+
+APR_DECLARE(apr_status_t) apr_os_thread_put(apr_thread_t **thd,
+                                            apr_os_thread_t *thethd,
+                                            apr_pool_t *cont)
+{
+    if (cont == NULL) {
+        return APR_ENOPOOL;
+    }
+    if ((*thd) == NULL) {
+        (*thd) = (apr_thread_t *)apr_palloc(cont, sizeof(apr_thread_t));
+        (*thd)->cntxt = cont;
+    }
+    (*thd)->td = thethd;
+    return APR_SUCCESS;
+}
+
+*/
