29569d2cceb6c89ba8ee72b748f5c4a920778adc
Jeff Trawick <trawick@apache.org>
2001-08-07 19:57:44 +0000

    Fix apr_dir_rewind() for Win32 to avoid returning a bogus error.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62123 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/dir.c
+++ file_io/win32/dir.c
@@ -257,18 +257,10 @@ APR_DECLARE(apr_status_t) apr_dir_read(apr_finfo_t *finfo, apr_int32_t wanted,
 
 APR_DECLARE(apr_status_t) apr_dir_rewind(apr_dir_t *dir)
 {
-    /* XXX FIXME...
-     * 1) dir_cleanup() does FindClose(), so the FindClose() call
-     *    here will always return a meaningless error (bad handle).
-     * 2) Don't we need to re-open the directory, or is there a
-     *    Win32 way to rewind?
+    /* this will mark the handle as invalid and we'll open it
+     * again if apr_dir_read() is subsequently called
      */
-    dir_cleanup(dir);
-    if (!FindClose(dir->dirhand)) {
-        return apr_get_os_error();
-    }    
-    dir->dirhand = INVALID_HANDLE_VALUE;
-    return APR_SUCCESS;
+    return dir_cleanup(dir);
 }
 
 APR_DECLARE(apr_status_t) apr_dir_make(const char *path, apr_fileperms_t perm,
