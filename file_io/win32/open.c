9082d45b21baf2fd0f2c747fb9215ff468c1489a
William A. Rowe Jr <wrowe@apache.org>
2001-09-05 04:49:08 +0000

    Matt astutely observed that we were only long-pathing the filename iff   the path was slash delimited.  Provide for backslash delimiters.
      Note all our canonicalization refuses to speak \\?\ (and backs it out,
      if it's encountered) since this is entirely unportable even to Win9x.
      Therefore we don't have to worry about encountering it here.
    
    Submitted by:	Mats Nilsson <mats.nilsson@xware.se>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62289 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/open.c
+++ file_io/win32/open.c
@@ -84,12 +84,13 @@ apr_status_t utf8_to_unicode_path(apr_wchar_t* retstr, apr_size_t retlen,
     int retremains = srcremains;
     apr_wchar_t *t = retstr;
     apr_status_t rv;
-    if (srcstr[1] == ':' && srcstr[2] == '/') {
+    if (srcstr[1] == ':' && (srcstr[2] == '/' || srcstr[2] == '\\')) {
         wcscpy (retstr, L"\\\\?\\");
         retlen -= 4;
         t += 4;
     }
-    else if (srcstr[0] == '/' && srcstr[1] == '/') {
+    else if ((srcstr[0] == '/' || srcstr[0] == '\\')
+          && (srcstr[1] == '/' || srcstr[1] == '\\')) {
         /* Skip the slashes */
         srcstr += 2;
         wcscpy (retstr, L"\\\\?\\UNC\\");
