fd962590caa10d15341e90aa4dc9a2ed18f96b7a
Ryan Bloom <rbb@apache.org>
2001-09-19 20:06:44 +0000

    New process locking API for APR.
    Submitted by:	Aaron Bannert <aaron@clove.org>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62347 13f79535-47bb-0310-9956-ffa450edef68

---
+++ include/apr_proc_mutex.h
@@ -0,0 +1,165 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#ifndef APR_PROC_MUTEX_H
+#define APR_PROC_MUTEX_H
+
+#include "apr.h"
+#include "apr_lock.h"    /* only for apr_lockmech_e_np */
+#include "apr_pools.h"
+#include "apr_errno.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * @file apr_proc_mutex.h
+ * @brief APR Process Locking Routines
+ */
+
+/**
+ * @defgroup APR_ProcMutex Process Locking Routines
+ * @ingroup APR
+ * @{
+ */
+
+typedef struct apr_proc_mutex_t apr_proc_mutex_t;
+
+/*   Function definitions */
+
+/**
+ * Create and initialize a mutex that can be used to synchronize processes.
+ * @param mutex the memory address where the newly created mutex will be
+ *        stored.
+ * @param fname A file name to use if the lock mechanism requires one.  This
+ *        argument should always be provided.  The lock code itself will
+ *        determine if it should be used.
+ * @param pool the pool from which to allocate the mutex.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_create(apr_proc_mutex_t **mutex,
+                                                const char *fname,
+                                                apr_pool_t *pool);
+
+/**
+ * non-portable interface to apr_proc_mutex_create()
+ *
+ * Create and initialize a mutex that can be used to synchronize processes.
+ * @param mutex the memory address where the newly created mutex will be
+ *        stored.
+ * @param fname A file name to use if the lock mechanism requires one.  This
+ *        argument should always be provided.  The lock code itself will
+ *        determine if it should be used.
+ * @param mech The mechanism to use for the interprocess lock, if any; one of
+ * <PRE>
+ *            APR_LOCK_FCNTL
+ *            APR_LOCK_FLOCK
+ *            APR_LOCK_SYSVSEM
+ *            APR_LOCK_PROC_PTHREAD
+ *            APR_LOCK_DEFAULT     pick the default mechanism for the platform
+ * </PRE>
+ * @param pool the pool from which to allocate the mutex.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_create_np(apr_proc_mutex_t **mutex,
+                                                   const char *fname,
+                                                   apr_lockmech_e_np mech,
+                                                   apr_pool_t *pool);
+/**
+ * Re-open a mutex in a child process.
+ * @param mutex The newly re-opened mutex structure.
+ * @param fname A file name to use if the mutex mechanism requires one.  This
+ *              argument should always be provided.  The mutex code itself will
+ *              determine if it should be used.  This filename should be the 
+ *              same one that was passed to apr_proc_mutex_create().
+ * @param pool The pool to operate on.
+ * @remark This function must be called to maintain portability, even
+ *         if the underlying lock mechanism does not require it.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_child_init(apr_proc_mutex_t **mutex,
+                                                    const char *fname,
+                                                    apr_pool_t *pool);
+
+/**
+ * Acquire the lock for the given mutex. If the mutex is already locked,
+ * the current thread will be put to sleep until the lock becomes available.
+ * @param mutex the mutex on which to acquire the lock.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_lock(apr_proc_mutex_t *mutex);
+
+/**
+ * Attempt to acquire the lock for the given mutex. If the mutex has already
+ * been acquired, the call returns immediately with APR_EBUSY. Note: it
+ * is important that the APR_STATUS_IS_EBUSY(s) macro be used to determine
+ * if the return value was APR_EBUSY, for portability reasons.
+ * @param mutex the mutex on which to attempt the lock acquiring.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_trylock(apr_proc_mutex_t *mutex);
+
+/**
+ * Release the lock for the given mutex.
+ * @param mutex the mutex from which to release the lock.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_unlock(apr_proc_mutex_t *mutex);
+
+/**
+ * Destroy the mutex and free the memory associated with the lock.
+ * @param mutex the mutex to destroy.
+ */
+APR_DECLARE(apr_status_t) apr_proc_mutex_destroy(apr_proc_mutex_t *mutex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* ! APR_PROC_MUTEX_H */
--- include/arch/unix/proc_mutex.h
+++ include/arch/unix/proc_mutex.h
@@ -52,17 +52,16 @@
  * <http://www.apache.org/>.
  */
 
-#ifndef LOCKS_H
-#define LOCKS_H
+#ifndef PROC_MUTEX_H
+#define PROC_MUTEX_H
 
 #include "apr.h"
 #include "apr_private.h"
 #include "apr_general.h"
 #include "apr_lib.h"
-#include "apr_lock.h"
-#include "apr_sms.h"
+#include "apr_proc_mutex.h"
+#include "apr_pools.h"
 #include "apr_portable.h"
-#include "proc_mutex.h"
 
 /* System headers required by Locks library */
 #if APR_HAVE_SYS_TYPES_H
@@ -98,43 +97,50 @@
 #endif
 /* End System Headers */
 
-struct apr_unix_lock_methods_t {
+struct apr_proc_mutex_unix_lock_methods_t {
     unsigned int flags;
-    apr_status_t (*create)(apr_lock_t *, const char *);
-    apr_status_t (*acquire)(apr_lock_t *);
-    apr_status_t (*tryacquire)(apr_lock_t *);
-    apr_status_t (*acquire_read)(apr_lock_t *);
-    apr_status_t (*acquire_write)(apr_lock_t *);
-    apr_status_t (*release)(apr_lock_t *);
-    apr_status_t (*destroy)(apr_lock_t *);
-    apr_status_t (*child_init)(apr_lock_t **, apr_pool_t *, const char *);
+    apr_status_t (*create)(apr_proc_mutex_t *, const char *);
+    apr_status_t (*acquire)(apr_proc_mutex_t *);
+    apr_status_t (*tryacquire)(apr_proc_mutex_t *);
+    apr_status_t (*release)(apr_proc_mutex_t *);
+    apr_status_t (*destroy)(apr_proc_mutex_t *);
+    apr_status_t (*child_init)(apr_proc_mutex_t **, apr_pool_t *, const char *);
 };
-typedef struct apr_unix_lock_methods_t apr_unix_lock_methods_t;
+typedef struct apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_lock_methods_t;
 
 /* bit values for flags field in apr_unix_lock_methods_t */
 #define APR_PROCESS_LOCK_MECH_IS_GLOBAL          1
 
 #if APR_HAS_SYSVSEM_SERIALIZE
-extern const apr_unix_lock_methods_t apr_unix_sysv_methods;
+extern const apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_sysv_methods;
 #endif
 #if APR_HAS_FCNTL_SERIALIZE
-extern const apr_unix_lock_methods_t apr_unix_fcntl_methods;
+extern const apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_fcntl_methods;
 #endif
 #if APR_HAS_FLOCK_SERIALIZE
-extern const apr_unix_lock_methods_t apr_unix_flock_methods;
+extern const apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_flock_methods;
 #endif
 #if APR_HAS_PROC_PTHREAD_SERIALIZE
-extern const apr_unix_lock_methods_t apr_unix_proc_pthread_methods;
+extern const apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_proc_pthread_methods;
 #endif
 #if APR_HAS_RWLOCK_SERIALIZE
-extern const apr_unix_lock_methods_t apr_unix_rwlock_methods;
+extern const apr_proc_mutex_unix_lock_methods_t apr_proc_mutex_unix_rwlock_methods;
 #endif
 
-struct apr_lock_t {
+
+#if !APR_HAVE_UNION_SEMUN && defined(APR_HAS_SYSVSEM_SERIALIZE)
+/* it makes no sense, but this isn't defined on solaris */
+union semun {
+    long val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+#endif
+
+struct apr_proc_mutex_t {
     apr_pool_t *pool;
-    const apr_unix_lock_methods_t *meth;
-    const apr_unix_lock_methods_t *inter_meth, *intra_meth; /* for APR_LOCK_ALL */
-    apr_locktype_e type;
+    const apr_proc_mutex_unix_lock_methods_t *meth;
+    const apr_proc_mutex_unix_lock_methods_t *inter_meth;
     apr_lockscope_e scope;
     int curr_locked;
     char *fname;
@@ -145,29 +151,15 @@ struct apr_lock_t {
     pthread_mutex_t *pthread_interproc;
 #endif
 #if APR_HAS_THREADS
-    /* APR doesn't have threads, no sense in having an thread lock mechanism.
+    /* APR doesn't have threads, no sense in having a thread lock mechanism.
      */
 
     apr_os_thread_t owner;
     int owner_ref;
-
-#if APR_USE_PTHREAD_SERIALIZE
-    pthread_mutex_t *intraproc;
 #endif
-#ifdef HAVE_PTHREAD_RWLOCK_INIT
-    pthread_rwlock_t rwlock;
-#endif
-#endif
-    /* At some point, we should do a scope for both inter and intra process
-     * locking here.  Something like pthread_mutex with PTHREAD_PROCESS_SHARED
-     */    
 };
 
-#if APR_HAS_THREADS
-extern const apr_unix_lock_methods_t apr_unix_intra_methods;
-#endif
+void apr_proc_mutex_unix_setup_lock(void);
 
-void apr_unix_setup_lock(void);
-
-#endif  /* LOCKS_H */
+#endif  /* PROC_MUTEX_H */
 
