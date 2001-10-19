a1642b0c48db55d1a1068b87b859302be1adb6d0
Bradley Nicholes <bnicholes@apache.org>
2001-10-19 19:25:03 +0000

    Scripts for configuring the build environment on NetWare
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62442 13f79535-47bb-0310-9956-ffa450edef68

---
+++ build/prebuildNW.bat
@@ -0,0 +1,25 @@
+# As part of the pre-build process, the utility GenURI.NLM
+#  (Gen URI Delims) must be built, copied to a NetWare server 
+#  and run using the following command:
+#
+# genuri >uri_delims.h
+#
+#  The file "sys:\uri_delims.h" must then be copied to
+#  "apr-util\uri\uri_delims.h" on the build machine.
+
+# Fix up the APR headers
+copy ..\include\apr.hnw ..\include\apr.h
+
+# Fix up the APR-Util headers
+copy ..\..\apr-util\include\apu.h.in ..\..\apr-util\include\apu.h
+copy ..\..\apr-util\include\private\apu_config.hw ..\..\apr-util\include\private\apu_config.h
+copy ..\..\apr-util\xml\expat\lib\expat.h.in ..\..\apr-util\xml\expat\lib\expat.h
+copy ..\..\apr-util\include\private\apu_select_dbm.hw ..\..\apr-util\include\private\apu_select_dbm.h
+
+# Fix up the pcre headers
+copy ..\..\pcre\config.hw ..\..\pcre\config.h
+copy ..\..\pcre\pcre.hw ..\..\pcre\pcre.h
+
+# Generate the import list
+awk95 -f make_nw_export.awk ..\include\*.h |sort > ..\misc\netware\aprlib.imp
+awk95 -f make_nw_export.awk ..\..\apr-util\include\*.h |sort > ..\misc\netware\aprutil.imp
