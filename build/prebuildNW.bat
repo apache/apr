9538ff7a0d74f9052d56a7c9f0f454c55f0c27bd
Bradley Nicholes <bnicholes@apache.org>
2001-10-25 15:37:16 +0000

    More cleanup on the pre-build scripts
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62463 13f79535-47bb-0310-9956-ffa450edef68

--- build/prebuildNW.bat
+++ build/prebuildNW.bat
@@ -1,25 +1,26 @@
-# As part of the pre-build process, the utility GenURI.NLM
-#  (Gen URI Delims) must be built, copied to a NetWare server 
-#  and run using the following command:
-#
-# genuri >uri_delims.h
-#
-#  The file "sys:\uri_delims.h" must then be copied to
-#  "apr-util\uri\uri_delims.h" on the build machine.
+@echo off
+@echo # As part of the pre-build process, the utility GenURI.NLM
+@echo #  (Gen URI Delims) must be built, copied to a NetWare server 
+@echo #  and run using the following command:
+@echo #
+@echo # "sys:\genuri >sys:\uri_delims.h"
+@echo #
+@echo #  The file "sys:\uri_delims.h" must then be copied to
+@echo #  "apr-util\uri\uri_delims.h" on the build machine.
 
-# Fix up the APR headers
+@echo Fixing up the APR headers
 copy ..\include\apr.hnw ..\include\apr.h
 
-# Fix up the APR-Util headers
+@echo Fixing up the APR-Util headers
 copy ..\..\apr-util\include\apu.h.in ..\..\apr-util\include\apu.h
 copy ..\..\apr-util\include\private\apu_config.hw ..\..\apr-util\include\private\apu_config.h
 copy ..\..\apr-util\xml\expat\lib\expat.h.in ..\..\apr-util\xml\expat\lib\expat.h
 copy ..\..\apr-util\include\private\apu_select_dbm.hw ..\..\apr-util\include\private\apu_select_dbm.h
 
-# Fix up the pcre headers
+@echo Fixing up the pcre headers
 copy ..\..\pcre\config.hw ..\..\pcre\config.h
 copy ..\..\pcre\pcre.hw ..\..\pcre\pcre.h
 
-# Generate the import list
-awk95 -f make_nw_export.awk ..\include\*.h |sort > ..\aprlib.imp
-awk95 -f make_nw_export.awk ..\..\apr-util\include\*.h |sort > ..\aprutil.imp
+@echo Generating the import list...
+awk -f make_nw_export.awk ..\include\*.h |sort > ..\aprlib.imp
+awk -f make_nw_export.awk ..\..\apr-util\include\*.h |sort > ..\aprutil.imp
