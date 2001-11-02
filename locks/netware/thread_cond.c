8a064c696e53cf627a5b533f092c50c75c030090
Bradley Nicholes <bnicholes@apache.org>
2001-11-02 23:27:16 +0000

    Implemented apr_cond_timedwait
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62485 13f79535-47bb-0310-9956-ffa450edef68

--- locks/netware/thread_cond.c
+++ locks/netware/thread_cond.c
@@ -103,7 +103,11 @@ APR_DECLARE(apr_status_t) apr_thread_cond_wait(apr_thread_cond_t *cond,
 APR_DECLARE(apr_status_t) apr_thread_cond_timedwait(apr_thread_cond_t *cond,
                                                     apr_thread_mutex_t *mutex,
                                                     apr_interval_time_t timeout){
-    return APR_ENOTIMPL;
+    if (NXCondTimedWait(cond->cond, mutex->mutex, 
+        (timeout*1000)/NXGetSystemTick()) == NX_ETIMEDOUT) {
+        return APR_TIMEUP;
+    }
+    return APR_SUCCESS;
 }
 
 APR_DECLARE(apr_status_t) apr_thread_cond_signal(apr_thread_cond_t *cond)
