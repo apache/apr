42cc6a34d51a2f28da8f15e978775c6fad1967e5
Bradley Nicholes <bnicholes@apache.org>
2001-11-13 21:07:21 +0000

    NetWare DSO handling routines
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62512 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dso/netware/dso.c
@@ -0,0 +1,166 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+#include "dso.h"
+#include "apr_strings.h"
+#include "apr_portable.h"
+
+#include <library.h>
+#include <unistd.h>
+
+APR_DECLARE(apr_status_t) apr_os_dso_handle_put(apr_dso_handle_t **aprdso,
+                                                apr_os_dso_handle_t osdso,
+                                                apr_pool_t *pool)
+{
+    *aprdso = apr_pcalloc(pool, sizeof **aprdso);
+    (*aprdso)->handle = osdso;
+    (*aprdso)->pool = pool;
+    return APR_SUCCESS;
+}
+
+APR_DECLARE(apr_status_t) apr_os_dso_handle_get(apr_os_dso_handle_t *osdso,
+                                                apr_dso_handle_t *aprdso)
+{
+    *osdso = aprdso->handle;
+    return APR_SUCCESS;
+}
+
+static apr_status_t dso_cleanup(void *thedso)
+{
+    apr_dso_handle_t *dso = thedso;
+    sym_list *symbol = NULL;
+    void *NLMHandle = getnlmhandle();
+
+    if (dso->handle == NULL)
+        return APR_SUCCESS;
+
+    if (dso->symbols != NULL) {
+        symbol = dso->symbols;
+        while (symbol) {
+            UnImportPublicObject(NLMHandle, symbol->symbol);
+            symbol = symbol->next;
+        }
+    }
+
+    if (dlclose(dso->handle) != 0)
+        return APR_EINIT;
+
+    dso->handle = NULL;
+    dso->symbols = NULL;
+    dso->path = NULL;
+
+    return APR_SUCCESS;
+}
+
+APR_DECLARE(apr_status_t) apr_dso_load(apr_dso_handle_t **res_handle, 
+                                       const char *path, apr_pool_t *pool)
+{
+
+    void *os_handle = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+
+    *res_handle = apr_pcalloc(pool, sizeof(**res_handle));
+
+    if(os_handle == NULL) {
+        (*res_handle)->errormsg = dlerror();
+        return APR_EDSOOPEN;
+    }
+
+    (*res_handle)->handle = (void*)os_handle;
+    (*res_handle)->pool = pool;
+    (*res_handle)->errormsg = NULL;
+    (*res_handle)->symbols = NULL;
+    (*res_handle)->path = apr_pstrdup(pool, path);
+
+    apr_pool_cleanup_register(pool, *res_handle, dso_cleanup, apr_pool_cleanup_null);
+
+    return APR_SUCCESS;
+}
+    
+APR_DECLARE(apr_status_t) apr_dso_unload(apr_dso_handle_t *handle)
+{
+    return apr_pool_cleanup_run(handle->pool, handle, dso_cleanup);
+}
+
+APR_DECLARE(apr_status_t) apr_dso_sym(apr_dso_handle_sym_t *ressym, 
+                                      apr_dso_handle_t *handle, 
+                                      const char *symname)
+{
+    sym_list *symbol = NULL;
+    void *retval = dlsym(handle->handle, symname);
+
+    if (retval == NULL) {
+        handle->errormsg = dlerror();
+        return APR_EINIT;
+    }
+
+    symbol = apr_pcalloc(handle->pool, sizeof(sym_list));
+    symbol->next = handle->symbols;
+    handle->symbols = symbol;
+    symbol->symbol = apr_pstrdup(handle->pool, symname);
+
+    *ressym = retval;
+    
+    return APR_SUCCESS;
+}
+
+APR_DECLARE(const char *) apr_dso_error(apr_dso_handle_t *dso, char *buffer, 
+                                        apr_size_t buflen)
+{
+    if (dso->errormsg) {
+        apr_cpystrn(buffer, dso->errormsg, buflen);
+        return dso->errormsg;
+    }
+    return "No Error";
+}
+
