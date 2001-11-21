7cb1dc3cce2ddaf8938b8af57df8e1d8b7f96f3b
Aaron Bannert <aaron@apache.org>
2001-11-21 04:21:04 +0000

    Conversion of the file_io routines from INTRAPROCESS apr_lock_t types to the new apr_thread_mutex_t. All calls to the lock API are protected with APR_HAS_THREADS. Tested on each of Linux, Solaris, and most importantly in this case FreeBSD.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62531 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/unix/fileio.h
+++ include/arch/unix/fileio.h
@@ -63,6 +63,7 @@
 #include "apr_file_info.h"
 #include "apr_errno.h"
 #include "apr_lib.h"
+#include "apr_thread_mutex.h"
 
 /* System headers the file I/O library needs */
 #if APR_HAVE_FCNTL_H
@@ -137,7 +138,7 @@ struct apr_file_t {
     int direction;            /* buffer being used for 0 = read, 1 = write */
     unsigned long filePtr;    /* position in file of handle */
 #if APR_HAS_THREADS
-    struct apr_lock_t *thlock;
+    struct apr_thread_mutex_t *thlock;
 #endif
 };
 
