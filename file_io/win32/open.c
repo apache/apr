18f7bad972a9480cd68a2df0fbe324864800de66
William A. Rowe Jr <wrowe@apache.org>
2002-01-08 22:48:25 +0000

    This helper stub will get us the appropriate Win32 wchar* or char*,   allocated from pool [no big stack vars], no longer than 255 characters,   and with no 'path decoration' so we can turn a filename into a faux   resource name string.  This function fixes resource names into the   Global\ or Local\ namespace on Windows 2000 or later, since this is   a requirement on Win2K Terminal Server, where we have multiple VMs.
      Oh, we don't fail on long names, we will truncate off the left, so as
      much unique info as possible is preserved.  We also shorten to 255 or
      fewer chars, so we don't have multiple utf8->unicode calls.  We aren't
      caring that we loose a few more wchars, as long as it's less than 255.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62726 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/open.c
+++ file_io/win32/open.c
@@ -165,6 +165,89 @@ apr_status_t unicode_to_utf8_path(char* retstr, apr_size_t retlen,
 }
 #endif
 
+void *res_name_from_filename(const char *file, int global, apr_pool_t *pool)
+{
+#if APR_HAS_UNICODE_FS
+    if (apr_os_level >= APR_WIN_NT) {
+        apr_wchar_t *wpre, *wfile, *ch;
+        apr_size_t n = strlen(file) + 1;
+        apr_size_t r, d;
+        apr_status_t rv;
+
+        if (apr_os_level >= APR_WIN_2000) {
+            if (global)
+                wpre = L"Global\\";
+            else
+                wpre = L"Local\\";
+        }
+        else
+            wpre = L"";
+        r = wcslen(wpre);
+
+        if (n > 256 - r) {
+            file += n - 256 - r;
+            n = 256;
+            /* skip utf8 continuation bytes */
+            while ((*file & 0xC0) == 0x80) {
+                ++file;
+                --n;
+            }
+        }
+        wfile = apr_palloc(pool, (r + n) * sizeof(apr_wchar_t));
+        wcscpy(wfile, wpre);
+        d = n;
+        if (rv = conv_utf8_to_ucs2(file, &n, wfile + r, &d)) {
+            return NULL;
+        }
+        for (ch = wfile + r; *ch; ++ch) {
+            if (*ch == ':' || *ch == '/' || *ch == '\\')
+                *ch = '_';
+        }
+    }
+    else
+#endif
+    {
+        char *nfile, *ch;
+        apr_size_t n = strlen(file) + 1;
+
+#if !APR_HAS_UNICODE_FS
+        apr_status_t rv;
+        apr_size_t r, d;
+        char *pre;
+
+        if (apr_os_level >= APR_WIN_2000) {
+            if (global)
+                pre = "Global\\";
+            else
+                pre = "Local\\";
+        }
+        else
+            pre = "";
+        r = strlen(pre);
+
+        if (n > 256 - r) {
+            file += n - 256 - r;
+            n = 256;
+        }
+        nfile = apr_palloc(pool, (r + n) * sizeof(apr_wchar_t));
+        memcpy(nfile, pre, r);
+        memcpy(nfile + r, file, n);
+#else
+        const apr_size_t r = 0;
+        if (n > 256) {
+            file += n - 256;
+            n = 256;
+        }
+        nfile = apr_pmemdup(pool, file, n);
+#endif
+        for (ch = nfile + r; *ch; ++ch) {
+            if (*ch == ':' || *ch == '/' || *ch == '\\')
+                *ch = '_';
+        }
+    }
+}
+
+
 apr_status_t file_cleanup(void *thefile)
 {
     apr_file_t *file = thefile;
