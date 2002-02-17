462ac975097b6fd94eeec4c6090ec42d353c059e
Ian Holsman <ianh@apache.org>
2002-02-17 19:54:03 +0000

    preperation for solaris sparc addition just need to figure out     a) where to put it     b) how to build it in a apr-nice way
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@62997 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_atomic.h
+++ include/apr_atomic.h
@@ -87,8 +87,19 @@ extern "C" {
 #define apr_atomic_read(mem)         atomic_read(mem)
 #define apr_atomic_init(pool)        APR_SUCCESS
 
-#else /* !defined(WIN32) && !defined(__linux) */
+#elif defined(__sparc__not_ready_yet)
+#define apr_atomic_t apr_uint32_t
+#define apr_atomic_read(p)  *p
 
+#define apr_atomic_add(mem, val)     apr_atomic_add_sparc(mem,val)
+#define apr_atomic_dec(mem)          apr_atomic_sub_sparc(mem,1)
+#define apr_atomic_inc(mem)          apr_atomic_add_sparc(mem,1)
+#define apr_atomic_set(mem, val)     *mem= val
+#define apr_atomic_init(pool)        APR_SUCCESS
+
+apr_uint32_t apr_atomic_add_sparc( volatile apr_atomic_t* mem, apr_uint32_t add);
+apr_uint32_t apr_atomic_sub_sparc( volatile apr_atomic_t* mem, apr_uint32_t sub);
+#else
 #if APR_HAS_THREADS
 
 #define apr_atomic_t apr_uint32_t
@@ -100,6 +111,7 @@ apr_uint32_t apr_atomic_inc(volatile apr_atomic_t *mem);
 apr_uint32_t apr_atomic_dec(volatile apr_atomic_t *mem);
 /*long apr_atomic_cas(volatile apr_atomic_t *mem,long with,long cmp);*/
 
+#define APR_ATOMIC_NEED_DEFAULT 1
 #endif /* APR_HAS_THREADS */
 
 #endif /* !defined(WIN32) && !defined(__linux) */
