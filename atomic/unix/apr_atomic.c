d6addbee60c4f38ecec7cc12e6657a41b95628a3
Greg Ames <gregames@apache.org>
2002-03-19 22:42:30 +0000

    apr_atomic_dec:  change the fallback version to return the new value of the memory variable.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63161 13f79535-47bb-0310-9956-ffa450edef68

--- atomic/unix/apr_atomic.c
+++ atomic/unix/apr_atomic.c
@@ -126,13 +126,13 @@ void apr_atomic_inc( volatile apr_uint32_t *mem)
 int apr_atomic_dec(volatile apr_atomic_t *mem) 
 {
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
-    apr_uint32_t prev;
+    apr_uint32_t new;
 
     if (apr_thread_mutex_lock(lock) == APR_SUCCESS) {
-        prev = *mem;
         (*mem)--;
+        new = *mem;
         apr_thread_mutex_unlock(lock);
-        return prev; 
+        return new; 
     }
     return *mem; 
 }
