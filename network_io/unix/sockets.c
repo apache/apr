4733faf28c952c9b3e7004ae0cdaa752714868df
Jeff Trawick <trawick@apache.org>
2002-04-01 14:13:45 +0000

    rename internal function apr_set_sockaddr_vars() to apr_sockaddr_vars_set()
    Submitted by:	Kevin Pilch-Bisson
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63210 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockets.c
+++ network_io/unix/sockets.c
@@ -79,8 +79,8 @@ static apr_status_t socket_cleanup(void *sock)
 static void set_socket_vars(apr_socket_t *sock, int family, int type)
 {
     sock->type = type;
-    apr_set_sockaddr_vars(sock->local_addr, family, 0);
-    apr_set_sockaddr_vars(sock->remote_addr, family, 0);
+    apr_sockaddr_vars_set(sock->local_addr, family, 0);
+    apr_sockaddr_vars_set(sock->remote_addr, family, 0);
     sock->netmask = 0;
 #if defined(BEOS) && !defined(BEOS_BONE)
     /* BeOS pre-BONE has TCP_NODELAY on by default and it can't be
