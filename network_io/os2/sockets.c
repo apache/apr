7d9e01b91e1c6fbe3f6d5e358a9e76100c5092b9
Brian Havard <bjh@apache.org>
2002-04-14 08:33:52 +0000

    OS/2: Fix apr_os_put_sock() to set local address as unknown & timeout to -1.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63263 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/os2/sockets.c
+++ network_io/os2/sockets.c
@@ -288,7 +288,10 @@ APR_DECLARE(apr_status_t) apr_os_sock_put(apr_socket_t **sock, apr_os_sock_t *th
     if ((*sock) == NULL) {
         alloc_socket(sock, cont);
         set_socket_vars(*sock, AF_INET, SOCK_STREAM);
+        (*sock)->timeout = -1;
     }
+
+    (*sock)->local_port_unknown = (*sock)->local_interface_unknown = 1;
     (*sock)->socketdes = *thesock;
     return APR_SUCCESS;
 }
