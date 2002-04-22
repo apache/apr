2967b601d919e572d479df36454c5e3ba82885df
Justin Erenkrantz <jerenkrantz@apache.org>
2002-04-22 01:49:46 +0000

    Style police are on the prowl.
    (No code changes)
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63288 13f79535-47bb-0310-9956-ffa450edef68

--- atomic/unix/apr_atomic.c
+++ atomic/unix/apr_atomic.c
@@ -52,29 +52,31 @@
  * <http://www.apache.org/>.
  */
 
-
 #include "apr.h"
 #include "apr_atomic.h"
 #include "apr_thread_mutex.h"
 
 #if APR_HAS_THREADS
 
-#if defined(APR_ATOMIC_NEED_DEFAULT)  || defined(APR_ATOMIC_NEED_CAS_DEFAULT)
+#if defined(APR_ATOMIC_NEED_DEFAULT) || defined(APR_ATOMIC_NEED_CAS_DEFAULT)
 
 #define NUM_ATOMIC_HASH 7
 /* shift by 2 to get rid of alignment issues */
 #define ATOMIC_HASH(x) (unsigned int)(((unsigned long)(x)>>2)%(unsigned int)NUM_ATOMIC_HASH)
 static apr_thread_mutex_t **hash_mutex;
 
-apr_status_t apr_atomic_init(apr_pool_t *p )
+apr_status_t apr_atomic_init(apr_pool_t *p)
 {
     int i;
     apr_status_t rv;
-    hash_mutex =apr_palloc(p,sizeof(apr_thread_mutex_t*) * NUM_ATOMIC_HASH);
-    for (i=0;i<NUM_ATOMIC_HASH;i++) {
-        rv = apr_thread_mutex_create(&(hash_mutex[i]), APR_THREAD_MUTEX_DEFAULT, p);
-        if (rv != APR_SUCCESS)
+    hash_mutex = apr_palloc(p, sizeof(apr_thread_mutex_t*) * NUM_ATOMIC_HASH);
+
+    for (i = 0; i < NUM_ATOMIC_HASH; i++) {
+        rv = apr_thread_mutex_create(&(hash_mutex[i]),
+                                     APR_THREAD_MUTEX_DEFAULT, p);
+        if (rv != APR_SUCCESS) {
            return rv;
+        }
     }
     return APR_SUCCESS;
 }
@@ -90,10 +92,9 @@ void apr_atomic_add(volatile apr_atomic_t *mem, apr_uint32_t val)
         prev = *mem;
         *mem += val;
         apr_thread_mutex_unlock(lock);
-/*        return prev; */
     }
-/*    return *mem; */
 }
+
 void apr_atomic_set(volatile apr_atomic_t *mem, apr_uint32_t val) 
 {
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
@@ -103,12 +104,10 @@ void apr_atomic_set(volatile apr_atomic_t *mem, apr_uint32_t val)
         prev = *mem;
         *mem = val;
         apr_thread_mutex_unlock(lock);
-/*        return prev; */
     }
-/*    return *mem; */
 }
 
-void apr_atomic_inc( volatile apr_uint32_t *mem) 
+void apr_atomic_inc(volatile apr_uint32_t *mem) 
 {
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
     apr_uint32_t prev;
@@ -117,10 +116,9 @@ void apr_atomic_inc( volatile apr_uint32_t *mem)
         prev = *mem;
         (*mem)++;
         apr_thread_mutex_unlock(lock);
-/*        return prev; */
     }
-/*    return *mem; */
 }
+
 int apr_atomic_dec(volatile apr_atomic_t *mem) 
 {
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
@@ -138,7 +136,7 @@ int apr_atomic_dec(volatile apr_atomic_t *mem)
 #endif /* APR_ATOMIC_NEED_DEFAULT */
 #if defined(APR_ATOMIC_NEED_CAS_DEFAULT)
 
-apr_uint32_t apr_atomic_cas(volatile apr_uint32_t *mem,long with, long cmp)
+apr_uint32_t apr_atomic_cas(volatile apr_uint32_t *mem, long with, long cmp)
 {
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
     long prev;
