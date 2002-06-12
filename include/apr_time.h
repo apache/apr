0080ce9eda7132f79a13e4fe9d580a5a378964ef
William A. Rowe Jr <wrowe@apache.org>
2002-06-12 22:23:56 +0000

    Lowercase macros, per Roy Fielding's comments and general concensus.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63500 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_time.h
+++ include/apr_time.h
@@ -95,13 +95,13 @@ typedef apr_int32_t apr_short_interval_time_t;
 /** number of microseconds per second */
 #define APR_USEC_PER_SEC APR_TIME_C(1000000)
 
-#define APR_TIME_USEC(time) ((apr_int32_t)(time) % APR_USEC_PER_SEC)
+#define apr_time_usec(time) ((apr_int32_t)(time) % APR_USEC_PER_SEC)
 
-#define APR_TIME_SEC(time) ((apr_int64_t)(time) / APR_USEC_PER_SEC)
+#define apr_time_sec(time) ((apr_int64_t)(time) / APR_USEC_PER_SEC)
 
-#define APR_TIME_FROM_SEC(sec) ((apr_time_t)(sec) * APR_USEC_PER_SEC)
+#define apr_time_from_sec(sec) ((apr_time_t)(sec) * APR_USEC_PER_SEC)
 
-#define APR_TIME_MAKE(sec, usec) ((apr_time_t)(sec) * APR_USEC_PER_SEC \
+#define apr_time_make(sec, usec) ((apr_time_t)(sec) * APR_USEC_PER_SEC \
                                 + (apr_time_t)(usec))
 
 /**
