d8eb3ec21a27bc4e33097642732eaa7c7fdfd5b0
William A. Rowe Jr <wrowe@apache.org>
2002-07-15 07:39:32 +0000

    Missed a semicol
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@63676 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockopt.c
+++ network_io/unix/sockopt.c
@@ -356,7 +356,7 @@ apr_status_t apr_setsocketopt(apr_socket_t *sock,
 apr_status_t apr_getsocketopt(apr_socket_t *sock,
                               apr_int32_t opt, apr_int32_t *on)
 {
-    return apr_socket_opt_get(sock, opt, on)
+    return apr_socket_opt_get(sock, opt, on);
 }
                                            
 
