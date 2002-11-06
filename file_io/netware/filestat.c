bfa435e1cda587482ea0b5f3cb9b43c1f1ae56c5
Bradley Nicholes <bnicholes@apache.org>
2002-11-06 17:08:53 +0000

    Store and use the path context to each directory and file rather than relying on the directory path alone.  This allows for direct access into the file system on every stat() and open() call rather than having to traverse the file system each time.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64000 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/netware/filestat.c
+++ file_io/netware/filestat.c
@@ -53,9 +53,7 @@
  */
 
 #include "fileio.h"
-#ifdef FAST_STAT
 #include "fsio.h"
-#endif
 #include "nks/dirio.h"
 #include "apr_file_io.h"
 #include "apr_general.h"
@@ -106,32 +104,6 @@ static void fill_out_finfo(apr_finfo_t *finfo, struct stat *info,
      * }
      */
 }
-
-#ifndef FAST_STAT
-char *case_filename(apr_pool_t *pPool, const char *szFile)
-{
-    char *casedFileName = NULL;
-    char name[1024];
-    int rc;
-
-    rc = realname(szFile, name);
-    if (rc == 0) {
-        casedFileName = apr_pstrdup(pPool, name);
-    }
-    else
-    {
-        char *s;
-        s = strrchr(szFile, '/');
-        if (!s)
-            s = strrchr(szFile, ':');
-        if (s) {
-            casedFileName = apr_pstrdup(pPool, &s[1]);
-        }
-    }
-    return casedFileName;
-}
-#endif
-
 
 APR_DECLARE(apr_status_t) apr_file_info_get(apr_finfo_t *finfo, 
                                             apr_int32_t wanted,
@@ -210,7 +182,6 @@ APR_DECLARE(apr_status_t) apr_file_attrs_set(const char *fname,
     return apr_file_perms_set(fname, finfo.protection);
 }
 
-#ifdef FAST_STAT
 int cstat (const char *path, struct stat *buf, char **casedName, apr_pool_t *pool)
 {
     apr_hash_t *statCache = (apr_hash_t *)getStatCache(CpuCurrentProcessor);
@@ -310,7 +281,6 @@ int cstat (const char *path, struct stat *buf, char **casedName, apr_pool_t *poo
                     }
                 }
 
-/* xxx Need to handle error codes here */
                 err = NXCreatePathContext(pathCtx, dirPath, 0, NULL, &pathCtx);
 
                 key = apr_pstrdup (gPool, path);
@@ -379,113 +349,6 @@ int cstat (const char *path, struct stat *buf, char **casedName, apr_pool_t *poo
     }
     return 0;
 }
-#else
-int cstat (const char *path, struct stat *buf, char **casedName, apr_pool_t *pool)
-{
-    apr_hash_t *statCache = (apr_hash_t *)getStatCache(CpuCurrentProcessor);
-    apr_pool_t *gPool = (apr_pool_t *)getGlobalPool(CpuCurrentProcessor);
-    apr_stat_entry_t *stat_entry;
-    struct stat *info;
-    apr_time_t now = apr_time_now();
-    char *key;
-    int ret;
-    int found = 0;
-
-    *casedName = NULL;
-
-    /* If there isn't a global pool then just stat the file
-       and return */
-    if (!gPool) {
-        char poolname[50];
-
-        if (apr_pool_create(&gPool, NULL) != APR_SUCCESS) {
-            ret = stat(path, buf);
-            if (ret == 0)
-                *casedName = case_filename(pool, path);
-            return ret;
-        }
-
-        sprintf (poolname, "cstat_mem_pool_%d", CpuCurrentProcessor);
-        apr_pool_tag(gPool, poolname);
-
-        setGlobalPool(gPool, CpuCurrentProcessor);
-    }
-
-    /* If we have a statCache hash table then use it.
-       Otherwise we need to create it and initialized it
-       with a new mutex lock. */
-    if (!statCache) {
-        statCache = apr_hash_make(gPool);
-        setStatCache((void*)statCache, CpuCurrentProcessor);
-    }
-
-    /* If we have a statCache then try to pull the information
-       from the cache.  Otherwise just stat the file and return.*/
-    if (statCache) {
-        stat_entry = (apr_stat_entry_t*) apr_hash_get(statCache, path, APR_HASH_KEY_STRING);
-        /* If we got an entry then check the expiration time.  If the entry
-           hasn't expired yet then copy the information and return. */
-        if (stat_entry) {
-            if ((now - stat_entry->expire) <= APR_USEC_PER_SEC) {
-                memcpy (buf, &(stat_entry->info), sizeof(struct stat));
-                if (stat_entry->casedName)
-                    *casedName = apr_pstrdup (pool, stat_entry->casedName);
-                else
-                    *casedName = case_filename(pool, path);
-                found = 1;
-            }
-        }
-
-        /* Since we are creating a separate stat cache for each processor, we
-           don't need to worry about locking the hash table before manipulating
-           it. */
-        if (!found) {
-            /* Bind the thread to the current cpu so that we don't wake
-               up on some other cpu and try to manipulate the wrong cache. */
-            NXThreadBind (CpuCurrentProcessor);
-            ret = stat(path, buf);
-            if (ret == 0) {
-                *casedName = case_filename(pool, path);
-                /* If we don't have a stat_entry then create one, copy
-                   the data and add it to the hash table. */
-                if (!stat_entry) {
-                    key = apr_pstrdup (gPool, path);
-                    stat_entry = apr_palloc (gPool, sizeof(apr_stat_entry_t));
-                    memcpy (&(stat_entry->info), buf, sizeof(struct stat));
-                    if (*casedName)
-                        stat_entry->casedName = apr_pstrdup (gPool, *casedName);
-                    stat_entry->expire = now;
-                    apr_hash_set(statCache, key, APR_HASH_KEY_STRING, stat_entry);
-                }
-                else {
-                    /* If we do have a stat_entry then it must have expired.  Just
-                       copy the data and reset the expiration. */
-                    memcpy (&(stat_entry->info), buf, sizeof(struct stat));
-
-                    /* If we have a casedName and don't have a cached name or the names don't
-                       compare, then cache the name. */
-                    if (*casedName && (!stat_entry->casedName || strcmp(*casedName, stat_entry->casedName))) {
-                        stat_entry->casedName = apr_pstrdup (gPool, *casedName);
-                    }
-                    stat_entry->expire = now;
-                }
-                NXThreadBind (NX_THR_UNBOUND);
-            }
-            else{
-                NXThreadBind (NX_THR_UNBOUND);
-                return ret;
-			}
-        }
-    }
-    else {
-        ret = stat(path, buf);
-        if (ret == 0)
-            *casedName = case_filename(pool, path);
-        return ret;
-    }
-    return 0;
-}
-#endif
 
 APR_DECLARE(apr_status_t) apr_stat(apr_finfo_t *finfo, 
                                    const char *fname, 
