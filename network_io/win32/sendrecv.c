7066fc2c57f714929abe59b49fd80d47aecad083
Bill Stoddard <stoddard@apache.org>
2002-11-20 15:30:43 +0000

    Win32: Fix Win32 compile break due to last commit (renames) Submitted by: Sebastian Bergmann Reviewed by: Bill Stoddard
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64046 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/win32/sendrecv.c
+++ network_io/win32/sendrecv.c
@@ -474,7 +474,7 @@ APR_DECLARE(apr_status_t) apr_sendfile(apr_socket_t *sock, apr_file_t *file,
 APR_DECLARE(apr_status_t) apr_send(apr_socket_t *sock, const char *buf,
                                    apr_size_t *len)
 {
-    return apr_socket_send(sock. buf, len);
+    return apr_socket_send(sock, buf, len);
 }
 
 /* Deprecated */
