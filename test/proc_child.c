a6bbd257cff014eeeddb456a166f6ab8c6f675de
Joe Orton <jorton@apache.org>
2002-11-27 12:30:18 +0000

    Fix to not assume input is NUL-terminated.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64099 13f79535-47bb-0310-9956-ffa450edef68

--- test/proc_child.c
+++ test/proc_child.c
@@ -8,9 +8,11 @@
 int main(void)
 {
     char buf[256];
-
-    read(STDIN_FILENO, buf, 256);
-    fprintf(stdout, "%s", buf);
+    ssize_t bytes;
+    
+    bytes = read(STDIN_FILENO, buf, 256);
+    if (bytes > 0)
+	write(STDOUT_FILENO, buf, bytes);
 
     return 0; /* just to keep the compiler happy */
 }
