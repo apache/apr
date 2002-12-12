1e39a118c9d1f07c5930540826f50c6fa134e98c
William A. Rowe Jr <wrowe@apache.org>
2002-12-12 15:41:16 +0000

    Since Jeff indicates we trip on FIFO... here should be a legitmiate   workaround for those platforms without FIFO support.  This is still   quite a bit simpler for the mainline cases if you look at the extra   work (discounting the potential benefits of optimizing compilers)   that happens within the S_ISxxx() macros.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64160 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/filestat.c
+++ file_io/unix/filestat.c
@@ -73,13 +73,29 @@ static apr_filetype_e filetype_from_mode(mode_t mode)
         type = APR_CHR;  break;
     case S_IFBLK:
         type = APR_BLK;  break;
+#if defined(S_IFFIFO)
     case S_IFFIFO:
         type = APR_PIPE; break;
+#endif
 #if !defined(BEOS) && defined(S_IFSOCK)
     case S_IFSOCK:
         type = APR_SOCK; break;
 #endif
+
     default:
+	/* Work around missing S_IFxxx values above
+         * for Linux et al.
+         */
+#if !defined(S_IFFIFO) && defined(S_ISFIFO)
+    	if (S_ISFIFO(type)) {
+            type = APR_PIPE;
+	} else
+#endif
+#if !defined(BEOS) && !defined(S_IFSOCK) && defined(S_ISSOCK)
+    	if (S_ISSOCK(type)) {
+            type = APR_SOCK;
+	} else
+#endif
         type = APR_NOFILE;
     }
     return type;
