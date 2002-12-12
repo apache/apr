796c0f533b7e9013836d2694b9314fea3112ab4a
William A. Rowe Jr <wrowe@apache.org>
2002-12-12 07:01:51 +0000

    switch {case} and default: are probably better for handling this case.   Is anyone aware of a platform where S_IFxxx # isn't available, yet   S_ISxxx(#) is?
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64157 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/filestat.c
+++ file_io/unix/filestat.c
@@ -60,24 +60,28 @@
 
 static apr_filetype_e filetype_from_mode(mode_t mode)
 {
-    apr_filetype_e type = APR_NOFILE;
+    apr_filetype_e type;
 
-    if (S_ISREG(mode))
-        type = APR_REG;
-    if (S_ISDIR(mode))
-        type = APR_DIR;
-    if (S_ISCHR(mode))
-        type = APR_CHR;
-    if (S_ISBLK(mode))
-        type = APR_BLK;
-    if (S_ISFIFO(mode))
-        type = APR_PIPE;
-    if (S_ISLNK(mode))
-        type = APR_LNK;
-#if !defined(BEOS) && defined(S_ISSOCK)
-    if (S_ISSOCK(mode))
-        type = APR_SOCK;
+    switch (type & S_IFMT) {
+    case S_IFREG:
+        type = APR_REG;  break;
+    case S_IFDIR:
+        type = APR_DIR;  break;
+    case S_IFLNK:
+        type = APR_LNK;  break;
+    case S_IFCHR:
+        type = APR_CHR;  break;
+    case S_IFBLK:
+        type = APR_BLK;  break;
+    case S_IFFIFO:
+        type = APR_PIPE; break;
+#if !defined(BEOS) && defined(S_IFSOCK)
+    case S_IFSOCK:
+        type = APR_SOCK; break;
 #endif
+    default:
+        type = APR_NOFILE;
+    }
     return type;
 }
 
