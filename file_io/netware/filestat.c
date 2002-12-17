8ffc50b5ec3e35a8b41ab0e2cddee93281d562a0
William A. Rowe Jr <wrowe@apache.org>
2002-12-17 01:08:12 +0000

    Brad, please review.  Adopt the alternate 'UNKFILE' if we failed   to S_ISxxx() the file.  Note: might as well drag over the unix code,   or better yet, start using the unix filestat if it's this tightly   applicable.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64183 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/netware/filestat.c
+++ file_io/netware/filestat.c
@@ -67,18 +67,20 @@ static apr_filetype_e filetype_from_mode(mode_t mode)
 
     if (S_ISREG(mode))
         type = APR_REG;
-    if (S_ISDIR(mode))
+    else if (S_ISDIR(mode))
         type = APR_DIR;
-    if (S_ISCHR(mode))
+    else if (S_ISCHR(mode))
         type = APR_CHR;
-    if (S_ISBLK(mode))
+    else if (S_ISBLK(mode))
         type = APR_BLK;
-    if (S_ISFIFO(mode))
+    else if (S_ISFIFO(mode))
         type = APR_PIPE;
-    if (S_ISLNK(mode))
+    else if (S_ISLNK(mode))
         type = APR_LNK;
-    if (S_ISSOCK(mode))
+    else if (S_ISSOCK(mode))
         type = APR_SOCK;
+    else
+        type = APR_UNKFILE;
     return type;
 }
 
