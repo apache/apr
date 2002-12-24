e394debc0a72207353f119914ea5870fdb83d40e
Ryan Bloom <rbb@apache.org>
2002-12-24 15:59:58 +0000

    Fix an infinite loop in apr_poll_socket_clear. Submitted by:	INOUE Seiichiro <inoue@ariel-networks.com>
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64206 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/pollacc.c
+++ poll/unix/pollacc.c
@@ -169,6 +169,7 @@ APR_DECLARE(apr_status_t) apr_poll_socket_clear(apr_pollfd_t *aprset, apr_int16_
         if (curr->reqevents & events) {
             curr->reqevents &= ~events;
         }
+        curr++
     }
     return APR_SUCCESS;
 }
