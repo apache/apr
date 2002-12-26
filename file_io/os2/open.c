1b2ec8913162258815aeeda96944f8266a31216c
Brian Havard <bjh@apache.org>
2002-12-26 08:27:26 +0000

    OS/2: Fix file open flag manipulation so that APR_EXCL actually works. This also fixes a number of other testfile failures.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64209 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/os2/open.c
+++ file_io/os2/open.c
@@ -120,7 +120,7 @@ APR_DECLARE(apr_status_t) apr_file_open(apr_file_t **new, const char *fname, apr
 
     if (flag & APR_TRUNCATE) {
         oflags |= OPEN_ACTION_REPLACE_IF_EXISTS;
-    } else if ((oflags & 0xF) == 0) {
+    } else if ((oflags & 0xFF) == 0) {
         oflags |= OPEN_ACTION_OPEN_IF_EXISTS;
     }
     
