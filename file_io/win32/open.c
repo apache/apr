9c713702c830693d91019a6e0cc5e8d8e43c3e1d
William A. Rowe Jr <wrowe@apache.org>
2002-12-28 20:29:12 +0000

    Remove the segfault from testfile.c and introduce new breakage.
      If the filehandle we are closing corresponds to a standard handle,
      we must protect against these dup2'ed handles in file_cleanup and
      invalidate the corresponding StdHandle entries, or they may point
      (in the future) to a newly reused handle identifier.
    
      Why testfile.c thinks it can close a std handle and then simply
      obtain it again is beyond me.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64210 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/open.c
+++ file_io/win32/open.c
@@ -264,6 +264,21 @@ apr_status_t file_cleanup(void *thefile)
     apr_status_t flush_rv = APR_SUCCESS;
 
     if (file->filehand != INVALID_HANDLE_VALUE) {
+
+        /* In order to avoid later segfaults with handle 'reuse',
+         * we must protect against the case that a dup2'ed handle
+         * is being closed, and invalidate the corresponding StdHandle 
+         */
+        if (file->filehand == GetStdHandle(STD_ERROR_HANDLE)) {
+            SetStdHandle(STD_ERROR_HANDLE, INVALID_HANDLE_VALUE);
+        }
+        else if (file->filehand == GetStdHandle(STD_OUTPUT_HANDLE)) {
+            SetStdHandle(STD_OUTPUT_HANDLE, INVALID_HANDLE_VALUE);
+        }
+        else if (file->filehand == GetStdHandle(STD_INPUT_HANDLE)) {
+            SetStdHandle(STD_INPUT_HANDLE, INVALID_HANDLE_VALUE);
+        }
+
         if (file->buffered) {
             flush_rv = apr_file_flush((apr_file_t *)thefile);
         }
