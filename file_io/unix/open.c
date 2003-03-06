8ff307df477dcc03710ecf9d62667f240ac34fdb
Joe Orton <jorton@apache.org>
2003-03-06 09:24:17 +0000

    Eliminate a local variable in apr_unix_file_cleanup.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64402 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/open.c
+++ file_io/unix/open.c
@@ -68,13 +68,11 @@ apr_status_t apr_unix_file_cleanup(void *thefile)
 {
     apr_file_t *file = thefile;
     apr_status_t flush_rv = APR_SUCCESS, rv = APR_SUCCESS;
-    int rc;
 
     if (file->buffered) {
         flush_rv = apr_file_flush(file);
     }
-    rc = close(file->filedes);
-    if (rc == 0) {
+    if (close(file->filedes) == 0) {
         file->filedes = -1;
         if (file->flags & APR_DELONCLOSE) {
             unlink(file->fname);
