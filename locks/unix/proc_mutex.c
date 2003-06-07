b431029b0e384a105a0a14082ea691ce872284f3
Jeff Trawick <trawick@apache.org>
2003-06-07 12:37:48 +0000

    Don't require the lock file name to be passed into apr_proc_mutex_child_init() or apr_global_mutex_child_init(). This allows child init to work when the lock file was a temp file created by APR.  (The problem only occurred with flock- based mutexes.)
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64530 13f79535-47bb-0310-9956-ffa450edef68

--- locks/unix/proc_mutex.c
+++ locks/unix/proc_mutex.c
@@ -682,6 +682,9 @@ static apr_status_t proc_mutex_flock_child_init(apr_proc_mutex_t **mutex,
 
     memcpy(new_mutex, *mutex, sizeof *new_mutex);
     new_mutex->pool = pool;
+    if (!fname) {
+        fname = (*mutex)->fname;
+    }
     new_mutex->fname = apr_pstrdup(pool, fname);
     rv = apr_file_open(&new_mutex->interproc, new_mutex->fname,
                        APR_WRITE, 0, new_mutex->pool);
