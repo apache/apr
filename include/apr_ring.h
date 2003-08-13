1864ed4fa3e39990a88f777822c7f2a461116e32
Cliff Woolley <jwoolley@apache.org>
2003-08-13 01:32:11 +0000

    rename "this" to "here" since "this" is a reserved word in c++
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64584 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_ring.h
+++ include/apr_ring.h
@@ -488,28 +488,28 @@
 	APR_RING_CHECK_ELEM(APR_RING_SENTINEL(hp, elem, link), elem, link, msg)
 #define APR_RING_CHECK_ELEM(ep, elem, link, msg) do {			\
 	struct elem *start = (ep);					\
-	struct elem *this = start;					\
+	struct elem *here = start;					\
 	fprintf(stderr, "*** ring check start -- %s\n", msg);		\
 	do {								\
-	    fprintf(stderr, "\telem %p\n", this);			\
+	    fprintf(stderr, "\telem %p\n", here);			\
 	    fprintf(stderr, "\telem->next %p\n",			\
-		    APR_RING_NEXT(this, link));				\
+		    APR_RING_NEXT(here, link));				\
 	    fprintf(stderr, "\telem->prev %p\n",			\
-		    APR_RING_PREV(this, link));				\
+		    APR_RING_PREV(here, link));				\
 	    fprintf(stderr, "\telem->next->prev %p\n",			\
-		    APR_RING_PREV(APR_RING_NEXT(this, link), link));	\
+		    APR_RING_PREV(APR_RING_NEXT(here, link), link));	\
 	    fprintf(stderr, "\telem->prev->next %p\n",			\
-		    APR_RING_NEXT(APR_RING_PREV(this, link), link));	\
-	    if (APR_RING_PREV(APR_RING_NEXT(this, link), link) != this) { \
-		fprintf(stderr, "\t*** this->next->prev != this\n");	\
+		    APR_RING_NEXT(APR_RING_PREV(here, link), link));	\
+	    if (APR_RING_PREV(APR_RING_NEXT(here, link), link) != here) { \
+		fprintf(stderr, "\t*** elem->next->prev != elem\n");	\
 		break;							\
 	    }								\
-	    if (APR_RING_NEXT(APR_RING_PREV(this, link), link) != this) { \
-		fprintf(stderr, "\t*** this->prev->next != this\n");	\
+	    if (APR_RING_NEXT(APR_RING_PREV(here, link), link) != here) { \
+		fprintf(stderr, "\t*** elem->prev->next != elem\n");	\
 		break;							\
 	    }								\
-	    this = APR_RING_NEXT(this, link);				\
-	} while (this != start);					\
+	    here = APR_RING_NEXT(here, link);				\
+	} while (here != start);					\
 	fprintf(stderr, "*** ring check end\n");			\
     } while (0)
 #else
