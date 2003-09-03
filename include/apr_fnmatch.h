eb0213ee4ea1023cabc8c0fb6e51e87b729ebe3d
Jeff Trawick <trawick@apache.org>
2003-09-03 18:37:38 +0000

    axe these deprecated functions:
    apr_is_fnmatch
    apr_file_set_inherit
    apr_file_unset_inherit
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64608 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_fnmatch.h
+++ include/apr_fnmatch.h
@@ -87,9 +87,6 @@ APR_DECLARE(apr_status_t) apr_fnmatch(const char *pattern,
  */
 APR_DECLARE(int) apr_fnmatch_test(const char *pattern);
 
-/** @deprecated @see apr_fnmatch_test */
-APR_DECLARE(int) apr_is_fnmatch(const char *pattern);
-
 /** @} */
 
 #ifdef __cplusplus
