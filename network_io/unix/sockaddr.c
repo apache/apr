b1a62c6d8329d839a6d14d06b8c643e5de8b783d
Joe Orton <jorton@apache.org>
2003-09-29 19:08:15 +0000

    Revert rev1.38 on 0.9 branch pending further testing on HEAD; rev1.38 was:
    Use AI_ADDRCONFIG flag by default, where available.
    
    * sockaddr.c (call_resolver): For lookups of AF_UNSPEC addresses, set
    the AI_ADDRCONFIG flag; retry without the flag if getaddrinfo returns
    EAI_BADFLAGS.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/APR_0_9_BRANCH@64667 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockaddr.c
+++ network_io/unix/sockaddr.c
@@ -374,14 +374,6 @@ static apr_status_t call_resolver(apr_sockaddr_t **sa,
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = family;
     hints.ai_socktype = SOCK_STREAM;
-#ifdef AI_ADDRCONFIG
-    if (family == AF_UNSPEC) {
-        /* By default, only look up addresses using address types for
-         * which a local interface is configured, i.e. no IPv6 if no
-         * IPv6 interfaces configured. */
-        hints.ai_flags = AI_ADDRCONFIG;
-    }
-#endif
     if(hostname == NULL) {
 #ifdef AI_PASSIVE 
         /* If hostname is NULL, assume we are trying to bind to all
@@ -404,13 +396,6 @@ static apr_status_t call_resolver(apr_sockaddr_t **sa,
         servname = apr_itoa(p, port);
     }
     error = getaddrinfo(hostname, servname, &hints, &ai_list);
-#ifdef AI_ADDRCONFIG
-    if (error == EAI_BADFLAGS && family == AF_UNSPEC) {
-        /* Retry with no flags if AI_ADDRCONFIG was rejected. */
-        hints.ai_flags = 0;
-        error = getaddrinfo(hostname, servname, &hints, &ai_list);
-    }
-#endif
     if (error) {
 #ifndef WIN32
         if (error == EAI_SYSTEM) {
