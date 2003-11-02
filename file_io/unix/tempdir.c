980998f8d8951d2cb60424e2134f76110c9d167c
Thom May <thommay@apache.org>
2003-11-02 00:06:24 +0000

    Bah. *BSD's mkstemp doesn't check how many (or whether you have any) Xs you have in the mkstemp format. GNU libc does.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64706 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/tempdir.c
+++ file_io/unix/tempdir.c
@@ -69,7 +69,7 @@ static char global_temp_dir[APR_PATH_MAX+1] = { 0 };
 static int test_tempdir(const char *temp_dir, apr_pool_t *p)
 {
     apr_file_t *dummy_file;
-    const char *path = apr_pstrcat(p, temp_dir, "/apr-tmp", NULL);
+    const char *path = apr_pstrcat(p, temp_dir, "/apr-tmp.XXXXXX", NULL);
 
     if (apr_file_mktemp(&dummy_file, (char *)path, 0, p) == APR_SUCCESS) {
         if (apr_file_putc('!', dummy_file) == APR_SUCCESS) {
