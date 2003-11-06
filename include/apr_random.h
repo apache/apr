b51aa13f4f56f0da7bd480829a35d2d5d608c8d6
Ben Laurie <ben@apache.org>
2003-11-06 00:25:33 +0000

    Get rid of tabs.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64733 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_random.h
+++ include/apr_random.h
@@ -61,9 +61,9 @@ typedef struct apr_crypto_hash_t apr_crypto_hash_t;
 
 typedef void apr_crypto_hash_init_t(apr_crypto_hash_t *hash);
 typedef void apr_crypto_hash_add_t(apr_crypto_hash_t *hash,const void *data,
-				   apr_size_t bytes);
+                                   apr_size_t bytes);
 typedef void apr_crypto_hash_finish_t(apr_crypto_hash_t *hash,
-				      unsigned char *result);
+                                      unsigned char *result);
 
 /* FIXME: make this opaque */
 struct apr_crypto_hash_t {
@@ -79,15 +79,15 @@ apr_crypto_hash_t *apr_crypto_sha256_new(apr_pool_t *p);
 typedef struct apr_random_t apr_random_t;
 
 void apr_random_init(apr_random_t *g,apr_pool_t *p,
-		     apr_crypto_hash_t *pool_hash,apr_crypto_hash_t *key_hash,
-		     apr_crypto_hash_t *prng_hash);
+                     apr_crypto_hash_t *pool_hash,apr_crypto_hash_t *key_hash,
+                     apr_crypto_hash_t *prng_hash);
 apr_random_t *apr_random_standard_new(apr_pool_t *p);
 void apr_random_add_entropy(apr_random_t *g,const void *entropy_,
-			    apr_size_t bytes);
+                            apr_size_t bytes);
 apr_status_t apr_random_insecure_bytes(apr_random_t *g,void *random,
-				       apr_size_t bytes);
+                                       apr_size_t bytes);
 apr_status_t apr_random_secure_bytes(apr_random_t *g,void *random,
-				     apr_size_t bytes);
+                                     apr_size_t bytes);
 void apr_random_barrier(apr_random_t *g);
 apr_status_t apr_random_secure_ready(apr_random_t *r);
 apr_status_t apr_random_insecure_ready(apr_random_t *r);
