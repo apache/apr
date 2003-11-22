9816ed9cb557f8a5091f0ba819a386458fef5601
David Reid <dreid@apache.org>
2003-11-22 10:53:46 +0000

    This small fix allows for the passing of NULL as the descriptors arg in apr_pollset_poll. Basically if we have a single item pollset then we don't need to have the descriptors arg set as the return value is sufficient.
    I haven't added this for OS/2 as Brian may be moving the code into a
    seperate file again.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64788 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/poll.c
+++ poll/unix/poll.c
@@ -542,7 +542,8 @@ APR_DECLARE(apr_status_t) apr_pollset_poll(apr_pollset_t *pollset,
             j++;
         }
     }
-    *descriptors = pollset->result_set;
+    if (descriptors)
+        *descriptors = pollset->result_set;
     return APR_SUCCESS;
 }
 
@@ -616,7 +617,8 @@ APR_DECLARE(apr_status_t) apr_pollset_poll(apr_pollset_t *pollset,
         }
     }
 
-    *descriptors = pollset->result_set;
+    if (descriptors)
+        *descriptors = pollset->result_set;
     return APR_SUCCESS;
 }
 
