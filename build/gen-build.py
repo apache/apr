125666d12bf070a1454f737b429ae5b2aca0f0d3
Greg Stein <gstein@apache.org>
2004-02-21 00:31:49 +0000

    Fix the generation of the build-outputs.mk file. That file must remain platform independent since it gets included with the tarball, and the tarball is targeted for all platforms.
    In this new scheme, the build-outputs.mk includes symbols named
    OBJECTS_<plat> which specifies the necessary object files for each platform.
    The apr_rules.mk(.in) file then pulls in the right symbol for the configured
    platform. Since apr-util directly uses apr_rules.mk, then it picks up the
    same logic with no change.
    
    The build.conf was altered to include a set of platform-independent files,
    and subdirectories which contain per-platform subdirectories. This was
    needed to help out the logic which selects object files based on whether a
    platform is present in each source subdir. (and if a platform subdir is not
    present, then "parent" gets used instead; e.g. use 'unix' if 'beos' is not
    in the subdir)
    
    Lastly, configure.in was updated to select the appropriate set of objects
    for the build.
    
    * build.conf:
      (paths): remove per-platform symbols
      (platform_dirs): new option to list the dirs which have platform subdirs
    
    * configure.in:
      - define and substitute OBJECTS_PLATFORM to reference the platform
        specific set of objects, which get defined in build-outputs.mk
    
    * build/apr_rules.mk.in:
      - define OBJECTS in terms of the substituted OBJECTS_PLATFORM symbol
    
    * build/gen-build.py:
      (PLATFORMS): the various platforms found in APR. unused, actually.
      (MAKE_PLATFORMS): the platforms which use a Makefile, and what default
        platform should be used if SUBDIR/<platform> is not present.
      (main): update logic to generate multiple OBJECTS_* symbols. this now
        includes logic to deal with subdirs which may not have a platform subdir
        for the platform in question, thus needing to default to another.
      (write_objects): some code factored out of main() to process a list of
        source files into dependencies lines in the makefile.
      (get_files): no more need to substitute for {platform}, and simplified
        some of the .split() stuff.
      (get_platform): no longer required. we generate for all platforms.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64914 13f79535-47bb-0310-9956-ffa450edef68

--- build/gen-build.py
+++ build/gen-build.py
@@ -18,13 +18,26 @@ import re
 
 #import ezt
 
+#
+# legal platforms: aix, beos, netware, os2, os390, unix, win32
+# 'make' users: aix, beos, os2, os390, unix
+#
+PLATFORMS = [ 'aix', 'beos', 'netware', 'os2', 'os390', 'unix', 'win32' ]
+MAKE_PLATFORMS = [
+  ('unix', None),
+  ('aix', 'unix'),
+  ('beos', 'unix'),
+  ('os2', 'unix'),
+  ('os390', 'unix'),
+  ]
+# note: MAKE_PLATFORMS is an ordered set. we want to generate unix symbols
+#       first, so that the later platforms can reference them.
+
 
 def main():
   parser = ConfigParser.ConfigParser()
   parser.read('build.conf')
 
-  dirs = { }
-  files = get_files(parser.get('options', 'paths'))
   headers = get_files(parser.get('options', 'headers'))
 
   # compute the relevant headers, along with the implied includes
@@ -40,22 +53,45 @@ def main():
   f = open('build-outputs.mk', 'w')
   f.write('# DO NOT EDIT. AUTOMATICALLY GENERATED.\n\n')
 
-  objects = [ ]
-  for file in files:
-    assert file[-2:] == '.c'
-    obj = file[:-2] + '.lo'
-    objects.append(obj)
+  # write out the platform-independent files
+  files = get_files(parser.get('options', 'paths'))
+  objects, dirs = write_objects(f, legal_deps, h_deps, files)
+  f.write('\nOBJECTS_all = %s\n\n' % string.join(objects))
 
-    dirs[os.path.dirname(file)] = None
+  # for each platform and each subdirectory holding platform-specific files,
+  # write out their compilation rules, and an OBJECT_<subdir>_<plat> symbol.
+  for platform, parent in MAKE_PLATFORMS:
 
-    # what headers does this file include, along with the implied headers
-    deps = extract_deps(file, legal_deps)
-    for hdr in deps.keys():
-      deps.update(h_deps.get(hdr, {}))
+    # record the object symbols to build for each platform
+    group = [ '$(OBJECTS_all)' ]
 
-    f.write('%s: %s .make.dirs %s\n' % (obj, file, string.join(deps.values())))
+    for subdir in string.split(parser.get('options', 'platform_dirs')):
+      path = '%s/%s' % (subdir, platform)
+      if not os.path.exists(path):
+        # this subdir doesn't have a subdir for this platform, so we'll
+        # use the parent-platform's set of symbols
+        if parent:
+          group.append('$(OBJECTS_%s_%s)' % (subdir, parent))
+        continue
 
-  f.write('\nOBJECTS = %s\n\n' % string.join(objects))
+      # remember that this directory has files/objects
+      dirs[path] = None
+
+      # write out the compilation lines for this subdir
+      files = get_files(path + '/*.c')
+      objects, _unused = write_objects(f, legal_deps, h_deps, files)
+
+      symname = 'OBJECTS_%s_%s' % (subdir, platform)
+
+      # and write the symbol for the whole group
+      f.write('\n%s = %s\n\n' % (symname, string.join(objects)))
+
+      # and include that symbol in the group
+      group.append('$(%s)' % symname)
+
+    # write out a symbol which contains the necessary files
+    f.write('OBJECTS_%s = %s\n\n' % (platform, string.join(group)))
+
   f.write('HEADERS = $(top_srcdir)/%s\n\n' % string.join(headers, ' $(top_srcdir)/'))
   f.write('SOURCE_DIRS = %s $(EXTRA_SOURCE_DIRS)\n\n' % string.join(dirs.keys()))
 
@@ -75,6 +111,28 @@ def main():
   f.write('.make.dirs: $(srcdir)/build-outputs.mk\n' \
           '\t@for d in $(BUILD_DIRS); do test -d $$d || mkdir $$d; done\n' \
           '\t@echo timestamp > $@\n')
+
+
+def write_objects(f, legal_deps, h_deps, files):
+  dirs = { }
+  objects = [ ]
+
+  for file in files:
+    assert file[-2:] == '.c'
+    obj = file[:-2] + '.lo'
+    objects.append(obj)
+
+    dirs[os.path.dirname(file)] = None
+
+    # what headers does this file include, along with the implied headers
+    deps = extract_deps(file, legal_deps)
+    for hdr in deps.keys():
+      deps.update(h_deps.get(hdr, {}))
+
+    f.write('%s: %s .make.dirs %s\n' % (obj, file, string.join(deps.values())))
+
+  return objects, dirs
+
 
 def extract_deps(fname, legal_deps):
   "Extract the headers this file includes."
@@ -104,15 +162,10 @@ def resolve_deps(header_deps):
 
 
 def get_files(patterns):
-  patterns = string.replace(patterns, '{platform}', get_platform())
-  patterns = string.split(string.strip(patterns))
   files = [ ]
-  for pat in patterns:
+  for pat in string.split(patterns):
     files.extend(glob.glob(pat))
   return files
-
-def get_platform():
-  return 'unix'
 
 
 if __name__ == '__main__':
