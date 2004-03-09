ecbe0f5d787a288e718f6000ea17ff48b1350181
Bradley Nicholes <bnicholes@apache.org>
2004-03-09 23:46:54 +0000

    Allow support for random bytes on older hardware
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64957 13f79535-47bb-0310-9956-ffa450edef68

--- misc/netware/rand.c
+++ misc/netware/rand.c
@@ -22,10 +22,50 @@
 
 #include <nks/plat.h>
 
+static int NXSeedRandomInternal( size_t width, void *seed )
+{
+    static int init = 0;
+    int                        *s = (int *) seed;
+    union { int x; char y[4]; } u;
+
+    if (!init) {
+        srand(NXGetSystemTick());
+        init = 1;
+    }
+ 
+    if (width > 3)
+    {
+        do
+        {
+            *s++ = rand();
+        }
+        while ((width -= 4) > 3);
+    }
+ 
+    if (width > 0)
+    {
+        char *p = (char *) s;
+
+        u.x = rand();
+ 
+        while (width > 0)
+           *p++ = u.y[width--];
+    }
+ 
+    return APR_SUCCESS;
+}
+
 APR_DECLARE(apr_status_t) apr_generate_random_bytes(unsigned char *buf, 
                                                     apr_size_t length)
 {
-    return NXSeedRandom(length, buf);
+    int ret;
+     
+    if (NXSeedRandom(length, buf) != 0) {
+        return NXSeedRandomInternal (length, buf);
+    }
+    return APR_SUCCESS;
 }
+
+
 
 #endif /* APR_HAS_RANDOM */
