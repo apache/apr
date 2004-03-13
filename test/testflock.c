78996eb3108bb5cf0e9ca01e611e81f6fe9395d4
Ryan Bloom <rbb@apache.org>
2004-03-13 12:34:49 +0000

    Migrate testflock to unified test framework
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@64977 13f79535-47bb-0310-9956-ffa450edef68

--- test/testflock.c
+++ test/testflock.c
@@ -13,143 +13,94 @@
  * limitations under the License.
  */
 
-/*
- * USAGE
- *
- * Start one process, no args, and place it into the background. Start a
- * second process with the "-r" switch to attempt a read on the file
- * created by the first process.
- *
- * $ ./testflock &
- * ...messages...
- * $ ./testflock -r
- * ...messages...
- *
- * The first process will sleep for 30 seconds while holding a lock. The
- * second process will attempt to grab it (non-blocking) and fail. It
- * will then grab it with a blocking scheme. When the first process' 30
- * seconds are up, it will exit (thus releasing its lock). The second
- * process will acquire the lock, then exit.
- */
-
+#include "testflock.h"
+#include "test_apr.h"
 #include "apr_pools.h"
+#include "apr_thread_proc.h"
 #include "apr_file_io.h"
-#include "apr_time.h"
+#include "apr_file_info.h"
 #include "apr_general.h"
-#include "apr_getopt.h"
 #include "apr_strings.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+/* XXX I'm sure there has to be a better way to do this ... */
+#ifdef WIN32
+#define EXTENSION ".exe"
+#elif NETWARE
+#define EXTENSION ".nlm"
+#else
+#define EXTENSION
+#endif
 
-const char *testfile = "testfile.tmp";
 
-static apr_pool_t *pool = NULL;
-
-static void errmsg(const char *msg)
+static int launch_reader(CuTest *tc)
 {
-    if (pool != NULL)
-        apr_pool_destroy(pool);
-    fprintf(stderr, msg);
-    exit(1);
+    int i;
+    apr_proc_t proc = {0};
+    apr_procattr_t *procattr;
+    const char *args[2];
+    apr_status_t rv;
+    apr_exit_why_e why;
+    int exitcode;
+
+    rv = apr_procattr_create(&procattr, p);
+    apr_assert_success(tc, "Couldn't create procattr", rv);
+
+    rv = apr_procattr_io_set(procattr, APR_NO_PIPE, APR_NO_PIPE,
+            APR_NO_PIPE);
+    apr_assert_success(tc, "Couldn't set io in procattr", rv);
+
+    rv = apr_procattr_error_check_set(procattr, 1);
+    apr_assert_success(tc, "Couldn't set error check in procattr", rv);
+
+    args[0] = "tryread" EXTENSION;
+    args[1] = NULL;
+    rv = apr_proc_create(&proc, "./tryread" EXTENSION, args, NULL, procattr, p);
+    apr_assert_success(tc, "Couldn't launch program", rv);
+
+    CuAssert(tc, "wait for child process",
+            apr_proc_wait(&proc, &exitcode, &why, APR_WAIT) == APR_CHILD_DONE);
+
+    CuAssert(tc, "child terminated normally", why == APR_PROC_EXIT);
+    return exitcode;
 }
 
-static void errmsg2(const char *msg, apr_status_t rv)
-{
-    char *newmsg;
-    char errstr[120];
-
-    apr_strerror(rv, errstr, sizeof errstr);
-    newmsg = apr_psprintf(pool, "%s: %s (%d)\n",
-                          msg, errstr, rv);
-    errmsg(newmsg);
-    exit(1);
-}
-
-static void do_read(void)
-{
-    apr_file_t *file;
-    apr_status_t status;
-
-    if (apr_file_open(&file, testfile, APR_WRITE,
-                 APR_OS_DEFAULT, pool) != APR_SUCCESS)
-        errmsg("Could not open test file.\n");
-    printf("Test file opened.\n");
-
-    status = apr_file_lock(file, APR_FLOCK_EXCLUSIVE | APR_FLOCK_NONBLOCK);
-    if (!APR_STATUS_IS_EAGAIN(status)) {
-        char msg[200];
-        errmsg(apr_psprintf(pool, "Expected APR_EAGAIN. Got %d: %s.\n",
-                            status, apr_strerror(status, msg, sizeof(msg))));
-    }
-    printf("First attempt: we were properly locked out.\nWaiting for lock...");
-    fflush(stdout);
-
-    if (apr_file_lock(file, APR_FLOCK_EXCLUSIVE) != APR_SUCCESS)
-        errmsg("Could not establish lock on test file.");
-    printf(" got it.\n");
-
-    (void) apr_file_close(file);
-    printf("Exiting.\n");
-}
-
-static void do_write(void)
+static void test_withlock(CuTest *tc)
 {
     apr_file_t *file;
     apr_status_t rv;
+    int code;
+    
+    rv = apr_file_open(&file, TESTFILE, APR_WRITE|APR_CREATE, 
+                       APR_OS_DEFAULT, p);
+    apr_assert_success(tc, "Could not create file.", rv);
+    CuAssertPtrNotNull(tc, file);
 
-    if (apr_file_open(&file, testfile, APR_WRITE|APR_CREATE, APR_OS_DEFAULT,
-                 pool) != APR_SUCCESS)
-        errmsg("Could not create file.\n");
-    printf("Test file created.\n");
+    rv = apr_file_lock(file, APR_FLOCK_EXCLUSIVE);
+    apr_assert_success(tc, "Could not lock the file.", rv);
+    CuAssertPtrNotNull(tc, file);
 
-    if ((rv = apr_file_lock(file, APR_FLOCK_EXCLUSIVE)) != APR_SUCCESS)
-        errmsg2("Could not lock the file", rv);
-    printf("Lock created.\nSleeping...");
-    fflush(stdout);
-
-    apr_sleep(apr_time_from_sec(30));
+    code = launch_reader(tc);
+    CuAssertIntEquals(tc, FAILED_READ, code);
 
     (void) apr_file_close(file);
-    printf(" done.\nExiting.\n");
 }
 
-int main(int argc, const char * const *argv)
+static void test_withoutlock(CuTest *tc)
 {
-    int reader = 0;
-    apr_status_t status;
-    char optchar;
-    const char *optarg;
-    apr_getopt_t *opt;
+    apr_file_t *file;
+    apr_status_t rv;
+    int code;
+    
+    code = launch_reader(tc);
+    CuAssertIntEquals(tc, SUCCESSFUL_READ, code);
+}
 
-    if (apr_initialize() != APR_SUCCESS)
-        errmsg("Could not initialize APR.\n");
-    atexit(apr_terminate);
+CuSuite *testflock(void)
+{
+    CuSuite *suite = CuSuiteNew("Flock");
 
-    if (apr_pool_create(&pool, NULL) != APR_SUCCESS)
-        errmsg("Could not create global pool.\n");
+    SUITE_ADD_TEST(suite, test_withlock);
+    SUITE_ADD_TEST(suite, test_withoutlock);
 
-    if (apr_getopt_init(&opt, pool, argc, argv) != APR_SUCCESS)
-        errmsg("Could not parse options.\n");
-
-    while ((status = apr_getopt(opt, "rf:", &optchar, &optarg)) == APR_SUCCESS) {
-        if (optchar == 'r')
-            ++reader;
-        else if (optchar == 'f')
-            testfile = optarg;
-    }
-    if (status != APR_SUCCESS && status != APR_EOF) {
-        char msgbuf[80];
-
-        fprintf(stderr, "error: %s\n",
-                apr_strerror(status, msgbuf, sizeof msgbuf));
-        exit(1);
-    }
-
-    if (reader)
-        do_read();
-    else
-        do_write();
-
-    return 0;
+    return suite;
 }
