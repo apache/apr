0a074a5b0f093094a1620dff2c9e5fe973e6871f
Justin Erenkrantz <jerenkrantz@apache.org>
2004-06-06 21:19:19 +0000

    Add libtool* as installed by FreeBSD ports.  No 'libtool' is installed, all are suffixed by version number.  Prefer 1.5 over 1.4, if available.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@65170 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -38,7 +38,7 @@ fi
 # output is multiline from 1.5 onwards
 
 # Require libtool 1.3.3 or newer
-libtool=`build/PrintPath glibtool libtool`
+libtool=`build/PrintPath glibtool libtool libtool15 libtool14`
 lt_pversion=`$libtool --version 2>/dev/null|sed -e 's/([^)]*)//g;s/^[^0-9]*//;s/[- ].*//g;q'`
 if test -z "$lt_pversion"; then
 echo "buildconf: libtool not found."
