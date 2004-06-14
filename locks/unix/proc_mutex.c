5d1fdb1550fb162aeb7d73349b282c19b4d33ea8
Joe Orton <jorton@apache.org>
2004-06-14 11:35:53 +0000

    * locks/unix/proc_mutex.c (proc_mutex_posix_release): Revert previous change, move ->curr_locked manipulation back inside critical section.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@65192 13f79535-47bb-0310-9956-ffa450edef68

--- locks/unix/proc_mutex.c
+++ locks/unix/proc_mutex.c
@@ -126,10 +126,12 @@ static apr_status_t proc_mutex_posix_acquire(apr_proc_mutex_t *mutex)
 
 static apr_status_t proc_mutex_posix_release(apr_proc_mutex_t *mutex)
 {
+    mutex->curr_locked = 0;
     if (sem_post(mutex->psem_interproc) < 0) {
+        /* any failure is probably fatal, so no big deal to leave
+         * ->curr_locked at 0. */
         return errno;
     }
-    mutex->curr_locked = 0;
     return APR_SUCCESS;
 }
 
