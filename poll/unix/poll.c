85782b3d60082689283c041c0d134e51fca0333f
Paul Querna <pquerna@apache.org>
2004-11-19 23:53:52 +0000

    * poll/unix/poll.c: Remove Trailing Whitespace. No Functional Changes.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@105899 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/poll.c
+++ poll/unix/poll.c
@@ -107,17 +107,17 @@ static apr_int16_t get_event(apr_int16_t event)
     apr_int16_t rv = 0;
 
     if (event & APR_POLLIN)
-        rv |= POLLIN;        
+        rv |= POLLIN;
     if (event & APR_POLLPRI)
-        rv |= POLLPRI;        
+        rv |= POLLPRI;
     if (event & APR_POLLOUT)
-        rv |= POLLOUT;       
+        rv |= POLLOUT;
     if (event & APR_POLLERR)
-        rv |= POLLERR;        
+        rv |= POLLERR;
     if (event & APR_POLLHUP)
-        rv |= POLLHUP;        
+        rv |= POLLHUP;
     if (event & APR_POLLNVAL)
-        rv |= POLLNVAL;        
+        rv |= POLLNVAL;
 
     return rv;
 }
