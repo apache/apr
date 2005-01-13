75c84c4ad6c9ab454eb3efa5702d67d81d923382
Joe Orton <jorton@apache.org>
2005-01-13 10:48:22 +0000

    * configure.in: Require autoconf 2.50.  Use AC_CHECK_MEMBERS to check for struct tm members tm_gmtoff and __tm_gmtoff (the check for __tm_gmtoff seemed to be missing!).
    * time/unix/time.c: Define NO_GMTOFF_IN_STRUCT_TM if neither member is found.
    (get_offset, apr_os_exp_time_get, apr_os_exp_time_put): Use new macro names.
    (apr_unix_setup_time): Use NO_GMTOFF_IN_STRUCT_TM.
    
    * time/unix/timestr.c (apr_strftime): Use new macro names.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@125058 13f79535-47bb-0310-9956-ffa450edef68

--- time/unix/timestr.c
+++ time/unix/timestr.c
@@ -1,4 +1,4 @@
-/* Copyright 2000-2004 The Apache Software Foundation
+/* Copyright 2000-2005 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -142,9 +142,9 @@ apr_status_t apr_strftime(char *s, apr_size_t *retsize, apr_size_t max,
     tm.tm_wday = xt->tm_wday;
     tm.tm_yday = xt->tm_yday;
     tm.tm_isdst = xt->tm_isdst;
-#if defined(HAVE_GMTOFF)
+#if defined(HAVE_STRUCT_TM_TM_GMTOFF)
     tm.tm_gmtoff = xt->tm_gmtoff;
-#elif defined(HAVE___OFFSET)
+#elif defined(HAVE_STRUCT_TM___TM_GMTOFF)
     tm.__tm_gmtoff = xt->tm_gmtoff;
 #endif
     (*retsize) = strftime(s, max, format, &tm);
