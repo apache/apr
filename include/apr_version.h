17d0094b4084532d5f8a10895594215a3cc4cf38
Paul Querna <pquerna@apache.org>
2005-01-21 04:18:38 +0000

    bump /trunk/ to 1.2.0-dev
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@125887 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_version.h
+++ include/apr_version.h
@@ -57,7 +57,7 @@ extern "C" {
  * Minor API changes that do not cause binary compatibility problems.
  * Should be reset to 0 when upgrading APR_MAJOR_VERSION
  */
-#define APR_MINOR_VERSION       1
+#define APR_MINOR_VERSION       2
 
 /** patch level */
 #define APR_PATCH_VERSION       0
