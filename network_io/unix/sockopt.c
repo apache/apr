81f32b5bdd79ff76d10f475bde6673eab874f429
Wilfredo Sanchez <wsanchez@apache.org>
2005-05-24 23:59:02 +0000

    clean up a comment
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@178326 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockopt.c
+++ network_io/unix/sockopt.c
@@ -76,11 +76,12 @@ apr_status_t apr_socket_timeout_set(apr_socket_t *sock, apr_interval_time_t t)
 {
     apr_status_t stat;
 
-    /* If our timeout is positive or zero and our last timeout was
+    /* If our new timeout is non-negative and our old timeout was
      * negative, then we need to ensure that we are non-blocking.
-     * Conversely, if our timeout is negative and we had a positive
-     * or zero timeout, we must make sure our socket is blocking.
-     * We want to avoid calling fcntl more than necessary on the socket,
+     * Conversely, if our new timeout is negative and we had
+     * non-negative timeout, we must make sure our socket is blocking.
+     * We want to avoid calling fcntl more than necessary on the
+     * socket.
      */
     if (t >= 0 && sock->timeout < 0) {
         if (apr_is_option_set(sock, APR_SO_NONBLOCK) != 1) {
