522f987a08505f64487572ce3f7bacf7799e18e8
Joe Orton <jorton@apache.org>
2005-08-24 09:42:42 +0000

    Merge r239390, r239574 from trunk:
    * user/unix/userinfo.c (getpwnam_safe): Fix error handling; always use
    the getpwnam_r return value as the error code, and ignore errno, since
    POSIX does not require that getpwnam_r sets errno.
    
    * user/unix/groupinfo.c (apr_gid_name_get, apr_gid_get): Fix error
    handling as above; and check for the NULL -> "no entry" cases here
    too.
    
    * test/testuser.c (fail_userinfo): Add test cases for error handling
    (only one of them actually trips on the bugs in the old code with
    glibc).
    
    * user/unix/userinfo.c (getpwnam_safe, apr_uid_name_get): Fix error
    handling for platforms which do not set errno on non-threadsafe
    get{pw,gr}* failures; always return APR_ENOENT for that case.
    
    * user/unix/groupinfo.c (apr_gid_name_get, apr_gid_get): Likewise.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@239589 13f79535-47bb-0310-9956-ffa450edef68

--- user/unix/userinfo.c
+++ user/unix/userinfo.c
@@ -38,21 +38,31 @@ static apr_status_t getpwnam_safe(const char *username,
 {
     struct passwd *pwptr;
 #if APR_HAS_THREADS && defined(_POSIX_THREAD_SAFE_FUNCTIONS) && defined(HAVE_GETPWNAM_R)
-    /* IRIX getpwnam_r() returns 0 and sets pwptr to NULL on failure */
-    if (!getpwnam_r(username, pw, pwbuf, PWBUF_SIZE, &pwptr) && pwptr) {
-        /* nothing extra to do on success */
+    apr_status_t rv;
+
+    /* POSIX defines getpwnam_r() et al to return the error number
+     * rather than set errno, and requires pwptr to be set to NULL if
+     * the entry is not found, imply that "not found" is not an error
+     * condition; some implementations do return 0 with pwptr set to
+     * NULL. */
+    rv = getpwnam_r(username, pw, pwbuf, PWBUF_SIZE, &pwptr);
+    if (rv) {
+        return rv;
+    }
+    if (pwptr == NULL) {
+        return APR_ENOENT;
+    }
 #else
+    /* Some platforms (e.g. FreeBSD 4.x) do not set errno on NULL "not
+     * found" return values for the non-threadsafe function either. */
+    errno = 0;
     if ((pwptr = getpwnam(username)) != NULL) {
         memcpy(pw, pwptr, sizeof *pw);
-#endif
     }
     else {
-        if (errno == 0) {
-            /* this can happen with getpwnam() on FreeBSD 4.3 */
-            return APR_EGENERAL;
-        }
-        return errno;
+        return errno ? errno : APR_ENOENT;
     }
+#endif
     return APR_SUCCESS;
 }
 
@@ -126,8 +136,9 @@ APR_DECLARE(apr_status_t) apr_uid_name_get(char **username, apr_uid_t userid,
     }
 
 #else
+    errno = 0;
     if ((pw = getpwuid(userid)) == NULL) {
-        return errno;
+        return errno ? errno : APR_ENOENT;
     }
 #endif
     *username = apr_pstrdup(p, pw->pw_name);
