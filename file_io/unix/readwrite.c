3ec3058305bbe7b45870cf833f671a863168ef2d
Joe Orton <jorton@apache.org>
2005-09-02 12:22:45 +0000

    Merge r234013, r239221 from trunk:
    * file_io/unix/readwrite.c (apr_file_write): Catch apr_file_flush()
    failure for buffered files.
    (apr_file_read): Handle the apr_file_flush() return value when
    flushing buffered writes.
    
    * test/testfile.c (test_fail_write_flush, test_fail_read_flush): Add
    test cases.
    
    Submitted by: Erik Huelsmann <ehuels gmail.com>, jorton
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/0.9.x@267192 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -50,7 +50,15 @@ APR_DECLARE(apr_status_t) apr_file_read(apr_file_t *thefile, void *buf, apr_size
 #endif
 
         if (thefile->direction == 1) {
-            apr_file_flush(thefile);
+            rv = apr_file_flush(thefile);
+            if (rv) {
+#if APR_HAS_THREADS
+                if (thefile->thlock) {
+                    apr_thread_mutex_unlock(thefile->thlock);
+                }
+#endif
+                return rv;
+            }
             thefile->bufpos = 0;
             thefile->direction = 0;
             thefile->dataRead = 0;
@@ -173,7 +181,7 @@ APR_DECLARE(apr_status_t) apr_file_write(apr_file_t *thefile, const void *buf, a
         rv = 0;
         while (rv == 0 && size > 0) {
             if (thefile->bufpos == APR_FILE_BUFSIZE)   /* write buffer is full*/
-                apr_file_flush(thefile);
+                rv = apr_file_flush(thefile);
 
             blocksize = size > APR_FILE_BUFSIZE - thefile->bufpos ? 
                         APR_FILE_BUFSIZE - thefile->bufpos : size;
