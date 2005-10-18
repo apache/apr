111bd75ce10e93aced2e64c939e3fac1ff7ba6ce
Colm MacCarthaigh <colm@apache.org>
2005-10-18 15:14:44 +0000

    Initial implementation of variable file-io buffer sizes on the Netware platform; partially guesswork based - untested.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@326119 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/netware/apr_arch_file_io.h
+++ include/arch/netware/apr_arch_file_io.h
@@ -69,7 +69,9 @@
 
 /* End System headers */
 
-#define APR_FILE_BUFSIZE 4096
+#define APR_FILE_DEFAULT_BUFSIZE 4096
+/* For backwards compat */
+#define APR_FILE_BUFSIZE APR_FILE_DEFAULT_BUFSIZE
 
 #if APR_HAS_LARGE_FILES
 #define lseek(f,o,w) lseek64(f,o,w)
@@ -95,7 +97,8 @@ struct apr_file_t {
 
     /* Stuff for buffered mode */
     char *buffer;
-    int bufpos;               /* Read/Write position in buffer */
+    apr_size_t bufpos;    /* Read/Write position in buffer */
+    apr_size_t bufsize;   /* The buffer size */
     apr_off_t dataRead;   /* amount of valid data read into buffer */
     int direction;            /* buffer being used for 0 = read, 1 = write */
     apr_off_t filePtr;    /* position in file of handle */
