c378513c9a7856bf21da27d1bd647944d17a985c
Garrett Rooney <rooneg@apache.org>
2006-02-08 06:16:48 +0000

    Make apr_file_t::filePtr an apr_off_t on unix so that we avoid truncating the offset into the file when working with files over 2 gigabytes in size.
    * include/arch/unix/apr_arch_file_io.h
      (apr_file_t::filePtr): Make this an apr_off_t, since we really need the
       full 64 bits in many cases.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@375869 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/unix/apr_arch_file_io.h
+++ include/arch/unix/apr_arch_file_io.h
@@ -107,7 +107,7 @@ struct apr_file_t {
     apr_size_t bufsize;       /* The size of the buffer */
     unsigned long dataRead;   /* amount of valid data read into buffer */
     int direction;            /* buffer being used for 0 = read, 1 = write */
-    unsigned long filePtr;    /* position in file of handle */
+    apr_off_t filePtr;        /* position in file of handle */
 #if APR_HAS_THREADS
     struct apr_thread_mutex_t *thlock;
 #endif
