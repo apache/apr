a34e8abf342e13a8ead252e0bef52293eb2b277d
William A. Rowe Jr <wrowe@apache.org>
2006-03-10 22:55:49 +0000

    Identify - one 2Xtransformed error result, several missing mutex sections   and fix one last apr_file_flush result ignored (close the file, but return   the failure to flush error.)
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@384941 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/os2/readwrite.c
+++ file_io/os2/readwrite.c
@@ -150,6 +150,7 @@ APR_DECLARE(apr_status_t) apr_file_write(apr_file_t *thefile, const void *buf, a
 
         while (rc == 0 && size > 0) {
             if (thefile->bufpos == thefile->bufsize)   // write buffer is full
+                /* XXX bug; - rc is double-transformed os->apr below */
                 rc = apr_file_flush(thefile);
 
             blocksize = size > thefile->bufsize - thefile->bufpos ? thefile->bufsize - thefile->bufpos : size;
