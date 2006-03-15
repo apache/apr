847ad001f97d06aef7700fc53dacec01d77ac6b6
Garrett Rooney <rooneg@apache.org>
2006-03-15 19:59:51 +0000

    Fix a longstanding missunderstanding in our kqueue pollset code.  It turns out that the kqueue filter types are not bitfields, so checking for them via & EVFILT_READ or & EVFILT_WRITE is inappropriate.
    Test Fixes By: Joe Orton
    
    * poll/unix/kqueue.c
      (get_kqueue_revent): Use == instead of & when testing for filter types.
    
    * test/testpoll.c
      (multi_event_pollset): Handle the fact that we can sometimes get multiple
       events for a single socket.
    
    * CHANGES: Note fix.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@386154 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/kqueue.c
+++ poll/unix/kqueue.c
@@ -22,9 +22,9 @@ static apr_int16_t get_kqueue_revent(apr_int16_t event, apr_int16_t flags)
 {
     apr_int16_t rv = 0;
 
-    if (event & EVFILT_READ)
+    if (event == EVFILT_READ)
         rv |= APR_POLLIN;
-    if (event & EVFILT_WRITE)
+    if (event == EVFILT_WRITE)
         rv |= APR_POLLOUT;
     if (flags & EV_EOF)
         rv |= APR_POLLHUP;
