09c8fedf9f667eace2ffc08863c8f33ce8e54351
William A. Rowe Jr <wrowe@apache.org>
2006-03-18 01:23:06 +0000

    Close a kernel layer segfault when the user attempts to lock   a critical section after DeleteCriticalSection() has been   invoked.  There is no protection in the lock/unlock kernel   code, so ensure we don't enter the critical section path at   all with deleted mutex, and fail instead by attempting to   wait on a bad handle, resulting in a less drastic failure.
    Backports: 386780
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@386781 13f79535-47bb-0310-9956-ffa450edef68

--- locks/win32/thread_mutex.c
+++ locks/win32/thread_mutex.c
@@ -28,6 +28,7 @@ static apr_status_t thread_mutex_cleanup(void *data)
     apr_thread_mutex_t *lock = data;
 
     if (lock->type == thread_mutex_critical_section) {
+        lock->type = -1;
         DeleteCriticalSection(&lock->section);
     }
     else {
@@ -60,8 +61,8 @@ APR_DECLARE(apr_status_t) apr_thread_mutex_create(apr_thread_mutex_t **mutex,
          * use a [slower] mutex object, instead.
          */
         IF_WIN_OS_IS_UNICODE {
-            (*mutex)->type = thread_mutex_critical_section;
             InitializeCriticalSection(&(*mutex)->section);
+            (*mutex)->type = thread_mutex_critical_section;
         }
 #endif
 #if APR_HAS_ANSI_FS
