82f2873f73279ebe81b5abc47a458579c52d382b
Garrett Rooney <rooneg@apache.org>
2006-09-25 16:32:02 +0000

    Fix apr_atomic_cas on platforms with 64 bit longs.
    This is being committed to 0.9.x directly because it is no longer applicable
    to the 1.x branches.
    
    Submitted by: Philip Martin <philip codematters.co.uk>
    
    * atomic/unix/apr_atomic.c
      (apr_atomic_cas): Truncate 64 bit argument values.
    
    * CHANGES: Note change.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/0.9.x@449724 13f79535-47bb-0310-9956-ffa450edef68

--- atomic/unix/apr_atomic.c
+++ atomic/unix/apr_atomic.c
@@ -123,23 +123,23 @@ int apr_atomic_dec(volatile apr_atomic_t *mem)
 #if !defined(apr_atomic_cas) && !defined(APR_OVERRIDE_ATOMIC_CAS)
 apr_uint32_t apr_atomic_cas(volatile apr_uint32_t *mem, long with, long cmp)
 {
-    long prev;
+    apr_uint32_t prev;
 #if APR_HAS_THREADS
     apr_thread_mutex_t *lock = hash_mutex[ATOMIC_HASH(mem)];
 
     if (apr_thread_mutex_lock(lock) == APR_SUCCESS) {
-        prev = *(long*)mem;
-        if (prev == cmp) {
-            *(long*)mem = with;
+        prev = *mem;
+        if (prev == (apr_uint32_t)cmp) {
+            *mem = (apr_uint32_t)with;
         }
         apr_thread_mutex_unlock(lock);
         return prev;
     }
-    return *(long*)mem;
+    return *mem;
 #else
-    prev = *(long*)mem;
-    if (prev == cmp) {
-        *(long*)mem = with;
+    prev = *mem;
+    if (prev == (apr_uint32_t)cmp) {
+        *mem = (apr_uint32_t)with;
     }
     return prev;
 #endif /* APR_HAS_THREADS */
