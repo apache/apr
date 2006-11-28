1839453517167e5f54ebef57e59da4285ed9a9d3
William A. Rowe Jr <wrowe@apache.org>
2006-11-28 21:31:34 +0000

    Sync OS2 unknown local_port flags to unix src.
    Reviewed by wrowe and jorton
    Partial backport of 480212
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/0.9.x@480216 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/os2/sockets.c
+++ network_io/os2/sockets.c
@@ -140,6 +140,10 @@ APR_DECLARE(apr_status_t) apr_socket_bind(apr_socket_t *sock,
         return APR_OS2_STATUS(sock_errno());
     else {
         sock->local_addr = sa;
+        /* XXX IPv6 - this assumes sin_port and sin6_port at same offset */
+        if (sock->local_addr->sa.sin.sin_port == 0) { /* no need for ntohs() when comparing w/ 0 */
+            sock->local_port_unknown = 1; /* kernel got us an ephemeral port */
+        }
         return APR_SUCCESS;
     }
 }
