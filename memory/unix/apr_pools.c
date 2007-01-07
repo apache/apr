9c0b5cc183c055a001ac3a93f992bf03dd5268b2
Joe Orton <jorton@apache.org>
2007-01-07 21:06:18 +0000

    * memory/unix/apr_pools.c (apr_pool_initialize): Fix possible crash with verbose debugging enabled.
    PR: 41063
    Submitted by: Peter Steiner <peter.steiner+apache hugwi.ch>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@493838 13f79535-47bb-0310-9956-ffa450edef68

--- memory/unix/apr_pools.c
+++ memory/unix/apr_pools.c
@@ -1233,6 +1233,7 @@ APR_DECLARE(apr_status_t) apr_pool_initialize(void)
     apr_status_t rv;
 #if (APR_POOL_DEBUG & APR_POOL_DEBUG_VERBOSE_ALL)
     char *logpath;
+    apr_file_t *debug_log = NULL;
 #endif
 
     if (apr_pools_initialized++)
@@ -1262,13 +1263,20 @@ APR_DECLARE(apr_status_t) apr_pool_initialize(void)
 #if (APR_POOL_DEBUG & APR_POOL_DEBUG_VERBOSE_ALL)
     rv = apr_env_get(&logpath, "APR_POOL_DEBUG_LOG", global_pool);
 
+    /* Don't pass file_stderr directly to apr_file_open() here, since
+     * apr_file_open() can call back to apr_pool_log_event() and that
+     * may attempt to use then then non-NULL but partially set up file
+     * object. */
     if (rv == APR_SUCCESS) {
-        apr_file_open(&file_stderr, logpath, APR_APPEND|APR_WRITE|APR_CREATE,
+        apr_file_open(&debug_log, logpath, APR_APPEND|APR_WRITE|APR_CREATE,
                       APR_OS_DEFAULT, global_pool);
     }
     else {
-        apr_file_open_stderr(&file_stderr, global_pool);
+        apr_file_open_stderr(&debug_log, global_pool);
     }
+
+    /* debug_log is now a file handle. */
+    file_stderr = debug_log;
 
     if (file_stderr) {
         apr_file_printf(file_stderr,
