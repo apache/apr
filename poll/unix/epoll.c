e00b4e278a2811ec7a680452c95ef4b6faf1324e
Greg Ames <gregames@apache.org>
2007-01-29 15:56:41 +0000

    reverting rev 500321 pending investigation of problem reported by jorton.
    This line, and those below, will be ignored--
    
    M    include/arch/unix/apr_arch_poll_private.h
    M    poll/unix/epoll.c
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@501085 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/epoll.c
+++ poll/unix/epoll.c
@@ -219,10 +219,18 @@ APR_DECLARE(apr_status_t) apr_pollset_remove(apr_pollset_t *pollset,
         pollset_lock_rings();
 
         if (!APR_RING_EMPTY(&(pollset->query_ring), pfd_elem_t, link)) {
-            ep = (pfd_elem_t *) descriptor;
+            for (ep = APR_RING_FIRST(&(pollset->query_ring));
+                 ep != APR_RING_SENTINEL(&(pollset->query_ring),
+                                         pfd_elem_t, link);
+                 ep = APR_RING_NEXT(ep, link)) {
                 
-            APR_RING_REMOVE(ep, link);
-            APR_RING_INSERT_TAIL(&(pollset->dead_ring), ep, pfd_elem_t, link);
+                if (descriptor->desc.s == ep->pfd.desc.s) {
+                    APR_RING_REMOVE(ep, link);
+                    APR_RING_INSERT_TAIL(&(pollset->dead_ring),
+                                         ep, pfd_elem_t, link);
+                    break;
+                }
+            }
         }
 
         pollset_unlock_rings();
