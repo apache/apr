5805df99c0aaf0ec85b7b4d88fc42620f9bd2245
Paul Querna <pquerna@apache.org>
2007-03-01 07:13:54 +0000

    Only try to flush when the file is in buffered mode, inside apr_file_writev.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@513205 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -239,15 +239,18 @@ APR_DECLARE(apr_status_t) apr_file_write(apr_file_t *thefile, const void *buf, a
 APR_DECLARE(apr_status_t) apr_file_writev(apr_file_t *thefile, const struct iovec *vec,
                                           apr_size_t nvec, apr_size_t *nbytes)
 {
-    apr_status_t rv = apr_file_flush(thefile);
+#ifdef HAVE_WRITEV
+    apr_ssize_t bytes;
+#endif
 
-    if (rv != APR_SUCCESS) {
-        return rv;
+    if (thefile->buffered) {
+        apr_status_t rv = apr_file_flush(thefile);
+        if (rv != APR_SUCCESS) {
+            return rv;
+        }
     }
 
 #ifdef HAVE_WRITEV
-    apr_ssize_t bytes;
-
     if ((bytes = writev(thefile->filedes, vec, nvec)) < 0) {
         *nbytes = 0;
         return errno;
