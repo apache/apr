fb31bd181976452d21d7d4b6407f152bf188d6c1
Bojan Smojver <bojan@apache.org>
2007-05-12 12:40:14 +0000

    Backport r537402 from the trunk. Avoid compiler warning (rv only used if HAVE_WRITEV).
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@537403 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -226,8 +226,8 @@ APR_DECLARE(apr_status_t) apr_file_write(apr_file_t *thefile, const void *buf, a
 APR_DECLARE(apr_status_t) apr_file_writev(apr_file_t *thefile, const struct iovec *vec,
                                           apr_size_t nvec, apr_size_t *nbytes)
 {
-    apr_status_t rv;
 #ifdef HAVE_WRITEV
+    apr_status_t rv;
     int bytes;
 
     file_lock(thefile);
