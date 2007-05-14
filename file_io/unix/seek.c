1288f59efa2a138ea3939b99a735c0ed45322a57
Bojan Smojver <bojan@apache.org>
2007-05-14 23:45:20 +0000

    Avoid calling apr_file_flush_locked() on an unlocked file.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@538019 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/seek.c
+++ file_io/unix/seek.c
@@ -101,5 +101,5 @@ apr_status_t apr_file_trunc(apr_file_t *fp, apr_off_t offset)
     if (ftruncate(fp->filedes, offset) == -1) {
         return errno;
     }
-    return setptr(fp, offset);
+    return apr_file_seek(fp, APR_SET, &offset);
 }
