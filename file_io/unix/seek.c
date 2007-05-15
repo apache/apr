abe66bc69b25a96726ea014d12ed838587cef47a
Bojan Smojver <bojan@apache.org>
2007-05-15 00:36:40 +0000

    Backport r538009 and r538019 from the trunk. Revert nested mutexes in Unix file_io.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@538029 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/seek.c
+++ file_io/unix/seek.c
@@ -22,7 +22,7 @@ static apr_status_t setptr(apr_file_t *thefile, apr_off_t pos )
     apr_status_t rv;
 
     if (thefile->direction == 1) {
-        rv = apr_file_flush(thefile);
+        rv = apr_file_flush_locked(thefile);
         if (rv) {
             return rv;
         }
@@ -71,7 +71,7 @@ APR_DECLARE(apr_status_t) apr_file_seek(apr_file_t *thefile, apr_seek_where_t wh
             break;
 
         case APR_END:
-            rc = apr_file_info_get(&finfo, APR_FINFO_SIZE, thefile);
+            rc = apr_file_info_get_locked(&finfo, APR_FINFO_SIZE, thefile);
             if (rc == APR_SUCCESS)
                 rc = setptr(thefile, finfo.size + *offset);
             break;
@@ -101,5 +101,5 @@ apr_status_t apr_file_trunc(apr_file_t *fp, apr_off_t offset)
     if (ftruncate(fp->filedes, offset) == -1) {
         return errno;
     }
-    return setptr(fp, offset);
+    return apr_file_seek(fp, APR_SET, &offset);
 }
