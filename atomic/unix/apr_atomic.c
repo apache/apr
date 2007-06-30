86e6639a164798a2b7625317a287912b02a4ee95
Davi Arnaut <davi@apache.org>
2007-06-30 15:34:13 +0000

    Backport 552161 from trunk:
    Avoid overwriting the hash_mutex table for applications that incorrectly calls
    apr_atomic_init().
    
    Noticied by: Tim Jones
    PR: 42760
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@552163 13f79535-47bb-0310-9956-ffa450edef68

--- atomic/unix/apr_atomic.c
+++ atomic/unix/apr_atomic.c
@@ -171,12 +171,28 @@ APR_DECLARE(apr_uint32_t) apr_atomic_add32(volatile apr_uint32_t *mem,
 static apr_thread_mutex_t **hash_mutex;
 #endif /* APR_HAS_THREADS */
 
+#if APR_HAS_THREADS
+static apr_status_t atomic_cleanup(void *data)
+{
+    if (hash_mutex == data)
+        hash_mutex = NULL;
+
+    return APR_SUCCESS;
+}
+#endif
+
 apr_status_t apr_atomic_init(apr_pool_t *p)
 {
 #if APR_HAS_THREADS
     int i;
     apr_status_t rv;
+
+    if (hash_mutex != NULL)
+        return APR_SUCCESS;
+
     hash_mutex = apr_palloc(p, sizeof(apr_thread_mutex_t*) * NUM_ATOMIC_HASH);
+    apr_pool_cleanup_register(p, hash_mutex, atomic_cleanup,
+                              apr_pool_cleanup_null);
 
     for (i = 0; i < NUM_ATOMIC_HASH; i++) {
         rv = apr_thread_mutex_create(&(hash_mutex[i]),
