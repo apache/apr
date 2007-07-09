0c69d04472ae05023805e0ee2b333ab2a58d851a
Davi Arnaut <davi@apache.org>
2007-07-09 16:05:59 +0000

    Introduce macro definitions of the min/max characteristics of the apr integer types.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@554692 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/apr_private_common.h
+++ include/arch/apr_private_common.h
@@ -37,6 +37,5 @@ apr_status_t apr_filepath_list_merge_impl(char **liststr,
 /* temporary defines to handle 64bit compile mismatches */
 #define APR_INT_TRUNC_CAST    int
 #define APR_UINT32_TRUNC_CAST apr_uint32_t
-#define APR_UINT32_MAX        0xFFFFFFFFUL
 
 #endif  /*APR_PRIVATE_COMMON_H*/
