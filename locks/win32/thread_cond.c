f7c5f440868481e93e204c50ad8b46c235ad52be
Davi Arnaut <davi@apache.org>
2007-07-12 19:29:43 +0000

    Improve WIN32 condition variables fairness by using a generation count. The count assures that one thread won't steal wakeups from other threads in the queue.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@555730 13f79535-47bb-0310-9956-ffa450edef68

--- locks/win32/thread_cond.c
+++ locks/win32/thread_cond.c
@@ -67,27 +67,43 @@ static APR_INLINE apr_status_t _thread_cond_timedwait(apr_thread_cond_t *cond,
 {
     DWORD res;
     apr_status_t rv;
+    unsigned int wake = 0;
+    unsigned long generation;
 
     EnterCriticalSection(&cond->csection);
     cond->num_waiting++;
+    generation = cond->generation;
     LeaveCriticalSection(&cond->csection);
 
     apr_thread_mutex_unlock(mutex);
 
     do {
         res = WaitForSingleObject(cond->semaphore, timeout_ms);
+
         EnterCriticalSection(&cond->csection);
+
         if (cond->num_wake) {
-            cond->num_wake--;
-            rv = APR_SUCCESS;
-            break;
+            if (cond->generation != generation) {
+                cond->num_wake--;
+                cond->num_waiting--;
+                rv = APR_SUCCESS;
+                break;
+            } else {
+                wake = 1;
+            }
         }
         else if (res != WAIT_OBJECT_0) {
             cond->num_waiting--;
             rv = APR_TIMEUP;
             break;
         }
+
         LeaveCriticalSection(&cond->csection);
+
+        if (wake) {
+            wake = 0;
+            ReleaseSemaphore(cond->semaphore, 1, NULL);
+        }
     } while (1);
 
     LeaveCriticalSection(&cond->csection);
@@ -116,10 +132,10 @@ APR_DECLARE(apr_status_t) apr_thread_cond_signal(apr_thread_cond_t *cond)
     unsigned int wake = 0;
 
     EnterCriticalSection(&cond->csection);
-    if (cond->num_waiting) {
+    if (cond->num_waiting > cond->num_wake) {
         wake = 1;
         cond->num_wake++;
-        cond->num_waiting--;
+        cond->generation++;
     }
     LeaveCriticalSection(&cond->csection);
 
@@ -135,9 +151,10 @@ APR_DECLARE(apr_status_t) apr_thread_cond_broadcast(apr_thread_cond_t *cond)
     unsigned long num_wake = 0;
 
     EnterCriticalSection(&cond->csection);
-    if (cond->num_waiting) {
-        cond->num_wake += num_wake = cond->num_waiting;
-        cond->num_waiting = 0;
+    if (cond->num_waiting > cond->num_wake) {
+        num_wake = cond->num_waiting - cond->num_wake;
+        cond->num_wake = cond->num_waiting;
+        cond->generation++;
     }
     LeaveCriticalSection(&cond->csection);
 
