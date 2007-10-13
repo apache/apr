adf612981d787ffb739e80d068659979a1a81e54
Davi Arnaut <davi@apache.org>
2007-10-13 00:00:46 +0000

    Backport revision 584331 from trunk:
    The calls to UnmapViewOfFile() and CloseHandle() in the shm_cleanup()
    routine in the win32/shm.c source are not properly checking for errors.
    They currently assume a non-zero return is an error.Whereas these windows
    calls return non-zero on success and zero on failure.
    
    PR: 43065
    Submitted by: Joe Mudd
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.2.x@584341 13f79535-47bb-0310-9956-ffa450edef68

--- shmem/win32/shm.c
+++ shmem/win32/shm.c
@@ -40,10 +40,10 @@ static apr_status_t shm_cleanup(void* shm)
     apr_status_t rv = APR_SUCCESS;
     apr_shm_t *m = shm;
     
-    if (UnmapViewOfFile(m->memblk)) {
+    if (!UnmapViewOfFile(m->memblk)) {
         rv = apr_get_os_error();
     }
-    if (CloseHandle(m->hMap)) {
+    if (!CloseHandle(m->hMap)) {
         return (rv != APR_SUCCESS) ? rv : apr_get_os_error();
     }
     /* ### Do we want to make a point of unlinking m->file here? 
