e0eec35271edaa2cf7854442dc435e6ce81ecc81
Bojan Smojver <bojan@apache.org>
2008-05-28 23:33:00 +0000

    If the named resource was removed by apr_shm_remove(), it may not be there.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@661146 13f79535-47bb-0310-9956-ffa450edef68

--- shmem/unix/shm.c
+++ shmem/unix/shm.c
@@ -49,7 +49,12 @@ static apr_status_t shm_cleanup_owner(void *m_)
         if (munmap(m->base, m->realsize) == -1) {
             return errno;
         }
-        return apr_file_remove(m->filename, m->pool);
+        if (access(m->filename, F_OK)) {
+            return APR_SUCCESS;
+        }
+        else {
+            return apr_file_remove(m->filename, m->pool);
+        }
 #endif
 #if APR_USE_SHMEM_MMAP_SHM
         if (munmap(m->base, m->realsize) == -1) {
@@ -70,7 +75,12 @@ static apr_status_t shm_cleanup_owner(void *m_)
         if (shmdt(m->base) == -1) {
             return errno;
         }
-        return apr_file_remove(m->filename, m->pool);
+        if (access(m->filename, F_OK)) {
+            return APR_SUCCESS;
+        }
+        else {
+            return apr_file_remove(m->filename, m->pool);
+        }
 #endif
     }
 
