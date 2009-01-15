83d745f0294520310d772b27f94c3488c0993bed
Jeff Trawick <trawick@apache.org>
2009-01-15 13:55:04 +0000

    suppress gcc's warning about unexpected assignmet
    "gcc version 4.0.1 (Apple Inc. build 5490)"
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@734707 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/kqueue.c
+++ poll/unix/kqueue.c
@@ -336,7 +336,7 @@ APR_DECLARE(apr_status_t) apr_pollset_poll(apr_pollset_t *pollset,
                 j++;
             }
         }
-        if ((*num) = j)
+        if ((*num = j))
             rv = APR_SUCCESS;
         if (descriptors) {
             *descriptors = pollset->result_set;
