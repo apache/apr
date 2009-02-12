238d6346a6c916657a8241b19de3c4bc3a1f5827
Jim Jagielski <jim@apache.org>
2009-02-12 15:43:17 +0000

    with the intro of GNU libtool 2.x, many packages now install the 1.5.x version as glibtool1. So look for that
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@743783 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -38,7 +38,7 @@ fi
 # output is multiline from 1.5 onwards
 
 # Require libtool 1.4 or newer
-libtool=`build/PrintPath glibtool libtool libtool15 libtool14`
+libtool=`build/PrintPath glibtool1 glibtool libtool libtool15 libtool14`
 lt_pversion=`$libtool --version 2>/dev/null|sed -e 's/([^)]*)//g;s/^[^0-9]*//;s/[- ].*//g;q'`
 if test -z "$lt_pversion"; then
 echo "buildconf: libtool not found."
