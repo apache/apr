29d79f2bef34ad7882d7be60c3a637fbde2bf801
Jeff Trawick <trawick@apache.org>
2009-03-03 00:29:22 +0000

    pollset tweaks:   axe logic to set ignored conditions in the poll request structures   (these conditions are return-only and are always reported when   they occur)
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@749490 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/poll.c
+++ poll/unix/poll.c
@@ -39,12 +39,7 @@ static apr_int16_t get_event(apr_int16_t event)
         rv |= POLLPRI;
     if (event & APR_POLLOUT)
         rv |= POLLOUT;
-    if (event & APR_POLLERR)
-        rv |= POLLERR;
-    if (event & APR_POLLHUP)
-        rv |= POLLHUP;
-    if (event & APR_POLLNVAL)
-        rv |= POLLNVAL;
+    /* POLLERR, POLLHUP, and POLLNVAL aren't valid as requested events */
 
     return rv;
 }
