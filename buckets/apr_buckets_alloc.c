2c146776bb5401ad30c1577b3b2b1b7de6850b5b
Joe Orton <jorton@apache.org>
2009-03-25 19:16:28 +0000

    * buckets/apr_buckets_alloc.c (alloc_cleanup): Unconditionally   destroy the allocator since it's unconditionally created.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@758414 13f79535-47bb-0310-9956-ffa450edef68

--- buckets/apr_buckets_alloc.c
+++ buckets/apr_buckets_alloc.c
@@ -47,11 +47,7 @@ static apr_status_t alloc_cleanup(void *data)
 
     apr_allocator_free(list->allocator, list->blocks);
 
-#if APR_POOL_DEBUG
-    if (list->pool && list->allocator != apr_pool_allocator_get(list->pool)) {
-        apr_allocator_destroy(list->allocator);
-    }
-#endif
+    apr_allocator_destroy(list->allocator);
 
     return APR_SUCCESS;
 }
