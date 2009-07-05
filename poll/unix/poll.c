148928992b46618840efca4f6d3abdd4498ddb5c
Paul Querna <pquerna@apache.org>
2009-07-05 23:00:40 +0000

    For poll() backend of pollcb API, return ENOTIMPL if APR was compiled with threads.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@791333 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/poll.c
+++ poll/unix/poll.c
@@ -304,6 +304,10 @@ static apr_status_t impl_pollcb_create(apr_pollcb_t *pollcb,
                                        apr_pool_t *p,
                                        apr_uint32_t flags)
 {
+#if APR_HAS_THREADS
+  return APR_ENOTIMPL;
+#endif
+
     pollcb->fd = -1;
 #ifdef WIN32
     if (!APR_HAVE_LATE_DLL_FUNC(WSAPoll)) {
