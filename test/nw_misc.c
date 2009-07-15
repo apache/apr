8dbeaf218c6a1156ddb198f7d58e5a48626901ad
Guenter Knauf <fuankg@apache.org>
2009-07-15 18:56:38 +0000

    replaced pressanykey() since it blocks the NetWare loader when called inside _NonAppStop().
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@794365 13f79535-47bb-0310-9956-ffa450edef68

--- test/nw_misc.c
+++ test/nw_misc.c
@@ -1,11 +1,21 @@
+#include <stdlib.h>
 #include <netware.h>
 #include <screen.h>
+/*
 #include "testutil.h"
+*/
 
+/* function to keep the screen open if not launched from bash */
 void _NonAppStop( void )
 {
-  if (getenv("_IN_NETWARE_BASH_") == NULL)
-    pressanykey();
+  if (getenv("_IN_NETWARE_BASH_") == NULL) {
+    uint16_t row, col;
+
+    GetScreenSize(&row, &col);
+    gotorowcol(row-1, 0);
+    printf("<Press any key to close screen> ");
+    getcharacter();
+  }
 }
 
 /*
