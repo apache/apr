6f65d83dbf0c9c19381e76b9147a747f5d170fd8
Guenter Knauf <fuankg@apache.org>
2009-10-05 17:21:16 +0000

    improved NetWare build of test samples.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@821929 13f79535-47bb-0310-9956-ffa450edef68

--- test/nw_misc.c
+++ test/nw_misc.c
@@ -1,6 +1,4 @@
 #include <stdlib.h>
-#include <netware.h>
-#include <screen.h>
 /*
 #include "testutil.h"
 */
@@ -9,11 +7,7 @@
 void _NonAppStop( void )
 {
   if (getenv("_IN_NETWARE_BASH_") == NULL) {
-    uint16_t row, col;
-
-    GetScreenSize(&row, &col);
-    gotorowcol(row-1, 0);
-    printf("<Press any key to close screen> ");
+    printf("\r\n<Press any key to close screen> ");
     getcharacter();
   }
 }
