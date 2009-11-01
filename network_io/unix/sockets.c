46aec8bf58308cd1e84c345e9e7c795354087890
Bojan Smojver <bojan@apache.org>
2009-11-01 06:01:01 +0000

    Set file/socket descriptor to -1 before close(), so that there is no chance of returning an already closed FD from apr_os_file_get()/apr_os_sock_get().
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@831641 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/sockets.c
+++ network_io/unix/sockets.c
@@ -38,6 +38,12 @@ static char generic_inaddr_any[GENERIC_INADDR_ANY_LEN] = {0};
 static apr_status_t socket_cleanup(void *sock)
 {
     apr_socket_t *thesocket = sock;
+    int sd = thesocket->socketdes;
+
+    /* Set socket descriptor to -1 before close(), so that there is no
+     * chance of returning an already closed FD from apr_os_sock_get().
+     */
+    thesocket->socketdes = -1;
 
 #if APR_HAVE_SOCKADDR_UN
     if (thesocket->bound && thesocket->local_addr->family == APR_UNIX) {
@@ -45,11 +51,13 @@ static apr_status_t socket_cleanup(void *sock)
         unlink(thesocket->local_addr->hostname);
     }
 #endif        
-    if (close(thesocket->socketdes) == 0) {
-        thesocket->socketdes = -1;
+    if (close(sd) == 0) {
         return APR_SUCCESS;
     }
     else {
+        /* Restore, close() was not successful. */
+        thesocket->socketdes = sd;
+
         return errno;
     }
 }
