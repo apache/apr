ddd2eca3f0fdf8026910c52a4d2904d2caed5b2d
William A. Rowe Jr <wrowe@apache.org>
2009-12-18 22:21:11 +0000

    Cleanups for various gcc warnings, conditional assignment and type errors
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@892387 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/win32/dir.c
+++ file_io/win32/dir.c
@@ -76,8 +76,8 @@ APR_DECLARE(apr_status_t) apr_dir_open(apr_dir_t **new, const char *dirname,
     ELSE_WIN_OS_IS_ANSI
     {
         /* Note that we won't open a directory that is greater than MAX_PATH,
-         * including the trailing /* wildcard suffix.  If a * won't fit, then
-         * neither will any other file name within the directory.
+         * counting the additional '/' '*' wildcard suffix.  If a * won't fit
+         * then neither will any other file name within the directory.
          * The length not including the trailing '*' is stored as rootlen, to
          * skip over all paths which are too long.
          */
@@ -128,9 +128,9 @@ APR_DECLARE(apr_status_t) apr_dir_read(apr_finfo_t *finfo, apr_int32_t wanted,
         if (thedir->dirhand == INVALID_HANDLE_VALUE) 
         {
             apr_status_t rv;
-            if (rv = utf8_to_unicode_path(wdirname, sizeof(wdirname) 
-                                                     / sizeof(apr_wchar_t), 
-                                          thedir->dirname)) {
+            if ((rv = utf8_to_unicode_path(wdirname, sizeof(wdirname) 
+                                                   / sizeof(apr_wchar_t), 
+                                           thedir->dirname))) {
                 return rv;
             }
             eos = wcschr(wdirname, '\0');
@@ -162,8 +162,8 @@ APR_DECLARE(apr_status_t) apr_dir_read(apr_finfo_t *finfo, apr_int32_t wanted,
                 return apr_get_os_error();
             }
         }
-        if (rv = unicode_to_utf8_path(thedir->name, APR_FILE_MAX * 3 + 1, 
-                                      thedir->w.entry->cFileName))
+        if ((rv = unicode_to_utf8_path(thedir->name, APR_FILE_MAX * 3 + 1, 
+                                       thedir->w.entry->cFileName)))
             return rv;
         fname = thedir->name;
     }
@@ -280,8 +280,9 @@ APR_DECLARE(apr_status_t) apr_dir_make(const char *path, apr_fileperms_t perm,
     {
         apr_wchar_t wpath[APR_PATH_MAX];
         apr_status_t rv;
-        if (rv = utf8_to_unicode_path(wpath, sizeof(wpath) 
-                                              / sizeof(apr_wchar_t), path)) {
+        if ((rv = utf8_to_unicode_path(wpath,
+                                       sizeof(wpath) / sizeof(apr_wchar_t),
+                                       path))) {
             return rv;
         }
         if (!CreateDirectoryW(wpath, NULL)) {
@@ -357,8 +358,9 @@ APR_DECLARE(apr_status_t) apr_dir_remove(const char *path, apr_pool_t *pool)
     {
         apr_wchar_t wpath[APR_PATH_MAX];
         apr_status_t rv;
-        if (rv = utf8_to_unicode_path(wpath, sizeof(wpath) 
-                                              / sizeof(apr_wchar_t), path)) {
+        if ((rv = utf8_to_unicode_path(wpath,
+                                       sizeof(wpath) / sizeof(apr_wchar_t),
+                                       path))) {
             return rv;
         }
         if (!RemoveDirectoryW(wpath)) {
