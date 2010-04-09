05ac6a72874d2be92a6457dfa5262d0f56dd7886
Jeff Trawick <trawick@apache.org>
2010-04-09 20:55:57 +0000

    backport from trunk r932585:
    fix misuse of autodata in initialization of the wakeup
    pipe when the pollset was created with APR_POLLSET_NOCOPY
    
    Submitted by: Neil Conway <nrc cs.berkeley.edu>
    Reviewed by: trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@932608 13f79535-47bb-0310-9956-ffa450edef68

--- poll/unix/pollset.c
+++ poll/unix/pollset.c
@@ -46,18 +46,18 @@ apr_file_socket_pipe_close(apr_file_t *file);
 static apr_status_t create_wakeup_pipe(apr_pollset_t *pollset)
 {
     apr_status_t rv;
-    apr_pollfd_t fd;
 
     if ((rv = apr_file_socket_pipe_create(&pollset->wakeup_pipe[0],
                                           &pollset->wakeup_pipe[1],
                                           pollset->pool)) != APR_SUCCESS)
         return rv;
-    fd.reqevents = APR_POLLIN;
-    fd.desc_type = APR_POLL_FILE;
-    fd.desc.f = pollset->wakeup_pipe[0];
-    /* Add the pipe to the pollset
-     */
-    return apr_pollset_add(pollset, &fd);
+
+    pollset->wakeup_pfd.p = pollset->pool;
+    pollset->wakeup_pfd.reqevents = APR_POLLIN;
+    pollset->wakeup_pfd.desc_type = APR_POLL_FILE;
+    pollset->wakeup_pfd.desc.f = pollset->wakeup_pipe[0];
+
+    return apr_pollset_add(pollset, &pollset->wakeup_pfd);
 }
 
 #else  /* !WIN32 */
@@ -79,15 +79,16 @@ static apr_status_t apr_file_socket_pipe_close(apr_file_t *file)
 static apr_status_t create_wakeup_pipe(apr_pollset_t *pollset)
 {
     apr_status_t rv;
-    apr_pollfd_t fd;
 
     if ((rv = apr_file_pipe_create(&pollset->wakeup_pipe[0],
                                    &pollset->wakeup_pipe[1],
                                    pollset->pool)) != APR_SUCCESS)
         return rv;
-    fd.reqevents = APR_POLLIN;
-    fd.desc_type = APR_POLL_FILE;
-    fd.desc.f = pollset->wakeup_pipe[0];
+
+    pollset->wakeup_pfd.p = pollset->pool;
+    pollset->wakeup_pfd.reqevents = APR_POLLIN;
+    pollset->wakeup_pfd.desc_type = APR_POLL_FILE;
+    pollset->wakeup_pfd.desc.f = pollset->wakeup_pipe[0];
 
     {
         int flags;
@@ -110,9 +111,7 @@ static apr_status_t create_wakeup_pipe(apr_pollset_t *pollset)
             return errno;
     }
 
-    /* Add the pipe to the pollset
-     */
-    return apr_pollset_add(pollset, &fd);
+    return apr_pollset_add(pollset, &pollset->wakeup_pfd);
 }
 #endif /* !APR_FILES_AS_SOCKETS */
 
@@ -125,7 +124,7 @@ void apr_pollset_drain_wakeup_pipe(apr_pollset_t *pollset)
 
     while (apr_file_read(pollset->wakeup_pipe[0], rb, &nr) == APR_SUCCESS) {
         /* Although we write just one byte to the other end of the pipe
-         * during wakeup, multiple treads could call the wakeup.
+         * during wakeup, multiple threads could call the wakeup.
          * So simply drain out from the input side of the pipe all
          * the data.
          */
