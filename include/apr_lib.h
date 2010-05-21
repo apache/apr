60002af05efb00e2a536b724dad3f629bdfd2c05
Greg Ames <gregames@apache.org>
2010-05-21 20:22:39 +0000

    use the OS's isascii() on EBCDIC systems
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@947160 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_lib.h
+++ include/apr_lib.h
@@ -212,7 +212,7 @@ APR_DECLARE(apr_status_t) apr_password_get(const char *prompt, char *pwbuf,
 /** @see islower*/
 #define apr_islower(c) (islower(((unsigned char)(c))))
 /** @see isascii */
-#ifdef isascii
+#if APR_CHARSET_EBCDIC || defined(isascii)
 #define apr_isascii(c) (isascii(((unsigned char)(c))))
 #else
 #define apr_isascii(c) (((c) & ~0x7f)==0)
