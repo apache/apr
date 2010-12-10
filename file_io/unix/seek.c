10c1fcd8f427820e77320a978fc2d005f121aa7f
Mladen Turk <mturk@apache.org>
2010-12-10 17:06:49 +0000

    Fix file_trunc for buffered files. Make sure the write buffer is flushed before truncate call
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1044440 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/seek.c
+++ file_io/unix/seek.c
@@ -33,7 +33,7 @@ static apr_status_t setptr(apr_file_t *thefile, apr_off_t pos )
     if (newbufpos >= 0 && newbufpos <= thefile->dataRead) {
         thefile->bufpos = newbufpos;
         rv = APR_SUCCESS;
-    } 
+    }
     else {
         if (lseek(thefile->filedes, pos, SEEK_SET) != -1) {
             thefile->bufpos = thefile->dataRead = 0;
@@ -98,6 +98,30 @@ APR_DECLARE(apr_status_t) apr_file_seek(apr_file_t *thefile, apr_seek_where_t wh
 
 apr_status_t apr_file_trunc(apr_file_t *fp, apr_off_t offset)
 {
+    if (fp->buffered) {
+        int rc = 0;
+        file_lock(fp);
+        if (fp->direction == 1 && fp->bufpos != 0) {
+            apr_off_t len = fp->filePtr + fp->bufpos;
+            if (offset < len) {
+                /* New file end fall below our write buffer limit.
+                 * Figure out if and what needs to be flushed.
+                 */
+                apr_off_t off = len - offset;
+                if (off >= 0 && off <= fp->bufpos)
+                    fp->bufpos = fp->bufpos - (size_t)off;
+                else
+                    fp->bufpos = 0;
+            }
+            rc = apr_file_flush_locked(fp);
+            /* Reset buffer positions for write mode */
+            fp->bufpos = fp->direction = fp->dataRead = 0;
+        }
+        if (rc) {
+            return rc;
+        }
+        file_unlock(fp);
+    }
     if (ftruncate(fp->filedes, offset) == -1) {
         return errno;
     }
