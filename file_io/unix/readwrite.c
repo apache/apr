2a9af0aaa9118cfebf35efcc523d0890f8d6006e
Stefan Fritsch <sf@apache.org>
2011-02-21 21:28:37 +0000

    apr_file_flush_locked(): Handle short writes.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@1073146 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/readwrite.c
+++ file_io/unix/readwrite.c
@@ -311,12 +311,16 @@ apr_status_t apr_file_flush_locked(apr_file_t *thefile)
     apr_status_t rv = APR_SUCCESS;
 
     if (thefile->direction == 1 && thefile->bufpos) {
-        apr_ssize_t written;
+        apr_ssize_t written = 0, ret;
 
         do {
-            written = write(thefile->filedes, thefile->buffer, thefile->bufpos);
-        } while (written == -1 && errno == EINTR);
-        if (written == -1) {
+            ret = write(thefile->filedes, thefile->buffer + written,
+                        thefile->bufpos - written);
+            if (ret > 0)
+                written += ret;
+        } while (written < thefile->bufpos &&
+                 (ret > 0 || (ret == -1 && errno == EINTR)));
+        if (ret == -1) {
             rv = errno;
         } else {
             thefile->filePtr += written;
