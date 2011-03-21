d5999f087ef8591f590e59e39e27281a7b146def
Jeff Trawick <trawick@apache.org>
2011-03-21 14:30:14 +0000

    merge r949209 from trunk
    axe unused variables
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.5.x@1083803 13f79535-47bb-0310-9956-ffa450edef68

--- mmap/win32/mmap.c
+++ mmap/win32/mmap.c
@@ -29,7 +29,6 @@ static apr_status_t mmap_cleanup(void *themmap)
 {
     apr_mmap_t *mm = themmap;
     apr_mmap_t *next = APR_RING_NEXT(mm,link);
-    apr_status_t rv = 0;
 
     /* we no longer refer to the mmaped region */
     APR_RING_REMOVE(mm,link);
