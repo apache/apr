3d2c16f36cefadec91ee8d66b9ef0ada40a2681e
Stefan Fritsch <sf@apache.org>
2011-06-01 19:40:14 +0000

    Fix some "no return statement in function returning non-void" warnings
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1130269 13f79535-47bb-0310-9956-ffa450edef68

--- crypto/apr_crypto_openssl.c
+++ crypto/apr_crypto_openssl.c
@@ -256,6 +256,7 @@ static apr_status_t crypto_get_block_key_types(apr_hash_t **types,
         const apr_crypto_t *f)
 {
     *types = f->types;
+    return APR_SUCCESS;
 }
 
 /**
@@ -270,6 +271,7 @@ static apr_status_t crypto_get_block_key_modes(apr_hash_t **modes,
         const apr_crypto_t *f)
 {
     *modes = f->modes;
+    return APR_SUCCESS;
 }
 
 /**
