25bd0fd15c64b62f5e5f7212dfa05e77b2f7f98d
Stefan Fritsch <sf@apache.org>
2011-10-15 20:48:52 +0000

    Fix race condition that could lead to EEXIST being returned
    PR: 51254
    Submitted by: William Lee <william lee rainstor com>,
                  Wim Lewis <wiml omnigroup com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1183688 13f79535-47bb-0310-9956-ffa450edef68

--- file_io/unix/dir.c
+++ file_io/unix/dir.c
@@ -305,9 +305,6 @@ apr_status_t apr_dir_make_recursive(const char *path, apr_fileperms_t perm,
     
     apr_err = apr_dir_make (path, perm, pool); /* Try to make PATH right out */
     
-    if (apr_err == EEXIST) /* It's OK if PATH exists */
-        return APR_SUCCESS;
-    
     if (apr_err == ENOENT) { /* Missing an intermediate dir */
         char *dir;
         
@@ -322,6 +319,14 @@ apr_status_t apr_dir_make_recursive(const char *path, apr_fileperms_t perm,
         if (!apr_err) 
             apr_err = apr_dir_make (path, perm, pool);
     }
+
+    /*
+     * It's OK if PATH exists. Timing issues can lead to the second
+     * apr_dir_make being called on existing dir, therefore this check
+     * has to come last.
+     */
+    if (APR_STATUS_IS_EEXIST(apr_err))
+        return APR_SUCCESS;
 
     return apr_err;
 }
