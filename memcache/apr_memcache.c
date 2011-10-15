d78ae5f8d80a8c4f1b75da41c73e8bdb64256ef2
Stefan Fritsch <sf@apache.org>
2011-10-15 20:49:40 +0000

    Fix possible segfault.
    PR: 51064
    Submitted by: Michajlo Matijkiw <michajlo_matijkiw comcast com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1183689 13f79535-47bb-0310-9956-ffa450edef68

--- memcache/apr_memcache.c
+++ memcache/apr_memcache.c
@@ -418,15 +418,17 @@ APR_DECLARE(apr_status_t) apr_memcache_server_create(apr_pool_t *p,
                                mc_conn_construct,       /* Make a New Connection */
                                mc_conn_destruct,        /* Kill Old Connection */
                                server, np);
+    if (rv != APR_SUCCESS) {
+        return rv;
+    }
 
     apr_reslist_cleanup_order_set(server->conns, APR_RESLIST_CLEANUP_FIRST);
 #else
     rv = mc_conn_construct((void**)&(server->conn), server, np);
-#endif
-
     if (rv != APR_SUCCESS) {
         return rv;
     }
+#endif
 
     *ms = server;
 
