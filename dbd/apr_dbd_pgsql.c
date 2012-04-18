4b2ed3427b49929af7b8a78cd649e768dbc6dd0e
Nick Kew <niq@apache.org>
2012-04-18 19:29:06 +0000

    Switch pgsql module from PQescapeString (which pgsql now deprecates) to PQescapeStringConn
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1327636 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_pgsql.c
+++ dbd/apr_dbd_pgsql.c
@@ -469,7 +469,7 @@ static const char *dbd_pgsql_escape(apr_pool_t *pool, const char *arg,
 {
     size_t len = strlen(arg);
     char *ret = apr_palloc(pool, 2*len + 2);
-    PQescapeString(ret, arg, len);
+    PQescapeStringConn(sql->conn, ret, arg, len, NULL);
     return ret;
 }
 
