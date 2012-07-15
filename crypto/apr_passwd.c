185c46b3d44d4f5f39621ddd98f9fe3f50f88b3e
Stefan Fritsch <sf@apache.org>
2012-07-15 21:44:55 +0000

    Increase the buffer size for the hashed string
    sha512-crypt with custom rounds= prefix needs 115 bytes plus length of the
    number of rounds string to store the resulting hash. An usable buffer size of
    119 limited this to 9999 rounds.
    
    Use 200 to allow for future hash algorithms with longer string lengths (e.g.
    due to longer salt).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1361811 13f79535-47bb-0310-9956-ffa450edef68

--- crypto/apr_passwd.c
+++ crypto/apr_passwd.c
@@ -99,7 +99,7 @@ static void crypt_mutex_unlock()
 APR_DECLARE(apr_status_t) apr_password_validate(const char *passwd, 
                                                 const char *hash)
 {
-    char sample[120];
+    char sample[200];
 #if !defined(WIN32) && !defined(BEOS) && !defined(NETWARE)
     char *crypt_pw;
 #endif
