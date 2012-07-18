e85467294b53e8aa86af4955754004d44e401b8b
Jeff Trawick <trawick@apache.org>
2012-07-18 11:51:37 +0000

    APR dbd FreeTDS support: Fix spurious API errors caused by unitialized fields.
    Use apr_pcalloc instead of apr_palloc when allocating the dbd
    structure so that all fields are initialized.
    
    Submitted by: TROY.LIU 劉春偉 <TROY.LIU deltaww.com.cn>
    Reviewed by: trawick
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1362892 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_freetds.c
+++ dbd/apr_dbd_freetds.c
@@ -627,7 +627,7 @@ static apr_dbd_t *dbd_freetds_open(apr_pool_t *pool, const char *params,
     if (process == NULL) {
         return NULL;
     }
-    sql = apr_palloc (pool, sizeof (apr_dbd_t));
+    sql = apr_pcalloc(pool, sizeof (apr_dbd_t));
     sql->pool = pool;
     sql->proc = process;
     sql->params = params;
