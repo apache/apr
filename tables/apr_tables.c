85844b217ebbd10415fedca69017cc70301fc1ad
Ruediger Pluem <rpluem@apache.org>
2012-08-03 07:45:18 +0000

    Merge r1367050 from trunk:
    * Prevent apr_table_mergen from aborting when APR_POOL_DEBUG is set and key or
      value are static literals not stored in pool managed memory.
    
    Submitted by: rpluem
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@1368817 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_tables.c
+++ tables/apr_tables.c
@@ -730,15 +730,18 @@ APR_DECLARE(void) apr_table_mergen(apr_table_t *t, const char *key,
     apr_table_entry_t *next_elt;
     apr_table_entry_t *end_elt;
     apr_uint32_t checksum;
+    apr_pool_t *pool;
     int hash;
 
 #if APR_POOL_DEBUG
     {
-	if (!apr_pool_is_ancestor(apr_pool_find(key), t->a.pool)) {
+	pool = apr_pool_find(key);
+	if ((pool != key) && (!apr_pool_is_ancestor(pool, t->a.pool))) {
 	    fprintf(stderr, "apr_table_mergen: key not in ancestor pool of t\n");
 	    abort();
 	}
-	if (!apr_pool_is_ancestor(apr_pool_find(val), t->a.pool)) {
+	pool = apr_pool_find(val);
+	if ((pool != val) && (!apr_pool_is_ancestor(pool, t->a.pool))) {
 	    fprintf(stderr, "apr_table_mergen: val not in ancestor pool of t\n");
 	    abort();
 	}
