edd6aa876a5b7e9a5a5003007ac9b0e65ff6579e
Ruediger Pluem <rpluem@apache.org>
2012-08-03 07:47:37 +0000

    * We only need the pool variable in the debug case.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1368819 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_tables.c
+++ tables/apr_tables.c
@@ -730,11 +730,11 @@ APR_DECLARE(void) apr_table_mergen(apr_table_t *t, const char *key,
     apr_table_entry_t *next_elt;
     apr_table_entry_t *end_elt;
     apr_uint32_t checksum;
-    apr_pool_t *pool;
     int hash;
 
 #if APR_POOL_DEBUG
     {
+	apr_pool_t *pool;
 	pool = apr_pool_find(key);
 	if ((pool != key) && (!apr_pool_is_ancestor(pool, t->a.pool))) {
 	    fprintf(stderr, "apr_table_mergen: key not in ancestor pool of t\n");
