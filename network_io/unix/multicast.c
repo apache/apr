apache/apr

commit 5b052120e6abedb31db8ba10548552c1b4da5974

Backport fix for apr_mcast_hops returning EINVAL from trunk
git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.4.x@1309386 13f79535-47bb-0310-9956-ffa450edef68

network_io/unix/multicast.c
@@ -194,7 +194,7 @@ static apr_status_t do_mcast(int type, apr_socket_t *sock,
 }
 
 static apr_status_t do_mcast_opt(int type, apr_socket_t *sock,
-                                 apr_byte_t value)
+                                 apr_uint32_t value)
 {
     apr_status_t rv = APR_SUCCESS;
 
@@ -205,24 +205,19 @@ static apr_status_t do_mcast_opt(int type, apr_socket_t *sock,
         }
     }
 #if APR_HAVE_IPV6
-    else if (sock_is_ipv6(sock) && type == IP_MULTICAST_LOOP) {
-        unsigned int loopopt = value;
-        type = IPV6_MULTICAST_LOOP;
-        if (setsockopt(sock->socketdes, IPPROTO_IPV6, type,
-                       (const void *) &loopopt, sizeof(loopopt)) == -1) {
-            rv = errno;
-        }
-    }
     else if (sock_is_ipv6(sock)) {
         if (type == IP_MULTICAST_TTL) {
             type = IPV6_MULTICAST_HOPS;
+        }
+        else if (type == IP_MULTICAST_LOOP) {
+            type = IPV6_MULTICAST_LOOP;
         }
         else {
             return APR_ENOTIMPL;
         }
 
         if (setsockopt(sock->socketdes, IPPROTO_IPV6, type,
-                       &value, sizeof(value)) == -1) {
+                       (const void *) &value, sizeof(value)) == -1) {
             rv = errno;
         }
     }

commit 05b54613e900a485e877954fb0c583f77476cd70

Merge 1487796 from trunk:
* network_io/unix/multicast.c (do_mcast_opt): Fix regression in
  handling IPv4 options introduced in r1309386.



git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.5.x@1487797 13f79535-47bb-0310-9956-ffa450edef68

network_io/unix/multicast.c
@@ -193,12 +193,17 @@ static apr_status_t do_mcast(int type, apr_socket_t *sock,
     return rv;
 }
 
+/* Set the IP_MULTICAST_TTL or IP_MULTICAST_LOOP option, or IPv6
+ * equivalents, for the socket, to the given value.  Note that this
+ * function *only works* for those particular option types. */
 static apr_status_t do_mcast_opt(int type, apr_socket_t *sock,
-                                 apr_uint32_t value)
+                                 apr_byte_t value)
 {
     apr_status_t rv = APR_SUCCESS;
 
     if (sock_is_ipv4(sock)) {
+        /* For the IP_MULTICAST_* options, this must be a (char *)
+         * pointer. */
         if (setsockopt(sock->socketdes, IPPROTO_IP, type,
                        (const void *) &value, sizeof(value)) == -1) {
             rv = errno;
@@ -206,6 +211,9 @@ static apr_status_t do_mcast_opt(int type, apr_socket_t *sock,
     }
 #if APR_HAVE_IPV6
     else if (sock_is_ipv6(sock)) {
+        /* For the IPV6_* options, an (int *) pointer must be used. */
+        int ivalue = value;
+
         if (type == IP_MULTICAST_TTL) {
             type = IPV6_MULTICAST_HOPS;
         }
@@ -217,7 +225,7 @@ static apr_status_t do_mcast_opt(int type, apr_socket_t *sock,
         }
 
         if (setsockopt(sock->socketdes, IPPROTO_IPV6, type,
-                       (const void *) &value, sizeof(value)) == -1) {
+                       (const void *) &ivalue, sizeof(ivalue)) == -1) {
             rv = errno;
         }
     }