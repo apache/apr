430d741e5910ab8a57c3b0fe1af1d3be3e2d7829
Jeff Trawick <trawick@apache.org>
2013-06-25 10:58:31 +0000

    Fix doc errors in APR header files.
    PR: 55133
    Submitted by: Mike Rumph <mike.rumph oracle.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1496407 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_thread_proc.h
+++ include/apr_thread_proc.h
@@ -115,7 +115,7 @@ typedef enum {
 #define APR_OC_REASON_DEATH         0     /**< child has died, caller must call
                                            * unregister still */
 #define APR_OC_REASON_UNWRITABLE    1     /**< currently unused. */
-#define APR_OC_REASON_RESTART       2     /**< a restart is occuring, perform
+#define APR_OC_REASON_RESTART       2     /**< a restart is occurring, perform
                                            * any necessary cleanup (including
                                            * sending a special signal to child)
                                            */
@@ -124,7 +124,7 @@ typedef enum {
                                            * kill the child) */
 #define APR_OC_REASON_LOST          4     /**< somehow the child exited without
                                            * us knowing ... buggy os? */
-#define APR_OC_REASON_RUNNING       5     /**< a health check is occuring, 
+#define APR_OC_REASON_RUNNING       5     /**< a health check is occurring, 
                                            * for most maintainence functions
                                            * this is a no-op.
                                            */
