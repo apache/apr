59e18ad776f78da4363cce693b31d5e8ea571d4a
Jeff Trawick <trawick@apache.org>
2013-10-20 20:50:11 +0000

    Follow-up to r1531768:
    Add apr_shm_create_ex() and apr_shm_attach_ex(), which
    provide the ability to select the Global or Local
    namespace on Windows.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1533979 13f79535-47bb-0310-9956-ffa450edef68

--- shmem/beos/shm.c
+++ shmem/beos/shm.c
@@ -71,6 +71,15 @@ APR_DECLARE(apr_status_t) apr_shm_create(apr_shm_t **m,
     return APR_SUCCESS;
 }
 
+APR_DECLARE(apr_status_t) apr_shm_create_ex(apr_shm_t **m, 
+                                            apr_size_t reqsize, 
+                                            const char *filename, 
+                                            apr_pool_t *p,
+                                            apr_int32_t flags)
+{
+    return apr_shm_create(m, reqsize, filename, p);
+}
+
 APR_DECLARE(apr_status_t) apr_shm_destroy(apr_shm_t *m)
 {
     delete_area(m->aid);
@@ -131,6 +140,14 @@ APR_DECLARE(apr_status_t) apr_shm_attach(apr_shm_t **m,
     (*m) = new_m;
     
     return APR_SUCCESS;
+}
+
+APR_DECLARE(apr_status_t) apr_shm_attach_ex(apr_shm_t **m,
+                                            const char *filename,
+                                            apr_pool_t *pool,
+                                            apr_int32_t flags)
+{
+    return apr_shm_attach(m, filename, pool);
 }
 
 APR_DECLARE(apr_status_t) apr_shm_detach(apr_shm_t *m)
