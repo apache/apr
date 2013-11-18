aa722626b47c75b2cf2c3d4e69ca9ecd6a237c6b
Jim Jagielski <jim@apache.org>
2013-11-18 14:10:58 +0000

    OSX (Darwin) returns APR_POLLHUP|APR_POLLIN, so the test for equality fails. Instead, just check for the bit.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1543033 13f79535-47bb-0310-9956-ffa450edef68

--- network_io/unix/socket_util.c
+++ network_io/unix/socket_util.c
@@ -46,7 +46,12 @@ APR_DECLARE(apr_status_t) apr_socket_atreadeof(apr_socket_t *sock, int *atreadeo
         /* Some other error -> unexpected error. */
         return rv;
     }
+#if defined(DARWIN)
+    /* OSX returns APR_POLLHUP|APR_POLLIN */
+    else if (nfds == 1 && (pfds[0].rtnevents & APR_POLLIN)  == APR_POLLIN) {
+#else
     else if (nfds == 1 && pfds[0].rtnevents == APR_POLLIN) {
+#endif
         apr_sockaddr_t unused;
         apr_size_t len = 1;
         char buf;
