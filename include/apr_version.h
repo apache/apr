c9a4e99bfd5784faf4d0d067031f3746fc342ec2
Rainer Jung <rjung@apache.org>
2014-01-03 18:57:28 +0000

    Happy new 2014!
    Backport of r1555197 from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.3.x@1555203 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_version.h
+++ include/apr_version.h
@@ -38,7 +38,7 @@
  */
 
 
-#define APR_COPYRIGHT "Copyright (c) 2013 The Apache Software " \
+#define APR_COPYRIGHT "Copyright (c) 2000-2014 The Apache Software " \
                       "Foundation or its licensors, as applicable."
 
 /* The numeric compile-time version constants. These constants are the
