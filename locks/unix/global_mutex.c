277d5dfd9bcc5a0c32ebb8adb9dc2a4fcab47bc6
Stefan Fritsch <sf@apache.org>
2014-05-14 20:06:22 +0000

    Backport r741871 from trunk:
        Use macro for determinig function name
    
    This fixes an undefined reference to apr_proc_mutex_set_perms()
    and  makes 1.6.x actually build.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.6.x@1594696 13f79535-47bb-0310-9956-ffa450edef68

--- locks/unix/global_mutex.c
+++ locks/unix/global_mutex.c
@@ -190,7 +190,7 @@ APR_PERMS_SET_IMPLEMENT(global_mutex)
     apr_status_t rv;
     apr_global_mutex_t *mutex = (apr_global_mutex_t *)theglobal_mutex;
 
-    rv = apr_proc_mutex_set_perms(mutex->proc_mutex, perms, uid, gid);
+    rv = APR_PERMS_SET_FN(proc_mutex)(mutex->proc_mutex, perms, uid, gid);
     return rv;
 }
 
