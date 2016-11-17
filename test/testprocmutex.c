542443095782afa2aaf275d8138bad48f0903150
Gregg Lewis Smith <gsmith@apache.org>
2016-11-17 08:29:24 +0000

    We may not have fork but we still need lockmech_t
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.6.x@1770128 13f79535-47bb-0310-9956-ffa450edef68

--- test/testprocmutex.c
+++ test/testprocmutex.c
@@ -25,6 +25,11 @@
 #include <stdlib.h>
 #include "testutil.h"
 
+typedef struct lockmech {
+    apr_lockmech_e num;
+    const char *name;
+} lockmech_t;
+
 #if APR_HAS_FORK
 
 #define MAX_ITER 200
@@ -34,11 +39,6 @@
 
 static apr_proc_mutex_t *proc_lock;
 static volatile int *x;
-
-typedef struct lockmech {
-    apr_lockmech_e num;
-    const char *name;
-} lockmech_t;
 
 /* a slower more racy way to implement (*x)++ */
 static int increment(int n)
