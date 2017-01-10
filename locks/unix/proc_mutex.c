c1dadca890e47a7c15ddaf22ad94490170d35d8c
Rainer Jung <rjung@apache.org>
2017-01-10 11:25:00 +0000

    locks: follow up to r1667900.
    Not all pthread implementations have mutex_timedlock(),
    like Solaris 8, so proc_mutex_proc_pthread_timedacquire()
    can return APR_ENOTIMPL.
    
    Backport of r1774973 from trunk.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.6.x@1778111 13f79535-47bb-0310-9956-ffa450edef68

--- locks/unix/proc_mutex.c
+++ locks/unix/proc_mutex.c
@@ -654,6 +654,7 @@ static apr_status_t proc_mutex_pthread_timedacquire(apr_proc_mutex_t *mutex,
                                                     apr_time_t timeout,
                                                     int absolute)
 {
+#ifdef HAVE_PTHREAD_MUTEX_TIMEDLOCK
     if (timeout < 0) {
         return proc_mutex_pthread_acquire(mutex);
     }
@@ -688,6 +689,9 @@ static apr_status_t proc_mutex_pthread_timedacquire(apr_proc_mutex_t *mutex,
     }
     mutex->curr_locked = 1;
     return APR_SUCCESS;
+#else
+    return APR_ENOTIMPL;
+#endif
 }
 
 static apr_status_t proc_mutex_pthread_release(apr_proc_mutex_t *mutex)
