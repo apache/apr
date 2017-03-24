5100b8a86a122795ef00388cd0b6fe7401db363c
Yann Ylavic <ylavic@apache.org>
2017-03-24 14:00:00 +0000

    Merge r1788334, r1788337 from trunk:
    apr_allocator: Provide apr_allocator_align() to get the true size that
    would be allocated for the given size (including the header and alignment).
    
    
    Follow up to r1788334: better apr_allocator_align() description.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.6.x@1788449 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_allocator.h
+++ include/apr_allocator.h
@@ -103,6 +103,14 @@ APR_DECLARE(apr_memnode_t *) apr_allocator_alloc(apr_allocator_t *allocator,
 APR_DECLARE(void) apr_allocator_free(apr_allocator_t *allocator,
                                      apr_memnode_t *memnode)
                   __attribute__((nonnull(1,2)));
+ 
+/**
+ * Get the true size that would be allocated for the given size (including
+ * the header and alignment).
+ * @param size The size to align
+ * @return The aligned size (or zero on apr_size_t overflow)
+ */
+APR_DECLARE(apr_size_t) apr_allocator_align(apr_size_t size);
 
 #include "apr_pools.h"
 
