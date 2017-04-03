2f663ccadbdd4d1141f62b5cce36966f0c8efd26
Yann Ylavic <ylavic@apache.org>
2017-04-03 20:18:45 +0000

    Merge r1790045 from trunk:
    Follow up to r1667900: don't fail test{proc,global}mutex if
    APR_LOCK_DEFAULT_TIMED is not implemented (e.g. MacOS).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.6.x@1790047 13f79535-47bb-0310-9956-ffa450edef68

--- test/testglobalmutex.c
+++ test/testglobalmutex.c
@@ -84,6 +84,11 @@ static void test_exclusive(abts_case *tc, void *data)
     abts_log_message(mutexname(mech));
  
     rv = apr_global_mutex_create(&global_lock, LOCKNAME, mech, p);
+    if (rv == APR_ENOTIMPL) {
+        /* MacOS lacks TIMED implementation, so don't fail for ENOTIMPL */
+        ABTS_NOT_IMPL(tc, "global mutex TIMED not implemented");
+        return;
+    }
     APR_ASSERT_SUCCESS(tc, "Error creating mutex", rv);
 
     launch_child(tc, mech, &p1, p);
