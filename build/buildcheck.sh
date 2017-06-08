1116d61e301a749eaedc8fbf827d89e66a01ac86
William A. Rowe Jr <wrowe@apache.org>
2017-06-08 18:39:35 +0000

    Rather than fetch-me-a-rock, report all tool check results for all the tools on any attempt, then fail if any of the tools are missing/old.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1798105 13f79535-47bb-0310-9956-ffa450edef68

--- build/buildcheck.sh
+++ build/buildcheck.sh
@@ -1,35 +1,37 @@
 #! /bin/sh
 
 echo "buildconf: checking installation..."
+res=0
 
 # any python
 python=`build/PrintPath python`
 if test -z "$python"; then
-echo "buildconf: python not found."
-echo "           You need python installed"
-echo "           to build APR from SVN."
-exit 1
+  echo "buildconf: python not found."
+  echo "           You need python installed"
+  echo "           to build APR from SVN."
+  res=1
 else
-py_version=`python -c 'import sys; print sys.version' 2>&1|sed 's/ .*//;q'`
-echo "buildconf: python version $py_version (ok)"
+  py_version=`python -c 'import sys; print sys.version' 2>&1|sed 's/ .*//;q'`
+  echo "buildconf: python version $py_version (ok)"
 fi
 
 # autoconf 2.59 or newer
 ac_version=`${AUTOCONF:-autoconf} --version 2>/dev/null|sed -e 's/^[^0-9]*//;s/[a-z]* *$//;q'`
 if test -z "$ac_version"; then
-echo "buildconf: autoconf not found."
-echo "           You need autoconf version 2.59 or newer installed"
-echo "           to build APR from SVN."
-exit 1
-fi
-IFS=.; set $ac_version; IFS=' '
-if test "$1" = "2" -a "$2" -lt "59" || test "$1" -lt "2"; then
-echo "buildconf: autoconf version $ac_version found."
-echo "           You need autoconf version 2.59 or newer installed"
-echo "           to build APR from SVN."
-exit 1
+  echo "buildconf: autoconf not found."
+  echo "           You need autoconf version 2.59 or newer installed"
+  echo "           to build APR from SVN."
+  res=1
 else
-echo "buildconf: autoconf version $ac_version (ok)"
+  IFS=.; set $ac_version; IFS=' '
+  if test "$1" = "2" -a "$2" -lt "59" || test "$1" -lt "2"; then
+    echo "buildconf: autoconf version $ac_version found."
+    echo "           You need autoconf version 2.59 or newer installed"
+    echo "           to build APR from SVN."
+    res=1
+  else
+    echo "buildconf: autoconf version $ac_version (ok)"
+  fi
 fi
 
 # Sample libtool --version outputs:
@@ -41,26 +43,28 @@ fi
 libtool=`build/PrintPath glibtool1 glibtool libtool libtool15 libtool14`
 lt_pversion=`$libtool --version 2>/dev/null|sed -e 's/([^)]*)//g;s/^[^0-9]*//;s/[- ].*//g;q'`
 if test -z "$lt_pversion"; then
-echo "buildconf: libtool not found."
-echo "           You need libtool version 1.4 or newer installed"
-echo "           to build APR from SVN."
-exit 1
-fi
-lt_version=`echo $lt_pversion|sed -e 's/\([a-z]*\)$/.\1/'`
-IFS=.; set $lt_version; IFS=' '
-lt_status="good"
-if test "$1" = "1"; then
-   if test "$2" -lt "4"; then
+  echo "buildconf: libtool not found."
+  echo "           You need libtool version 1.4 or newer installed"
+  echo "           to build APR from SVN."
+  res=1
+else
+  lt_version=`echo $lt_pversion|sed -e 's/\([a-z]*\)$/.\1/'`
+  IFS=.; set $lt_version; IFS=' '
+  lt_status="good"
+  if test "$1" = "1"; then
+    if test "$2" -lt "4"; then
       lt_status="bad"
-   fi
-fi
-if test $lt_status = "good"; then
-   echo "buildconf: libtool version $lt_pversion (ok)"
-   exit 0
+    fi
+  fi
+  if test $lt_status = "good"; then
+    echo "buildconf: libtool version $lt_pversion (ok)"
+  else
+    echo "buildconf: libtool version $lt_pversion found."
+    echo "           You need libtool version 1.4 or newer installed"
+    echo "           to build APR from SVN."
+    res=1
+  fi
 fi
 
-echo "buildconf: libtool version $lt_pversion found."
-echo "           You need libtool version 1.4 or newer installed"
-echo "           to build APR from SVN."
+exit $res
 
-exit 1
