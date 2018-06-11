f1eace80be8fd28026ae6bb11aafb075065104f2
Graham Leggett <minfrin@apache.org>
2018-06-11 22:26:23 +0000

    Correct spelling.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1833361 13f79535-47bb-0310-9956-ffa450edef68

--- encoding/apr_base64.c
+++ encoding/apr_base64.c
@@ -170,7 +170,7 @@ APR_DECLARE(int) apr_base64_decode_binary(unsigned char *bufplain,
 	nprbytes -= 4;
     }
 
-    /* Note: (nprbytes == 1) would be an error, so just ingore that case */
+    /* Note: (nprbytes == 1) would be an error, so just ignore that case */
     if (nprbytes > 1) {
 	*(bufout++) =
 	    (unsigned char) (pr2six[*bufin] << 2 | pr2six[bufin[1]] >> 4);
