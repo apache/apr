cd5bd6972b83ba99d44242db0c4a809763284fc0
Christophe Jaillet <jailletc36@apache.org>
2019-05-04 11:48:09 +0000

    Fix some missing doxygen documentation (even if ready to use!)
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1858596 13f79535-47bb-0310-9956-ffa450edef68

--- include/apr_network_io.h
+++ include/apr_network_io.h
@@ -441,7 +441,8 @@ APR_DECLARE(apr_status_t) apr_sockaddr_info_copy(apr_sockaddr_t **dst,
                                                  const apr_sockaddr_t *src,
                                                  apr_pool_t *p);
 
-/* Set the zone of an IPv6 link-local address object.
+/**
+ * Set the zone of an IPv6 link-local address object.
  * @param sa Socket address object
  * @param zone_id Zone ID (textual "eth0" or numeric "3").
  * @return Returns APR_EBADIP for non-IPv6 socket or an IPv6 address
@@ -451,7 +452,8 @@ APR_DECLARE(apr_status_t) apr_sockaddr_zone_set(apr_sockaddr_t *sa,
                                                 const char *zone_id);
 
 
-/* Retrieve the zone of an IPv6 link-local address object.
+/**
+ * Retrieve the zone of an IPv6 link-local address object.
  * @param sa Socket address object
  * @param name If non-NULL, set to the textual representation of the zone id
  * @param id If non-NULL, set to the integer zone id
