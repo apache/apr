bde57b2568fc67f2d766c47e14d820b01511e3ac
Ivan Zhakov <ivan@apache.org>
2019-06-11 17:22:46 +0000

    Fix problem that apr_get_oslevel() was returning APR_WIN_XP on Windows 10.
    * include/arch/win32/apr_arch_misc.h
      (enum apr_oslevel_e): Add APR_WIN_10.
    
    * misc/win32/misc.c
      (apr_get_oslevel): Return APR_WIN_10 when dwMajorVersion is greater than 6.
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1861054 13f79535-47bb-0310-9956-ffa450edef68

--- include/arch/win32/apr_arch_misc.h
+++ include/arch/win32/apr_arch_misc.h
@@ -111,7 +111,8 @@ typedef enum {
         APR_WIN_VISTA =    80,
         APR_WIN_7  =       90,
         APR_WIN_8  =       100,
-        APR_WIN_8_1 =      110
+        APR_WIN_8_1 =      110,
+        APR_WIN_10 =       120
 } apr_oslevel_e;
 
 extern APR_DECLARE_DATA apr_oslevel_e apr_os_level;
