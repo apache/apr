f56a1322d8dbc6a830ae5249d6cc2820fe87c541
Yann Ylavic <ylavic@apache.org>
2021-11-26 18:15:23 +0000

    apr_sleep: use nanosleep() when available.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1895361 13f79535-47bb-0310-9956-ffa450edef68

--- time/unix/time.c
+++ time/unix/time.c
@@ -242,6 +242,11 @@ APR_DECLARE(void) apr_sleep(apr_interval_time_t t)
     snooze(t);
 #elif defined(NETWARE)
     delay(t/1000);
+#elif defined(HAVE_NANOSLEEP)
+    struct timespec ts;
+    ts.tv_sec = t / APR_USEC_PER_SEC; 
+    ts.tv_nsec = (t % APR_USEC_PER_SEC) * 1000;
+    nanosleep(&ts, NULL);
 #else
     struct timeval tv;
     tv.tv_usec = t % APR_USEC_PER_SEC;
