6573ac6e2fb17c2819ca65708d39f1deb37b1790
Joe Orton <jorton@apache.org>
2022-02-15 09:47:41 +0000

    Merge r1898076 from trunk:
    Fix various harmless cases of undefined behaviour, and add a Travis
    job testing under UBSan.
    
    * poll/unix/poll.c (apr_poll): For the on-stack array allocation use
      num+1 since allocating a 0-length array is undefined behaviour.
    
    * tables/apr_skiplist.c (get_b_rand): Use unsigned integers to avoid
      signed integer overflow in the left shift.
      (skiplist_qpush): Avoid calling memcpy(,NULL,0).
    
    * random/unix/apr_random.c (apr_random_add_entropy): Avoid calling
      memcpy(,NULL,0).
    
    * test/teststr.c (overflow_strfsize): Avoid signed integer overflow.
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.8.x@1898099 13f79535-47bb-0310-9956-ffa450edef68

--- tables/apr_skiplist.c
+++ tables/apr_skiplist.c
@@ -60,15 +60,15 @@ struct apr_skiplistnode {
     apr_skiplist *sl;
 };
 
-static int get_b_rand(void)
+static unsigned int get_b_rand(void)
 {
-    static int ph = 32;         /* More bits than we will ever use */
-    static int randseq;
+    static unsigned int ph = 32;         /* More bits than we will ever use */
+    static unsigned int randseq;
     if (ph > 31) {              /* Num bits in return of rand() */
         ph = 0;
         randseq = rand();
     }
-    return randseq & (1 << ph++);
+    return randseq & (1U << ph++);
 }
 
 typedef struct {
@@ -159,7 +159,7 @@ static apr_status_t skiplist_qpush(apr_skiplist_q *q, apr_skiplistnode *m)
         size_t size = (q->pos) ? q->pos * 2 : 32;
         if (q->p) {
             data = apr_palloc(q->p, size * sizeof(*data));
-            if (data) {
+            if (data && q->data) {
                 memcpy(data, q->data, q->pos * sizeof(*data));
             }
         }
