95eed0b1b7ad268a2a64e8df37b4fa6a75978bc5
Yann Ylavic <ylavic@apache.org>
2022-06-23 10:40:39 +0000

    test/abts: More numeric types to compare/assert.
    * test/abts.h():
      Declare abts_{uint,long,ulong,llong,ullong}_{n,}equal() and the corresponding
      ABTS_{UINT,LONG,ULONG,LLONG,ULLONG}_{N,}EQUAL() wrappers.
    
    * test/abts.c():
      Implement abts_{uint,long,ulong,llong,ullong}_{n,}equal() using a common macro.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1902191 13f79535-47bb-0310-9956-ffa450edef68

--- test/abts.h
+++ test/abts.h
@@ -72,6 +72,28 @@ void abts_log_message(const char *fmt, ...);
 
 void abts_int_equal(abts_case *tc, const int expected, const int actual, int lineno);
 void abts_int_nequal(abts_case *tc, const int expected, const int actual, int lineno);
+void abts_uint_equal(abts_case *tc, const unsigned int expected,
+                     const unsigned int actual, int lineno);
+void abts_uint_nequal(abts_case *tc, const unsigned int expected,
+                      const unsigned int actual, int lineno);
+void abts_long_equal(abts_case *tc, const long expected,
+                     const long actual, int lineno);
+void abts_long_nequal(abts_case *tc, const long expected,
+                      const long actual, int lineno);
+void abts_ulong_equal(abts_case *tc, const unsigned long expected,
+                      const unsigned long actual, int lineno);
+void abts_ulong_nequal(abts_case *tc, const unsigned long expected,
+                       const unsigned long actual, int lineno);
+void abts_llong_equal(abts_case *tc, const long long expected,
+                      const long long actual, int lineno);
+void abts_llong_nequal(abts_case *tc, const long long expected,
+                       const long long actual, int lineno);
+void abts_ullong_equal(abts_case *tc, const unsigned long long expected,
+                       const unsigned long long actual, int lineno);
+void abts_ullong_nequal(abts_case *tc, const unsigned long long expected,
+                        const unsigned long long actual, int lineno);
+void abts_size_equal(abts_case *tc, size_t expected, size_t actual, int lineno);
+void abts_size_nequal(abts_case *tc, size_t expected, size_t actual, int lineno);
 void abts_str_equal(abts_case *tc, const char *expected, const char *actual, int lineno);
 void abts_str_nequal(abts_case *tc, const char *expected, const char *actual,
                        size_t n, int lineno);
@@ -82,11 +104,22 @@ void abts_fail(abts_case *tc, const char *message, int lineno);
 void abts_skip(abts_case *tc, const char *message, int lineno);
 void abts_not_impl(abts_case *tc, const char *message, int lineno);
 void abts_assert(abts_case *tc, const char *message, int condition, int lineno);
-void abts_size_equal(abts_case *tc, size_t expected, size_t actual, int lineno);
 
 /* Convenience macros. Ryan hates these! */
 #define ABTS_INT_EQUAL(a, b, c)     abts_int_equal(a, b, c, __LINE__)
 #define ABTS_INT_NEQUAL(a, b, c)    abts_int_nequal(a, b, c, __LINE__)
+#define ABTS_UINT_EQUAL(a, b, c)    abts_uint_equal(a, b, c, __LINE__)
+#define ABTS_UINT_NEQUAL(a, b, c)   abts_uint_nequal(a, b, c, __LINE__)
+#define ABTS_LONG_EQUAL(a, b, c)    abts_long_equal(a, b, c, __LINE__)
+#define ABTS_LONG_NEQUAL(a, b, c)   abts_long_nequal(a, b, c, __LINE__)
+#define ABTS_ULONG_EQUAL(a, b, c)   abts_ulong_equal(a, b, c, __LINE__)
+#define ABTS_ULONG_NEQUAL(a, b, c)  abts_ulong_nequal(a, b, c, __LINE__)
+#define ABTS_LLONG_EQUAL(a, b, c)   abts_llong_equal(a, b, c, __LINE__)
+#define ABTS_LLONG_NEQUAL(a, b, c)  abts_llong_nequal(a, b, c, __LINE__)
+#define ABTS_ULLONG_EQUAL(a, b, c)  abts_ullong_equal(a, b, c, __LINE__)
+#define ABTS_ULLONG_NEQUAL(a, b, c) abts_ullong_nequal(a, b, c, __LINE__)
+#define ABTS_SIZE_EQUAL(a, b, c)    abts_size_equal(a, b, c, __LINE__)
+#define ABTS_SIZE_NEQUAL(a, b, c)   abts_size_nequal(a, b, c, __LINE__)
 #define ABTS_STR_EQUAL(a, b, c)     abts_str_equal(a, b, c, __LINE__)
 #define ABTS_STR_NEQUAL(a, b, c, d) abts_str_nequal(a, b, c, d, __LINE__)
 #define ABTS_PTR_NOTNULL(a, b)      abts_ptr_notnull(a, b, __LINE__)
@@ -95,8 +128,6 @@ void abts_size_equal(abts_case *tc, size_t expected, size_t actual, int lineno);
 #define ABTS_FAIL(a, b)             abts_fail(a, b, __LINE__);
 #define ABTS_NOT_IMPL(a, b)         abts_not_impl(a, b, __LINE__);
 #define ABTS_ASSERT(a, b, c)        abts_assert(a, b, c, __LINE__);
-
-#define ABTS_SIZE_EQUAL(a, b, c)    abts_size_equal(a, b, c, __LINE__)
 
 /* When skipping tests, make a reference to the test data parameter
    to avoid unused variable warnings. */
