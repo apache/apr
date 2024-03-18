777b6880b29f9e14bfcdd9d293439540590ce35c
Yann Ylavic <ylavic@apache.org>
2024-03-18 15:51:53 +0000

    testescape: missing ';' in entity escaping.
    apr_escape_entity(): Don't truncate the final ';' in hex entities.
    
    apr_snprintf() takes the buffer size (include NUL), and can by used with
    NULL/zero-size buffer to determine the output length.
    
    
    Merges r1916392 from ^/apr/apr/branches/1.8.x
    Merges r1916332, r1916390 from ^/apr/apr/trunk
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/branches/1.7.x@1916394 13f79535-47bb-0310-9956-ffa450edef68

--- encoding/apr_escape.c
+++ encoding/apr_escape.c
@@ -571,7 +571,7 @@ APR_DECLARE(apr_status_t) apr_escape_entity(char *escaped, const char *str,
                     found = 1;
                 }
                 else if (toasc && !apr_isascii(c)) {
-                    int offset = apr_snprintf((char *) d, 6, "&#%3.3d;", c);
+                    int offset = apr_snprintf((char *) d, 7, "&#%3.3d;", c);
                     size += offset;
                     d += offset;
                     found = 1;
@@ -613,8 +613,7 @@ APR_DECLARE(apr_status_t) apr_escape_entity(char *escaped, const char *str,
                     found = 1;
                 }
                 else if (toasc && !apr_isascii(c)) {
-                    char buf[8];
-                    size += apr_snprintf(buf, 6, "&#%3.3d;", c);
+                    size += apr_snprintf(NULL, 0, "&#%3.3d;", c);
                     found = 1;
                 }
                 else {
