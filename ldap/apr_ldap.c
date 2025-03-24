d4219ebc71c7ea18bf858c98d0d92c065aa27d46
Graham Leggett <minfrin@apache.org>
2025-03-24 14:50:37 +0000

    apr_ldap: Fix the switching on and off of LDAP_OPT_SSL in the Microsoft LDAP SDK.
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1924556 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/apr_ldap.c
+++ ldap/apr_ldap.c
@@ -610,8 +610,7 @@ static int option_set_tls(LDAP *ldap, const void *invalue,
     /* Microsoft SDK */
 #if APR_HAS_MICROSOFT_LDAPSDK
     if (tls == APR_LDAP_NONE) {
-        ULONG ul = (ULONG) LDAP_OPT_OFF;
-        result->rc = ldap_set_option(ldap, LDAP_OPT_SSL, &ul);
+        result->rc = ldap_set_option(ldap, LDAP_OPT_SSL, LDAP_OPT_OFF);
         if (result->rc != LDAP_SUCCESS) {
             result->reason = "LDAP: an attempt to set LDAP_OPT_SSL off "
                              "failed.";
@@ -619,8 +618,7 @@ static int option_set_tls(LDAP *ldap, const void *invalue,
         }
     }
     else if (tls == APR_LDAP_SSL) {
-        ULONG ul = (ULONG) LDAP_OPT_ON;
-        result->rc = ldap_set_option(ldap, LDAP_OPT_SSL, &ul);
+        result->rc = ldap_set_option(ldap, LDAP_OPT_SSL, LDAP_OPT_ON);
         if (result->rc != LDAP_SUCCESS) {
             result->reason = "LDAP: an attempt to set LDAP_OPT_SSL on "
                              "failed.";
