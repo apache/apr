da2256b686fcccc8dddcb2285de539ace4b621a8
Eric Covener <covener@apache.org>
2026-08-06 12:39:19 +0000

    build break review from rpluem
    patch by IBM Bob, still not compiled.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/apr/apr/trunk@1936912 13f79535-47bb-0310-9956-ffa450edef68

--- dbd/apr_dbd_oracle.c
+++ dbd/apr_dbd_oracle.c
@@ -853,15 +853,15 @@ static const char *dbd_oracle_escape(apr_pool_t *pool, const char *arg,
     int qcount;
 
     /* return the original if there are no single-quotes */
-    if (!(sq = strchr(s, '\'')))
-        return (char *)s;
+    if (!(sq = strchr(arg, '\'')))
+        return (char *)arg;
     /* count the single-quotes and allocate a new buffer */
     for (qcount = 1; (sq = strchr(sq + 1, '\'')); )
         qcount++;
-    newstr = apr_palloc(pool, strlen(s) + qcount + 1);
+    newstr = apr_palloc(pool, strlen(arg) + qcount + 1);
 
     /* move chars, doubling all single-quotes */
-    src = (char *)s;
+    src = (char *)arg;
     for (dst = newstr; *src; src++) {
         if ((*dst++ = *src) == '\'')
             *dst++ = '\'';
